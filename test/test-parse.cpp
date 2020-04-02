8ac46408a7682aa635da3ab57e23cd50cb8a8e74
Matthew Glazar <strager.nds@gmail.com>
2020-04-01 20:49:00 -0700

    Report more kinds of let parsing errors

--- test/test-parse.cpp
+++ test/test-parse.cpp
@@ -20,40 +20,43 @@ class parser {
   void parse_let_bindings(Visitor &v) {
     token let = this->lexer_.peek();
     this->lexer_.skip();
-    const token &t = this->lexer_.peek();
-    switch (t.type) {
-      default:
-        v.visit_error_let_with_no_bindings(let.range(this->original_input_));
-        break;
-
-      case token_type::identifier:
-        v.visit_variable_declaration(t.identifier_name());
+    bool first_binding = true;
+    for (;;) {
+      token comma;
+      if (!first_binding) {
+        comma = this->lexer_.peek();
+        if (comma.type != token_type::comma) {
+          break;
+        }
         this->lexer_.skip();
+      }
 
-        for (;;) {
-          token comma = this->lexer_.peek();
-          if (comma.type != token_type::comma) {
-            break;
-          }
+      const token &t2 = this->lexer_.peek();
+      switch (t2.type) {
+        case token_type::identifier:
+          v.visit_variable_declaration(t2.identifier_name());
           this->lexer_.skip();
-
-          const token &t2 = this->lexer_.peek();
-          switch (t2.type) {
-            case token_type::identifier:
-              v.visit_variable_declaration(t2.identifier_name());
-              this->lexer_.skip();
-              break;
-            default:
-              v.visit_error_stray_comma_in_let_statement(
-                  comma.range(this->original_input_));
-              break;
+          break;
+        case token_type::_if:
+        case token_type::number:
+          v.visit_error_invalid_binding_in_let_statement(
+              t2.range(this->original_input_));
+          break;
+        default:
+          if (first_binding) {
+            v.visit_error_let_with_no_bindings(
+                let.range(this->original_input_));
+          } else {
+            v.visit_error_stray_comma_in_let_statement(
+                comma.range(this->original_input_));
           }
-        }
+          break;
+      }
+      first_binding = false;
+    }
 
-        if (this->lexer_.peek().type == token_type::semicolon) {
-          this->lexer_.skip();
-        }
-        break;
+    if (this->lexer_.peek().type == token_type::semicolon) {
+      this->lexer_.skip();
     }
   }
 
@@ -72,6 +75,10 @@ struct visitor {
   };
   std::vector<visited_variable_declaration> variable_declarations;
 
+  void visit_error_invalid_binding_in_let_statement(source_range where) {
+    this->errors.emplace_back(error_invalid_binding_in_let_statement{where});
+  }
+
   void visit_error_let_with_no_bindings(source_range where) {
     this->errors.emplace_back(error_let_with_no_bindings{where});
   }
@@ -83,9 +90,11 @@ struct visitor {
   struct error {
     source_range where;
   };
+  struct error_invalid_binding_in_let_statement : error {};
   struct error_let_with_no_bindings : error {};
   struct error_stray_comma_in_let_statement : error {};
-  using visited_error = std::variant<error_let_with_no_bindings,
+  using visited_error = std::variant<error_invalid_binding_in_let_statement,
+                                     error_let_with_no_bindings,
                                      error_stray_comma_in_let_statement>;
   std::vector<visited_error> errors;
 };
@@ -145,7 +154,7 @@ TEST_CASE("parse invalid let") {
     parser p("let");
     p.parse_statement(v);
     CHECK(v.variable_declarations.empty());
-    CHECK(v.errors.size() == 1);
+    REQUIRE(v.errors.size() == 1);
     auto *error =
         std::get_if<visitor::error_let_with_no_bindings>(&v.errors[0]);
     REQUIRE(error);
@@ -158,11 +167,50 @@ TEST_CASE("parse invalid let") {
     parser p("let a,");
     p.parse_statement(v);
     CHECK(v.variable_declarations.size() == 1);
-    CHECK(v.errors.size() == 1);
+    REQUIRE(v.errors.size() == 1);
     auto *error =
         std::get_if<visitor::error_stray_comma_in_let_statement>(&v.errors[0]);
     REQUIRE(error);
     CHECK(error->where.begin_offset() == 5);
+    CHECK(error->where.end_offset() == 6);
+  }
+
+  {
+    visitor v;
+    parser p("let x, 42");
+    p.parse_statement(v);
+    CHECK(v.variable_declarations.size() == 1);
+    REQUIRE(v.errors.size() == 1);
+    auto *error = std::get_if<visitor::error_invalid_binding_in_let_statement>(
+        &v.errors[0]);
+    REQUIRE(error);
+    CHECK(error->where.begin_offset() == 7);
+    CHECK(error->where.end_offset() == 9);
+  }
+
+  {
+    visitor v;
+    parser p("let if");
+    p.parse_statement(v);
+    CHECK(v.variable_declarations.size() == 0);
+    REQUIRE(v.errors.size() == 1);
+    auto *error = std::get_if<visitor::error_invalid_binding_in_let_statement>(
+        &v.errors[0]);
+    REQUIRE(error);
+    CHECK(error->where.begin_offset() == 4);
+    CHECK(error->where.end_offset() == 6);
+  }
+
+  {
+    visitor v;
+    parser p("let 42");
+    p.parse_statement(v);
+    CHECK(v.variable_declarations.size() == 0);
+    CHECK(v.errors.size() == 1);
+    auto *error = std::get_if<visitor::error_invalid_binding_in_let_statement>(
+        &v.errors[0]);
+    REQUIRE(error);
+    CHECK(error->where.begin_offset() == 4);
     CHECK(error->where.end_offset() == 6);
   }
 }
