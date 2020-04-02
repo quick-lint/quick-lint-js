19bcb97a24f9a95bac9d51aa291d5ea7b57d0595
Matthew Glazar <strager.nds@gmail.com>
2020-04-01 22:53:58 -0700

    Parse function calls

--- test/test-parse.cpp
+++ test/test-parse.cpp
@@ -19,6 +19,7 @@ class parser {
   void parse_expression(Visitor &v) {
     bool is_first_token = true;
     bool last_token_was_operator = false;
+    bool identifier_is_allowed = true;
     token last_operator;
     std::vector<token> left_parens;
     for (;;) {
@@ -26,17 +27,24 @@ class parser {
         case token_type::left_paren:
           left_parens.emplace_back(this->peek());
           this->lexer_.skip();
+          identifier_is_allowed = true;
           break;
 
         case token_type::right_paren:
           left_parens.pop_back();
           this->lexer_.skip();
+          identifier_is_allowed = false;
           break;
 
         case token_type::identifier:
+          if (!identifier_is_allowed) {
+            v.visit_error_unexpected_identifier(
+                this->peek().range(this->original_input_));
+          }
           v.visit_variable_use(this->peek().identifier_name());
           this->lexer_.skip();
           last_token_was_operator = false;
+          identifier_is_allowed = false;
           break;
 
         case token_type::number:
@@ -48,10 +56,12 @@ class parser {
           last_operator = this->peek();
           this->lexer_.skip();
           last_token_was_operator = true;
+          identifier_is_allowed = true;
           break;
 
         case token_type::ampersand:
         case token_type::circumflex:
+        case token_type::comma:
         case token_type::star:
           if (is_first_token || last_token_was_operator) {
             const token &bad_token =
@@ -62,6 +72,7 @@ class parser {
           last_operator = this->peek();
           this->lexer_.skip();
           last_token_was_operator = true;
+          identifier_is_allowed = true;
           break;
 
         default:
@@ -165,6 +176,10 @@ struct visitor {
     this->errors.emplace_back(error_stray_comma_in_let_statement{where});
   }
 
+  void visit_error_unexpected_identifier(source_range where) {
+    this->errors.emplace_back(error_unexpected_identifier{where});
+  }
+
   void visit_error_unmatched_parenthesis(source_range where) {
     this->errors.emplace_back(error_unmatched_parenthesis{where});
   }
@@ -176,11 +191,12 @@ struct visitor {
   struct error_let_with_no_bindings : error {};
   struct error_missing_oprand_for_operator : error {};
   struct error_stray_comma_in_let_statement : error {};
+  struct error_unexpected_identifier : error {};
   struct error_unmatched_parenthesis : error {};
   using visited_error = std::variant<
       error_invalid_binding_in_let_statement, error_let_with_no_bindings,
       error_missing_oprand_for_operator, error_stray_comma_in_let_statement,
-      error_unmatched_parenthesis>;
+      error_unexpected_identifier, error_unmatched_parenthesis>;
   std::vector<visited_error> errors;
 };
 
@@ -423,6 +439,60 @@ TEST_CASE("parse invalid math expression") {
     REQUIRE(error);
     CHECK(error->where.begin_offset() == 9);
     CHECK(error->where.end_offset() == 10);
+  }
+
+  {
+    visitor v;
+    parser p("ten ten");
+    p.parse_expression(v);
+    REQUIRE(v.errors.size() == 1);
+    auto *error =
+        std::get_if<visitor::error_unexpected_identifier>(&v.errors[0]);
+    REQUIRE(error);
+    CHECK(error->where.begin_offset() == 4);
+    CHECK(error->where.end_offset() == 7);
+  }
+}
+
+TEST_CASE("parse function calls") {
+  {
+    visitor v;
+    parser p("f(x)");
+    p.parse_expression(v);
+    REQUIRE(v.variable_uses.size() == 2);
+    CHECK(v.variable_uses[0].name == "f");
+    CHECK(v.variable_uses[1].name == "x");
+    CHECK(v.errors.empty());
+  }
+
+  {
+    visitor v;
+    parser p("f(x, y)");
+    p.parse_expression(v);
+    REQUIRE(v.variable_uses.size() == 3);
+    CHECK(v.variable_uses[0].name == "f");
+    CHECK(v.variable_uses[1].name == "x");
+    CHECK(v.variable_uses[2].name == "y");
+    CHECK(v.errors.empty());
+  }
+}
+
+TEST_CASE("parse invalid function calls") {
+  {
+    visitor v;
+    parser p("(x)f");
+    p.parse_expression(v);
+
+    REQUIRE(v.errors.size() == 1);
+    auto *error =
+        std::get_if<visitor::error_unexpected_identifier>(&v.errors[0]);
+    REQUIRE(error);
+    CHECK(error->where.begin_offset() == 3);
+    CHECK(error->where.end_offset() == 4);
+
+    REQUIRE(v.variable_uses.size() == 2);
+    CHECK(v.variable_uses[0].name == "x");
+    CHECK(v.variable_uses[1].name == "f");
   }
 }
 }  // namespace
