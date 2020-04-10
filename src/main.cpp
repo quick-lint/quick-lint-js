f7d266fbced7d01e9d45688cb815d0525f13cfb9
Matthew Glazar <strager.nds@gmail.com>
2020-04-10 02:06:20 -0700

    License quicklint-js under GPL-3.0-or-later

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+// quicklint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew Glazar
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <cerrno>
 #include <cstdio>
 #include <cstring>
