3871f1fdc96fe50cb109a35036597dc398a69edf
Matthew Glazar <strager.nds@gmail.com>
2020-07-27 19:20:09 -0700

    Move spy visitor into its own module
    I want to use test-parse.cpp's visitor class in another test file. Move
    visitor into its own module and rename it to spy_visitor.
    
    This commit should not change behavior.

---
+++ test/include/quick-lint-js/spy-visitor.h
@@ -0,0 +1,120 @@
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew Glazar
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef QUICK_LINT_JS_SPY_VISITOR_H
+#define QUICK_LINT_JS_SPY_VISITOR_H
+
+#include <quick-lint-js/error-collector.h>
+#include <quick-lint-js/language.h>
+#include <quick-lint-js/lex.h>
+#include <string_view>
+#include <vector>
+
+namespace quick_lint_js {
+struct spy_visitor : public error_collector {
+  std::vector<const char *> visits;
+
+  void visit_end_of_module() {
+    this->visits.emplace_back("visit_end_of_module");
+  }
+
+  void visit_enter_block_scope() {
+    this->visits.emplace_back("visit_enter_block_scope");
+  }
+
+  void visit_enter_class_scope() {
+    this->visits.emplace_back("visit_enter_class_scope");
+  }
+
+  void visit_enter_function_scope() {
+    this->visits.emplace_back("visit_enter_function_scope");
+  }
+
+  void visit_exit_block_scope() {
+    this->visits.emplace_back("visit_exit_block_scope");
+  }
+
+  void visit_exit_class_scope() {
+    this->visits.emplace_back("visit_exit_class_scope");
+  }
+
+  void visit_exit_function_scope() {
+    this->visits.emplace_back("visit_exit_function_scope");
+  }
+
+  void visit_property_declaration(identifier name) {
+    this->property_declarations.emplace_back(
+        visited_property_declaration{std::string(name.string_view())});
+    this->visits.emplace_back("visit_property_declaration");
+  }
+
+  struct visited_property_declaration {
+    std::string name;
+  };
+  std::vector<visited_property_declaration> property_declarations;
+
+  void visit_variable_assignment(identifier name) {
+    this->variable_assignments.emplace_back(
+        visited_variable_assignment{std::string(name.string_view())});
+    this->visits.emplace_back("visit_variable_assignment");
+  }
+
+  struct visited_variable_assignment {
+    std::string name;
+  };
+  std::vector<visited_variable_assignment> variable_assignments;
+
+  void visit_variable_declaration(identifier name, variable_kind kind) {
+    this->variable_declarations.emplace_back(
+        visited_variable_declaration{std::string(name.string_view()), kind});
+    this->visits.emplace_back("visit_variable_declaration");
+  }
+
+  struct visited_variable_declaration {
+    std::string name;
+    variable_kind kind;
+
+    bool operator==(const visited_variable_declaration &other) const {
+      return std::tie(name, kind) == std::tie(other.name, other.kind);
+    }
+
+    bool operator!=(const visited_variable_declaration &other) const {
+      return !(other == *this);
+    }
+  };
+  std::vector<visited_variable_declaration> variable_declarations;
+
+  void visit_variable_use(identifier name) {
+    this->variable_uses.emplace_back(
+        visited_variable_use{std::string(name.string_view())});
+    this->visits.emplace_back("visit_variable_use");
+  }
+
+  struct visited_variable_use {
+    std::string name;
+
+    bool operator==(const visited_variable_use &other) const {
+      return name == other.name;
+    }
+
+    bool operator!=(const visited_variable_use &other) const {
+      return !(other == *this);
+    }
+  };
+  std::vector<visited_variable_use> variable_uses;
+};
+}  // namespace quick_lint_js
+#endif
--- test/test-parse.cpp
+++ test/test-parse.cpp
@@ -21,6 +21,7 @@
 #include <quick-lint-js/language.h>
 #include <quick-lint-js/location.h>
 #include <quick-lint-js/parse.h>
+#include <quick-lint-js/spy-visitor.h>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -30,98 +31,7 @@ using ::testing::IsEmpty;
 
 namespace quick_lint_js {
 namespace {
-struct visitor : public error_collector {
-  std::vector<const char *> visits;
-
-  void visit_end_of_module() {
-    this->visits.emplace_back("visit_end_of_module");
-  }
-
-  void visit_enter_block_scope() {
-    this->visits.emplace_back("visit_enter_block_scope");
-  }
-
-  void visit_enter_class_scope() {
-    this->visits.emplace_back("visit_enter_class_scope");
-  }
-
-  void visit_enter_function_scope() {
-    this->visits.emplace_back("visit_enter_function_scope");
-  }
-
-  void visit_exit_block_scope() {
-    this->visits.emplace_back("visit_exit_block_scope");
-  }
-
-  void visit_exit_class_scope() {
-    this->visits.emplace_back("visit_exit_class_scope");
-  }
-
-  void visit_exit_function_scope() {
-    this->visits.emplace_back("visit_exit_function_scope");
-  }
-
-  void visit_property_declaration(identifier name) {
-    this->property_declarations.emplace_back(
-        visited_property_declaration{std::string(name.string_view())});
-    this->visits.emplace_back("visit_property_declaration");
-  }
-
-  struct visited_property_declaration {
-    std::string name;
-  };
-  std::vector<visited_property_declaration> property_declarations;
-
-  void visit_variable_assignment(identifier name) {
-    this->variable_assignments.emplace_back(
-        visited_variable_assignment{std::string(name.string_view())});
-    this->visits.emplace_back("visit_variable_assignment");
-  }
-
-  struct visited_variable_assignment {
-    std::string name;
-  };
-  std::vector<visited_variable_assignment> variable_assignments;
-
-  void visit_variable_declaration(identifier name, variable_kind kind) {
-    this->variable_declarations.emplace_back(
-        visited_variable_declaration{std::string(name.string_view()), kind});
-    this->visits.emplace_back("visit_variable_declaration");
-  }
-
-  struct visited_variable_declaration {
-    std::string name;
-    variable_kind kind;
-
-    bool operator==(const visited_variable_declaration &other) const {
-      return std::tie(name, kind) == std::tie(other.name, other.kind);
-    }
-
-    bool operator!=(const visited_variable_declaration &other) const {
-      return !(other == *this);
-    }
-  };
-  std::vector<visited_variable_declaration> variable_declarations;
-
-  void visit_variable_use(identifier name) {
-    this->variable_uses.emplace_back(
-        visited_variable_use{std::string(name.string_view())});
-    this->visits.emplace_back("visit_variable_use");
-  }
-
-  struct visited_variable_use {
-    std::string name;
-
-    bool operator==(const visited_variable_use &other) const {
-      return name == other.name;
-    }
-
-    bool operator!=(const visited_variable_use &other) const {
-      return !(other == *this);
-    }
-  };
-  std::vector<visited_variable_use> variable_uses;
-};
+using visitor = spy_visitor;
 
 TEST(test_parse, parse_simple_let) {
   {
