c71b16d794c45a1fb133285a409a6bcbc16d6f06
Matthew Glazar <strager.nds@gmail.com>
2020-07-28 18:51:16 -0700

    Speed up buffering_visitor compilation
    In GCC and Clang, std::variant is very slow to compile. Manually
    implement a specialized std::variant for buffering_visitor to improve
    compile times.
    
    This commit should not change behavior.

--- src/include/quick-lint-js/buffering-visitor.h
+++ src/include/quick-lint-js/buffering-visitor.h
@@ -20,7 +20,6 @@
 #include <quick-lint-js/language.h>
 #include <quick-lint-js/lex.h>
 #include <utility>
-#include <variant>
 #include <vector>
 
 namespace quick_lint_js {
@@ -28,150 +27,139 @@ class buffering_visitor {
  public:
   template <class Visitor>
   void move_into(Visitor &target) {
-    struct variant_visitor {
-      explicit variant_visitor(Visitor &target) noexcept : target(target) {}
-
-      void operator()(visited_end_of_module &) {
-        this->target.visit_end_of_module();
-      }
-
-      void operator()(visited_enter_block_scope &) {
-        this->target.visit_enter_block_scope();
-      }
-
-      void operator()(visited_enter_class_scope &) {
-        this->target.visit_enter_class_scope();
-      }
-
-      void operator()(visited_enter_function_scope &) {
-        this->target.visit_enter_function_scope();
-      }
-
-      void operator()(visited_enter_named_function_scope &visit) {
-        this->target.visit_enter_named_function_scope(visit.name);
-      }
-
-      void operator()(visited_exit_block_scope &) {
-        this->target.visit_exit_block_scope();
-      }
-
-      void operator()(visited_exit_class_scope &) {
-        this->target.visit_exit_class_scope();
-      }
-
-      void operator()(visited_exit_function_scope &) {
-        this->target.visit_exit_function_scope();
-      }
-
-      void operator()(visited_property_declaration &visit) {
-        this->target.visit_property_declaration(visit.name);
-      }
-
-      void operator()(visited_variable_assignment &visit) {
-        this->target.visit_variable_assignment(visit.name);
-      }
-
-      void operator()(visited_variable_declaration &visit) {
-        this->target.visit_variable_declaration(visit.name, visit.kind);
-      }
-
-      void operator()(visited_variable_use &visit) {
-        this->target.visit_variable_use(visit.name);
-      }
-
-      Visitor &target;
-    };
-
     for (auto &visit : this->visits_) {
-      std::visit(variant_visitor(target), visit);
+      switch (visit.kind) {
+        case visit_kind::end_of_module:
+          target.visit_end_of_module();
+          break;
+        case visit_kind::enter_block_scope:
+          target.visit_enter_block_scope();
+          break;
+        case visit_kind::enter_class_scope:
+          target.visit_enter_class_scope();
+          break;
+        case visit_kind::enter_function_scope:
+          target.visit_enter_function_scope();
+          break;
+        case visit_kind::enter_named_function_scope:
+          target.visit_enter_named_function_scope(visit.name);
+          break;
+        case visit_kind::exit_block_scope:
+          target.visit_exit_block_scope();
+          break;
+        case visit_kind::exit_class_scope:
+          target.visit_exit_class_scope();
+          break;
+        case visit_kind::exit_function_scope:
+          target.visit_exit_function_scope();
+          break;
+        case visit_kind::property_declaration:
+          target.visit_property_declaration(visit.name);
+          break;
+        case visit_kind::variable_assignment:
+          target.visit_variable_assignment(visit.name);
+          break;
+        case visit_kind::variable_use:
+          target.visit_variable_use(visit.name);
+          break;
+        case visit_kind::variable_declaration:
+          target.visit_variable_declaration(visit.name, visit.var_kind);
+          break;
+      }
     }
   }
 
   void visit_end_of_module() {
-    this->visits_.emplace_back(visited_end_of_module{});
+    this->visits_.emplace_back(visit_kind::end_of_module);
   }
 
   void visit_enter_block_scope() {
-    this->visits_.emplace_back(visited_enter_block_scope{});
+    this->visits_.emplace_back(visit_kind::enter_block_scope);
   }
 
   void visit_enter_class_scope() {
-    this->visits_.emplace_back(visited_enter_class_scope{});
+    this->visits_.emplace_back(visit_kind::enter_class_scope);
   }
 
   void visit_enter_function_scope() {
-    this->visits_.emplace_back(visited_enter_function_scope{});
+    this->visits_.emplace_back(visit_kind::enter_function_scope);
   }
 
   void visit_enter_named_function_scope(identifier name) {
-    this->visits_.emplace_back(visited_enter_named_function_scope{name});
+    this->visits_.emplace_back(visit_kind::enter_named_function_scope, name);
   }
 
   void visit_exit_block_scope() {
-    this->visits_.emplace_back(visited_exit_block_scope{});
+    this->visits_.emplace_back(visit_kind::exit_block_scope);
   }
 
   void visit_exit_class_scope() {
-    this->visits_.emplace_back(visited_exit_class_scope{});
+    this->visits_.emplace_back(visit_kind::exit_class_scope);
   }
 
   void visit_exit_function_scope() {
-    this->visits_.emplace_back(visited_exit_function_scope{});
+    this->visits_.emplace_back(visit_kind::exit_function_scope);
   }
 
   void visit_property_declaration(identifier name) {
-    this->visits_.emplace_back(visited_property_declaration{name});
+    this->visits_.emplace_back(visit_kind::property_declaration, name);
   }
 
   void visit_variable_assignment(identifier name) {
-    this->visits_.emplace_back(visited_variable_assignment{name});
+    this->visits_.emplace_back(visit_kind::variable_assignment, name);
   }
 
   void visit_variable_declaration(identifier name, variable_kind kind) {
-    this->visits_.emplace_back(visited_variable_declaration{name, kind});
+    this->visits_.emplace_back(visit_kind::variable_declaration, name, kind);
   }
 
   void visit_variable_use(identifier name) {
-    this->visits_.emplace_back(visited_variable_use{name});
+    this->visits_.emplace_back(visit_kind::variable_use, name);
   }
 
  private:
-  struct visited_end_of_module {};
-  struct visited_enter_block_scope {};
-  struct visited_enter_class_scope {};
-  struct visited_enter_function_scope {};
-  struct visited_enter_named_function_scope {
-    identifier name;
+  enum class visit_kind {
+    end_of_module,
+    enter_block_scope,
+    enter_class_scope,
+    enter_function_scope,
+    enter_named_function_scope,
+    exit_block_scope,
+    exit_class_scope,
+    exit_function_scope,
+    property_declaration,
+    variable_assignment,
+    variable_use,
+    variable_declaration,
   };
-  struct visited_exit_block_scope {};
-  struct visited_exit_class_scope {};
-  struct visited_exit_function_scope {};
-  struct visited_property_declaration {
-    identifier name;
+
+  struct visit {
+    explicit visit(visit_kind kind) noexcept : kind(kind) {}
+
+    explicit visit(visit_kind kind, identifier name) noexcept
+        : kind(kind), name(name) {}
+
+    explicit visit(visit_kind kind, identifier name,
+                   variable_kind var_kind) noexcept
+        : kind(kind), name(name), var_kind(var_kind) {}
+
+    visit_kind kind;
+
+    union {
+      // enter_named_function_scope, property_declaration, variable_assignment,
+      // variable_declaration, variable_use
+      identifier name;
+      static_assert(std::is_trivially_destructible_v<identifier>);
+    };
+
+    union {
+      // variable_declaration
+      variable_kind var_kind;
+      static_assert(std::is_trivially_destructible_v<variable_kind>);
+    };
   };
-  struct visited_variable_assignment {
-    identifier name;
-  };
-  struct visited_variable_declaration {
-    identifier name;
-    variable_kind kind;
-  };
-  struct visited_variable_use {
-    identifier name;
-  };
-  std::vector<std::variant<visited_end_of_module,               //
-                           visited_enter_block_scope,           //
-                           visited_enter_class_scope,           //
-                           visited_enter_function_scope,        //
-                           visited_enter_named_function_scope,  //
-                           visited_exit_block_scope,            //
-                           visited_exit_class_scope,            //
-                           visited_exit_function_scope,         //
-                           visited_property_declaration,        //
-                           visited_variable_assignment,         //
-                           visited_variable_use,                //
-                           visited_variable_declaration>>
-      visits_;
+
+  std::vector<visit> visits_;
 };
 }  // namespace quick_lint_js
 
