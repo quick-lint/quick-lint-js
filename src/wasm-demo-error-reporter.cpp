c4bf670e42164a58b2577a5201f9e284d6bad717
Matthew Glazar <strager.nds@gmail.com>
2020-09-22 18:46:45 -0700

    WASM: add error reporter for WASM demo
    Create an error_reporter class which can be called easily from
    JavaScript code (assuming quick-lint-js is compiled to WebAssembly).
    
    A future commit will include the JavaScript code which uses this
    error_reporter.

---
+++ src/wasm-demo-error-reporter.cpp
@@ -0,0 +1,122 @@
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew Glazar
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <boost/container/pmr/monotonic_buffer_resource.hpp>
+#include <boost/container/pmr/polymorphic_allocator.hpp>
+#include <iostream>
+#include <memory>
+#include <quick-lint-js/error.h>
+#include <quick-lint-js/lex.h>
+#include <quick-lint-js/location.h>
+#include <quick-lint-js/optional.h>
+#include <quick-lint-js/padded-string.h>
+#include <quick-lint-js/unreachable.h>
+#include <quick-lint-js/wasm-demo-error-reporter.h>
+#include <string>
+
+namespace quick_lint_js {
+wasm_demo_error_reporter::wasm_demo_error_reporter(padded_string_view input)
+    : input_(input.c_str()) {}
+
+#define QLJS_ERROR_TYPE(name, struct_body, format_call) \
+  void wasm_demo_error_reporter::report(name e) {       \
+    format_error(e, this->format());                    \
+  }
+QLJS_X_ERROR_TYPES
+#undef QLJS_ERROR_TYPE
+
+const wasm_demo_error_reporter::error *
+wasm_demo_error_reporter::get_errors() noexcept {
+  // Null-terminate the returned errors.
+  this->errors_.emplace_back();
+
+  return this->errors_.data();
+}
+
+void wasm_demo_error_reporter::report_fatal_error_unimplemented_character(
+    const char *qljs_file_name, int qljs_line, const char *qljs_function_name,
+    const char8 *character) {
+  error_reporter::write_fatal_error_unimplemented_character(
+      /*qljs_file_name=*/qljs_file_name,
+      /*qljs_line=*/qljs_line,
+      /*qljs_function_name=*/qljs_function_name,
+      /*character=*/character,
+      /*locator=*/nullptr,
+      /*out=*/std::cerr);
+}
+
+void wasm_demo_error_reporter::report_fatal_error_unimplemented_token(
+    const char *qljs_file_name, int qljs_line, const char *qljs_function_name,
+    token_type type, const char8 *token_begin) {
+  error_reporter::write_fatal_error_unimplemented_token(
+      /*qljs_file_name=*/qljs_file_name,
+      /*qljs_line=*/qljs_line,
+      /*qljs_function_name=*/qljs_function_name,
+      /*type=*/type,
+      /*token_begin=*/token_begin,
+      /*locator=*/nullptr,
+      /*out=*/std::cerr);
+}
+
+wasm_demo_error_formatter wasm_demo_error_reporter::format() {
+  return wasm_demo_error_formatter(this);
+}
+
+char8 *wasm_demo_error_reporter::allocate_c_string(string8_view string) {
+  boost::container::pmr::polymorphic_allocator<char8> allocator(
+      &this->string_memory_);
+  char8 *result = allocator.allocate(string.size() + 1);
+  std::uninitialized_copy(string.begin(), string.end(), result);
+  result[string.size()] = u8'\0';
+  return result;
+}
+
+wasm_demo_error_formatter::wasm_demo_error_formatter(
+    wasm_demo_error_reporter *reporter)
+    : reporter_(reporter) {}
+
+void wasm_demo_error_formatter::write_before_message(severity sev,
+                                                     const source_code_span &) {
+  if (sev == severity::note) {
+    // Don't write notes. Only write the main message.
+    return;
+  }
+  QLJS_ASSERT(this->current_message_.empty());
+}
+
+void wasm_demo_error_formatter::write_message_part(severity sev,
+                                                   string8_view message) {
+  if (sev == severity::note) {
+    // Don't write notes. Only write the main message.
+    return;
+  }
+  this->current_message_.append(message);
+}
+
+void wasm_demo_error_formatter::write_after_message(
+    severity sev, const source_code_span &origin) {
+  if (sev == severity::note) {
+    // Don't write notes. Only write the main message.
+    return;
+  }
+  wasm_demo_error_reporter::error &e = this->reporter_->errors_.emplace_back();
+  e.begin_offset =
+      narrow_cast<std::uint32_t>(origin.begin() - this->reporter_->input_);
+  e.end_offset =
+      narrow_cast<std::uint32_t>(origin.end() - this->reporter_->input_);
+  e.message = this->reporter_->allocate_c_string(this->current_message_);
+}
+}
---
+++ test/test-wasm-demo-error-reporter.cpp
@@ -0,0 +1,71 @@
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew Glazar
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+#include <quick-lint-js/char8.h>
+#include <quick-lint-js/padded-string.h>
+#include <quick-lint-js/wasm-demo-error-reporter.h>
+
+namespace quick_lint_js {
+namespace {
+TEST(test_wasm_demo_error_reporter, big_int_literal_contains_decimal_point) {
+  padded_string input(u8"12.34n");
+  source_code_span number_span(&input[1 - 1], &input[6 + 1 - 1]);
+  ASSERT_EQ(number_span.string_view(), u8"12.34n");
+
+  wasm_demo_error_reporter reporter(&input);
+  reporter.report(error_big_int_literal_contains_decimal_point{number_span});
+
+  const wasm_demo_error_reporter::error *errors = reporter.get_errors();
+  EXPECT_EQ(errors[0].begin_offset, 0);
+  EXPECT_EQ(errors[0].end_offset, 6);
+  EXPECT_EQ(errors[0].message,
+            string8_view(u8"BigInt literal contains decimal point"));
+  EXPECT_EQ(errors[1].message, nullptr);
+}
+
+TEST(test_wasm_demo_error_reporter, multiple_errors) {
+  padded_string input(u8"abc");
+  source_code_span a_span(&input[0], &input[1]);
+  source_code_span b_span(&input[1], &input[2]);
+  source_code_span c_span(&input[2], &input[3]);
+
+  wasm_demo_error_reporter reporter(&input);
+  reporter.report(
+      error_assignment_to_const_global_variable{identifier(a_span)});
+  reporter.report(
+      error_assignment_to_const_global_variable{identifier(b_span)});
+  reporter.report(
+      error_assignment_to_const_global_variable{identifier(c_span)});
+
+  const wasm_demo_error_reporter::error *errors = reporter.get_errors();
+
+  EXPECT_EQ(errors[0].begin_offset, 0);
+  EXPECT_EQ(errors[0].end_offset, 1);
+  EXPECT_NE(errors[0].message, nullptr);
+
+  EXPECT_EQ(errors[1].begin_offset, 1);
+  EXPECT_EQ(errors[1].end_offset, 2);
+  EXPECT_NE(errors[1].message, nullptr);
+
+  EXPECT_EQ(errors[2].begin_offset, 2);
+  EXPECT_EQ(errors[2].end_offset, 3);
+  EXPECT_NE(errors[2].message, nullptr);
+
+  EXPECT_EQ(errors[3].message, nullptr);
+}
+}
+}
