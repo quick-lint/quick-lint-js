16fc99503d2d4bca4f1ebab1cfd2c992d15f2036
Matthew Glazar <strager.nds@gmail.com>
2020-09-24 16:55:36 -0700

    Fix use of uninitialized variable
    lexer::parse_current_token reads from this->last_token_.end.
    this->last_token_.end is not initialized in lexer's constructor, so the
    read is UB. Initialize this->last_token_.end to fix this.
    
    Also, while we're here, remove the dead stores to
    this->last_token_.begin and this->last_last_token_end_.
    lexer::parse_current_token assigns to those variables already (before
    reading).
    
    This commit should not change behavior.

--- src/lex.cpp
+++ src/lex.cpp
@@ -124,8 +124,7 @@ lexer::lexer(padded_string_view input, error_reporter* error_reporter) noexcept
     : input_(input.c_str()),
       error_reporter_(error_reporter),
       original_input_(this->input_) {
-  this->last_token_.begin = nullptr;
-  this->last_last_token_end_ = nullptr;
+  this->last_token_.end = nullptr;
   this->parse_current_token();
 }
 
