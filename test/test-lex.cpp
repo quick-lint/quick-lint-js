2e70aa738189cfd468c3688550bd156825ffd7c4
Matthew Glazar <strager.nds@gmail.com>
2020-09-28 17:09:06 -0700

    Factor duplication in character tables in lexer tests
    This commit should not change behavior.

--- test/test-lex.cpp
+++ test/test-lex.cpp
@@ -20,6 +20,7 @@
 #include <gtest/gtest.h>
 #include <initializer_list>
 #include <iostream>
+#include <quick-lint-js/assert.h>
 #include <quick-lint-js/char8.h>
 #include <quick-lint-js/error-collector.h>
 #include <quick-lint-js/error-matcher.h>
@@ -28,6 +29,7 @@
 #include <quick-lint-js/narrow-cast.h>
 #include <quick-lint-js/padded-string.h>
 #include <string_view>
+#include <type_traits>
 
 using namespace std::literals::string_view_literals;
 
@@ -54,84 +56,72 @@ constexpr string8_view operator""_sv(const char8* string,
   return string8_view(string, length);
 }
 
-std::array<string8_view, 5> line_terminators = {
-    u8"\n"_sv,      //
-    u8"\r"_sv,      //
-    u8"\r\n"_sv,    //
-    u8"\u2028"_sv,  // 0xe2 0x80 0xa8 Line Separator
-    u8"\u2029"_sv,  // 0xe2 0x80 0xa9 Paragraph Separator
-};
+template <class T, std::size_t LHSSize, std::size_t RHSSize>
+constexpr std::array<T, LHSSize + RHSSize> concat(
+    const std::array<T, LHSSize>& lhs, const std::array<T, RHSSize>& rhs) {
+  std::array<T, LHSSize + RHSSize> result;
+  auto it = result.begin();
+  for (const auto& value : lhs) {
+    *it++ = value;
+  }
+  for (const auto& value : rhs) {
+    *it++ = value;
+  }
+  QLJS_ASSERT(it == result.end());
+  return result;
+}
 
-std::array<string8_view, 3> line_terminators_except_ls_ps = {
-    u8"\n"_sv,
-    u8"\r"_sv,
-    u8"\r\n"_sv,
-};
+template <class... Args>
+constexpr auto make_array(Args&&... items) {
+  using item_type = std::common_type_t<Args...>;
+  return std::array<item_type, sizeof...(items)>{std::forward<Args>(items)...};
+}
 
-string8_view control_characters_except_line_terminators[] = {
-    u8"\u0000"_sv,  // NUL Null character
-    u8"\u0001"_sv,  // SOH Start of Heading
-    u8"\u0002"_sv,  // STX Start of Text
-    u8"\u0003"_sv,  // ETX End-of-text character
-    u8"\u0004"_sv,  // EOT End-of-transmission character
-    u8"\u0005"_sv,  // ENQ Enquiry character
-    u8"\u0006"_sv,  // ACK Acknowledge character
-    u8"\u0007"_sv,  // BEL Bell character
-    u8"\u0008"_sv,  // BS Backspace
-    u8"\u0009"_sv,  // HT Horizontal tab
-    u8"\u000b"_sv,  // VT Vertical tab
-    u8"\u000c"_sv,  // FF Form feed
-    u8"\u000e"_sv,  // SO Shift Out
-    u8"\u000f"_sv,  // SI Shift In
-    u8"\u0010"_sv,  // DLE Data Link Escape
-    u8"\u0011"_sv,  // DC1 Device Control 1
-    u8"\u0012"_sv,  // DC2 Device Control 2
-    u8"\u0013"_sv,  // DC3 Device Control 3
-    u8"\u0014"_sv,  // DC4 Device Control 4
-    u8"\u0015"_sv,  // NAK Negative-acknowledge character
-    u8"\u0016"_sv,  // SYN Synchronous Idle
-    u8"\u0017"_sv,  // ETB End of Transmission Block
-    u8"\u0018"_sv,  // CAN Cancel character
-    u8"\u0019"_sv,  // EM End of Medium
-    u8"\u001a"_sv,  // SUB Substitute character
-    u8"\u001b"_sv,  // ESC Escape character
-    u8"\u001c"_sv,  // FS File Separator
-    u8"\u001d"_sv,  // GS Group Separator
-    u8"\u001e"_sv,  // RS Record Separator
-    u8"\u001f"_sv,  // US Unit Separator
-    u8"\u007f"_sv,  // DEL Delete
-};
+constexpr std::array line_terminators_except_ls_ps =
+    make_array(u8"\n"_sv, u8"\r"_sv, u8"\r\n"_sv);
 
-string8_view control_characters_except_whitespace[] = {
-    u8"\u0000"_sv,  // NUL Null character
-    u8"\u0001"_sv,  // SOH Start of Heading
-    u8"\u0002"_sv,  // STX Start of Text
-    u8"\u0003"_sv,  // ETX End-of-text character
-    u8"\u0004"_sv,  // EOT End-of-transmission character
-    u8"\u0005"_sv,  // ENQ Enquiry character
-    u8"\u0006"_sv,  // ACK Acknowledge character
-    u8"\u0007"_sv,  // BEL Bell character
-    u8"\u0008"_sv,  // BS Backspace
-    u8"\u000e"_sv,  // SO Shift Out
-    u8"\u000f"_sv,  // SI Shift In
-    u8"\u0010"_sv,  // DLE Data Link Escape
-    u8"\u0011"_sv,  // DC1 Device Control 1
-    u8"\u0012"_sv,  // DC2 Device Control 2
-    u8"\u0013"_sv,  // DC3 Device Control 3
-    u8"\u0014"_sv,  // DC4 Device Control 4
-    u8"\u0015"_sv,  // NAK Negative-acknowledge character
-    u8"\u0016"_sv,  // SYN Synchronous Idle
-    u8"\u0017"_sv,  // ETB End of Transmission Block
-    u8"\u0018"_sv,  // CAN Cancel character
-    u8"\u0019"_sv,  // EM End of Medium
-    u8"\u001a"_sv,  // SUB Substitute character
-    u8"\u001b"_sv,  // ESC Escape character
-    u8"\u001c"_sv,  // FS File Separator
-    u8"\u001d"_sv,  // GS Group Separator
-    u8"\u001e"_sv,  // RS Record Separator
-    u8"\u001f"_sv,  // US Unit Separator
-    u8"\u007f"_sv,  // DEL Delete
-};
+constexpr std::array ls_and_ps =
+    make_array(u8"\u2028"_sv,   // 0xe2 0x80 0xa8 Line Separator
+               u8"\u2029"_sv);  // 0xe2 0x80 0xa9 Paragraph Separator
+
+constexpr std::array line_terminators =
+    concat(line_terminators_except_ls_ps, ls_and_ps);
+
+constexpr std::array control_characters_except_whitespace =
+    make_array(u8"\u0000"_sv,   // NUL Null character
+               u8"\u0001"_sv,   // SOH Start of Heading
+               u8"\u0002"_sv,   // STX Start of Text
+               u8"\u0003"_sv,   // ETX End-of-text character
+               u8"\u0004"_sv,   // EOT End-of-transmission character
+               u8"\u0005"_sv,   // ENQ Enquiry character
+               u8"\u0006"_sv,   // ACK Acknowledge character
+               u8"\u0007"_sv,   // BEL Bell character
+               u8"\u0008"_sv,   // BS Backspace
+               u8"\u000e"_sv,   // SO Shift Out
+               u8"\u000f"_sv,   // SI Shift In
+               u8"\u0010"_sv,   // DLE Data Link Escape
+               u8"\u0011"_sv,   // DC1 Device Control 1
+               u8"\u0012"_sv,   // DC2 Device Control 2
+               u8"\u0013"_sv,   // DC3 Device Control 3
+               u8"\u0014"_sv,   // DC4 Device Control 4
+               u8"\u0015"_sv,   // NAK Negative-acknowledge character
+               u8"\u0016"_sv,   // SYN Synchronous Idle
+               u8"\u0017"_sv,   // ETB End of Transmission Block
+               u8"\u0018"_sv,   // CAN Cancel character
+               u8"\u0019"_sv,   // EM End of Medium
+               u8"\u001a"_sv,   // SUB Substitute character
+               u8"\u001b"_sv,   // ESC Escape character
+               u8"\u001c"_sv,   // FS File Separator
+               u8"\u001d"_sv,   // GS Group Separator
+               u8"\u001e"_sv,   // RS Record Separator
+               u8"\u001f"_sv,   // US Unit Separator
+               u8"\u007f"_sv);  // DEL Delete
+
+constexpr std::array control_characters_except_line_terminators =
+    concat(control_characters_except_whitespace,
+           make_array(u8"\u0009"_sv,    // HT Horizontal tab
+                      u8"\u000b"_sv,    // VT Vertical tab
+                      u8"\u000c"_sv));  // FF Form feed
 
 TEST(test_lex, lex_block_comments) {
   check_single_token(u8"/* */ hello", u8"hello");
