7467687f1cd7b7dcb379bf18866049c2c083fed2
Matthew "strager" Glazar <strager.nds@gmail.com>
2020-12-22 17:42:57 -0800

    Add more lsp_locator benchmarks

--- benchmark/benchmark-lsp-location.cpp
+++ benchmark/benchmark-lsp-location.cpp
@@ -37,6 +37,19 @@ void benchmark_location_scale_of_long_line(::benchmark::State &state) {
 }
 BENCHMARK(benchmark_location_scale_of_long_line);
 
+void benchmark_from_position_scale_of_long_line(::benchmark::State &state) {
+  int line_length = 10'000;
+  padded_string line(string8(narrow_cast<std::size_t>(line_length), u8'x'));
+  for (auto _ : state) {
+    lsp_locator l(&line);
+    for (int i = 0; i < line_length; ++i) {
+      const char8 *c = l.from_position(lsp_position{.line = 0, .character = i});
+      ::benchmark::DoNotOptimize(c);
+    }
+  }
+}
+BENCHMARK(benchmark_from_position_scale_of_long_line);
+
 void benchmark_location_scale_of_empty_lines(::benchmark::State &state) {
   int line_length = 10'000;
   padded_string line(string8(narrow_cast<std::size_t>(line_length), u8'\n'));
@@ -49,6 +62,19 @@ void benchmark_location_scale_of_empty_lines(::benchmark::State &state) {
   }
 }
 BENCHMARK(benchmark_location_scale_of_empty_lines);
+
+void benchmark_from_position_scale_of_empty_lines(::benchmark::State &state) {
+  int line_count = 10'000;
+  padded_string lines(string8(narrow_cast<std::size_t>(line_count), u8'\n'));
+  for (auto _ : state) {
+    lsp_locator l(&lines);
+    for (int i = 0; i < line_count; ++i) {
+      const char8 *c = l.from_position({.line = i, .character = 0});
+      ::benchmark::DoNotOptimize(c);
+    }
+  }
+}
+BENCHMARK(benchmark_from_position_scale_of_empty_lines);
 
 void benchmark_range_scale_of_empty_lines(::benchmark::State &state) {
   int line_length = 10'000;
@@ -80,5 +106,31 @@ void benchmark_location_realisticish(::benchmark::State &state) {
   }
 }
 BENCHMARK(benchmark_location_realisticish)->Arg(1)->Arg(50);
+
+void benchmark_from_position_realisticish(::benchmark::State &state) {
+  int line_count = 10'000;
+  int span_count = narrow_cast<int>(state.range(0));
+  source_code_with_spans code = make_realisticish_code(
+      /*line_count=*/line_count, /*span_count=*/span_count);
+
+  std::vector<lsp_position> positions;
+  {
+    lsp_locator l(code.source.get());
+    for (const source_code_span &span : code.spans) {
+      lsp_range r = l.range(span);
+      positions.push_back(r.start);
+      positions.push_back(r.end);
+    }
+  }
+
+  for (auto _ : state) {
+    lsp_locator l(code.source.get());
+    for (const lsp_position &position : positions) {
+      const char8 *c = l.from_position(position);
+      ::benchmark::DoNotOptimize(c);
+    }
+  }
+}
+BENCHMARK(benchmark_from_position_realisticish)->Arg(1)->Arg(50);
 }  // namespace
 }  // namespace quick_lint_js
