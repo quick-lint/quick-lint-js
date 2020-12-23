524f6209b7b96c38bc5dfebe85a4ec36461303d1
Matthew "strager" Glazar <strager.nds@gmail.com>
2020-12-22 17:27:32 -0800

    Move make_realisticish_code into helper module
    I want to use make_realisticish_code in more benchmarks. Move it into
    the generate-code module so it can be called from any benchmark.
    
    This commit should not change behavior.

--- benchmark/benchmark-cli-location.cpp
+++ benchmark/benchmark-cli-location.cpp
@@ -15,38 +15,18 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <benchmark/benchmark.h>
-#include <memory>
 #include <quick-lint-js/char8.h>
 #include <quick-lint-js/cli-location.h>
 #include <quick-lint-js/generate-code.h>
 #include <quick-lint-js/location.h>
 #include <quick-lint-js/narrow-cast.h>
 #include <quick-lint-js/padded-string.h>
-#include <random>
-#include <set>
-#include <vector>
 
 // TODO(strager): Also benchmark lsp_locator.
 // TODO(strager): Also benchmark vim_locator.
 
 namespace quick_lint_js {
 namespace {
-struct source_code_with_spans {
-  explicit source_code_with_spans(std::unique_ptr<padded_string> &&source,
-                                  std::vector<source_code_span> &&spans)
-      : source(std::move(source)), spans(std::move(spans)) {}
-
-  source_code_with_spans(const source_code_with_spans &) = delete;
-  source_code_with_spans &operator=(const source_code_with_spans &) = delete;
-
-  std::unique_ptr<padded_string> source;
-  std::vector<source_code_span> spans;
-};
-
-source_code_with_spans make_realisticish_code(int line_count, int span_count);
-template <class T>
-void partial_shuffle(std::vector<T> &, std::mt19937_64 &, int rounds);
-
 void benchmark_location_scale_of_long_line(::benchmark::State &state) {
   int line_length = 10'000;
   padded_string line(string8(narrow_cast<std::size_t>(line_length), u8'x'));
@@ -103,65 +83,5 @@ void benchmark_location_realisticish(::benchmark::State &state) {
   }
 }
 BENCHMARK(benchmark_location_realisticish)->Arg(1)->Arg(50);
-
-source_code_with_spans make_realisticish_code(int line_count, int span_count) {
-  std::mt19937_64 rng;
-
-  string8 newline = u8"\n";
-  std::vector<int> line_lengths = random_line_lengths(rng, line_count);
-  std::unique_ptr<padded_string> source =
-      std::make_unique<padded_string>(make_source_code(line_lengths, newline));
-
-  std::uniform_int_distribution span_length_distribution(1, 10);
-  auto random_span_in_line = [&](int line_length,
-                                 int line_begin_offset) -> source_code_span {
-    int span_length = std::min(span_length_distribution(rng), line_length);
-    int span_begin_line_offset =
-        std::uniform_int_distribution(0, line_length - span_length)(rng);
-    const char8 *span_begin =
-        &(*source)[line_begin_offset + span_begin_line_offset];
-    return source_code_span(span_begin, span_begin + span_length);
-  };
-
-  std::uniform_int_distribution span_line_number_distribution(1,
-                                                              line_count + 1);
-  std::multiset<int> span_line_numbers;
-  for (int i = 0; i < span_count; ++i) {
-    span_line_numbers.insert(span_line_number_distribution(rng));
-  }
-
-  std::vector<source_code_span> spans;
-  int line_begin_offset = 0;
-  for (int line_index = 0; line_index < narrow_cast<int>(line_lengths.size());
-       ++line_index) {
-    int line_length = line_lengths[narrow_cast<std::size_t>(line_index)];
-    int line_span_count =
-        narrow_cast<int>(span_line_numbers.count(line_index + 1));
-    for (int i = 0; i < line_span_count; ++i) {
-      spans.push_back(
-          random_span_in_line(/*line_length=*/line_length,
-                              /*line_begin_offset=*/line_begin_offset));
-    }
-    line_begin_offset += line_length + narrow_cast<int>(newline.size());
-  }
-
-  std::sort(spans.begin(), spans.end(),
-            [](const source_code_span &a, const source_code_span &b) {
-              return a.begin() < b.begin();
-            });
-  partial_shuffle(spans, rng, /*rounds=*/5);
-
-  return source_code_with_spans(std::move(source), std::move(spans));
-}
-
-template <class T>
-void partial_shuffle(std::vector<T> &items, std::mt19937_64 &rng, int rounds) {
-  std::uniform_int_distribution<std::size_t> index_distribution(
-      0, items.size() - 1);
-  using std::swap;
-  for (int round = 0; round < rounds; ++round) {
-    swap(items[index_distribution(rng)], items[index_distribution(rng)]);
-  }
-}
 }  // namespace
 }  // namespace quick_lint_js
--- benchmark/quick-lint-js/generate-code.h
+++ benchmark/quick-lint-js/generate-code.h
@@ -17,7 +17,9 @@
 #ifndef QUICK_LINT_JS_GENERATE_CODE_H
 #define QUICK_LINT_JS_GENERATE_CODE_H
 
+#include <memory>
 #include <quick-lint-js/char8.h>
+#include <quick-lint-js/location.h>
 #include <quick-lint-js/padded-string.h>
 #include <random>
 #include <vector>
@@ -26,6 +28,20 @@ namespace quick_lint_js {
 std::vector<int> random_line_lengths(std::mt19937_64 &, int line_count);
 padded_string make_source_code(const std::vector<int> &line_lengths,
                                const string8 &newline);
+
+struct source_code_with_spans {
+  explicit source_code_with_spans(std::unique_ptr<padded_string> &&source,
+                                  std::vector<source_code_span> &&spans)
+      : source(std::move(source)), spans(std::move(spans)) {}
+
+  source_code_with_spans(const source_code_with_spans &) = delete;
+  source_code_with_spans &operator=(const source_code_with_spans &) = delete;
+
+  std::unique_ptr<padded_string> source;
+  std::vector<source_code_span> spans;
+};
+
+source_code_with_spans make_realisticish_code(int line_count, int span_count);
 }
 
 #endif
