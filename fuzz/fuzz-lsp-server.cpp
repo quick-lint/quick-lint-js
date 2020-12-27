5a3175da82829a58469a6203b9f0d415c8b8ec64
Matthew "strager" Glazar <strager.nds@gmail.com>
2020-12-27 04:09:44 -0800

    Fuzz-test LSP server
    Fuzz-test the full LSP stack, except I/O. This little test already
    exposes some crashes.

---
+++ fuzz/fuzz-lsp-server.cpp
@@ -0,0 +1,60 @@
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew Glazar
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <quick-lint-js/char8.h>
+#include <quick-lint-js/lsp-endpoint.h>
+#include <quick-lint-js/lsp-server.h>
+
+namespace quick_lint_js {
+namespace {
+class null_lsp_endpoint_remote {
+ public:
+  void send_message(const byte_buffer&) {}
+};
+}
+}
+
+extern "C" {
+int LLVMFuzzerTestOneInput(const std::uint8_t* data, std::size_t size) {
+  using namespace quick_lint_js;
+
+  lsp_endpoint<linting_lsp_server_handler<lsp_javascript_linter>,
+               null_lsp_endpoint_remote>
+      server;
+
+  std::size_t i = 0;
+  auto size_remaining = [&]() -> std::size_t { return size - i; };
+  for (;;) {
+    std::size_t chunk_size;
+    if (size_remaining() < sizeof(chunk_size)) {
+      break;
+    }
+    std::memcpy(&chunk_size, &data[i], sizeof(chunk_size));
+    i += sizeof(chunk_size);
+    chunk_size = std::min(chunk_size, size_remaining());
+
+    server.append(
+        string8_view(reinterpret_cast<const char8*>(&data[i]), chunk_size));
+    i += chunk_size;
+  }
+
+  return 0;
+}
+}
