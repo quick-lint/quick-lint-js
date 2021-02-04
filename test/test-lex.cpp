4aaaf56286345f752d429064678fe6a2b3101f8a
Angel Avila <angel.j.avila@gmail.com>
2021-02-03 19:13:37 -0800

    update test source_code span to reflect quotes, update todo

--- test/test-lex.cpp
+++ test/test-lex.cpp
@@ -649,15 +649,14 @@ TEST_F(test_lex, lex_strings) {
                                 offsets_matcher(input, 0, 14))));
       });
 
-  // TODO (angel): report error_invalid_hex_escape_sequence
-  // currently reports nothing
+  // TODO (angel): add more tests
   this->check_tokens_with_errors(
       u8"'\\x1'", {token_type::string},
       [](padded_string_view input, const auto& errors) {
         EXPECT_THAT(errors,
                     ElementsAre(ERROR_TYPE_FIELD(
                       error_invalid_hex_escape_sequence, escape_sequence,
-                      offsets_matcher(input, 0, 3))));
+                      offsets_matcher(input, 2, 4))));
       });
   // TODO(strager): Report invalid hex escape sequences. For example:
   //
