990754493c643e51a0c8fb906371c945d2bc79be
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-03-20 20:18:41 -0700

    Add some padded_string tests
    Ensure padded_string::resize and moving padded_string-s work as
    expected.

--- test/test-padded-string.cpp
+++ test/test-padded-string.cpp
@@ -82,6 +82,53 @@ TEST(test_padded_string, std_string_view_excludes_padding_bytes) {
   EXPECT_TRUE(s.string_view() == string8_view(u8"hello"));
 }
 
+TEST(test_padded_string, growing_preserves_data_and_adds_null_bytes) {
+  padded_string s(u8"helloworld"_sv);
+  s.resize(15);
+  EXPECT_EQ(s.size(), 15);
+  EXPECT_EQ(s.string_view(), u8"helloworld\0\0\0\0\0"_sv);
+  expect_null_terminated(s);
+}
+
+TEST(test_padded_string, shrinking_preserves_data) {
+  padded_string s(u8"helloworld"_sv);
+  s.resize(5);
+  EXPECT_EQ(s.size(), 5);
+  EXPECT_EQ(s.string_view(), u8"hello"_sv);
+  expect_null_terminated(s);
+}
+
+TEST(test_padded_string, move_constructing_preserves_data) {
+  padded_string s1(u8"helloworld"_sv);
+  padded_string s2(std::move(s1));
+  EXPECT_EQ(s2.string_view(), u8"helloworld"_sv)
+      << "moving should not change data";
+  expect_null_terminated(s2);
+}
+
+TEST(test_padded_string, move_constructing_empty_string_preserves_data) {
+  padded_string s1;
+  padded_string s2(std::move(s1));
+  EXPECT_EQ(s2.string_view(), u8""_sv) << "moving should not change data";
+  expect_null_terminated(s2);
+}
+
+TEST(test_padded_string, move_assigning_changes_data) {
+  padded_string s1(u8"helloworld"_sv);
+  padded_string s2(u8"other"_sv);
+  s2 = std::move(s1);
+  EXPECT_EQ(s2.string_view(), u8"helloworld"_sv) << "moving should change data";
+  expect_null_terminated(s2);
+}
+
+TEST(test_padded_string, move_assigning_empty_string_changes_data) {
+  padded_string s1;
+  padded_string s2(u8"other"_sv);
+  s2 = std::move(s1);
+  EXPECT_EQ(s2.string_view(), u8""_sv) << "moving should change data";
+  expect_null_terminated(s2);
+}
+
 namespace {
 void expect_null_terminated(const padded_string &s) {
   const char8 *data = s.c_str();
