937d0abba2418bd91ea271dd9457c1955f0e5a50
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-03-21 15:17:24 -0700

    Adjust license notice per FSF recommendations
    The FSF recommends a different wording for license notices than what
    we use now:
    
    > For programs that are more than one file, it is better to replace
    > "this program" with the name of the program, ...
    
    https://www.gnu.org/licenses/gpl-howto.html
    
    Change our wording to refer to "quick-lint-js" instead of "this program".

--- src/quick-lint-js/locale.h
+++ src/quick-lint-js/locale.h
@@ -37,15 +37,15 @@ std::vector<std::string> locale_name_combinations(const char* locale_name);
 // quick-lint-js finds bugs in JavaScript programs.
 // Copyright (C) 2020  Matthew Glazar
 //
-// This program is free software: you can redistribute it and/or modify
+// quick-lint-js is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// quick-lint-js is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
--- src/utf-8.cpp
+++ src/utf-8.cpp
@@ -208,15 +208,15 @@ std::ptrdiff_t count_lsp_characters_in_utf_8(padded_string_view utf_8,
 // quick-lint-js finds bugs in JavaScript programs.
 // Copyright (C) 2020  Matthew Glazar
 //
-// This program is free software: you can redistribute it and/or modify
+// quick-lint-js is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// quick-lint-js is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
