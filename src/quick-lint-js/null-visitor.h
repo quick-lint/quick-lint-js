7f11a76701aa095e86911c127170f3153ef5a484
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-03-22 02:53:29 -0700

    Reformat null_visitor

--- src/quick-lint-js/null-visitor.h
+++ src/quick-lint-js/null-visitor.h
@@ -12,39 +12,22 @@ namespace quick_lint_js {
 class null_visitor {
  public:
   void visit_end_of_module() {}
-
   void visit_enter_block_scope() {}
-
   void visit_enter_class_scope() {}
-
   void visit_enter_for_scope() {}
-
   void visit_enter_function_scope() {}
-
   void visit_enter_function_scope_body() {}
-
   void visit_enter_named_function_scope(identifier) {}
-
   void visit_exit_block_scope() {}
-
   void visit_exit_class_scope() {}
-
   void visit_exit_for_scope() {}
-
   void visit_exit_function_scope() {}
-
   void visit_property_declaration() {}
-
   void visit_property_declaration(identifier) {}
-
   void visit_variable_assignment(identifier) {}
-
   void visit_variable_declaration(identifier, variable_kind) {}
-
   void visit_variable_export_use(identifier) {}
-
   void visit_variable_typeof_use(identifier) {}
-
   void visit_variable_use(identifier) {}
 };
 QLJS_STATIC_ASSERT_IS_PARSE_VISITOR(null_visitor);
