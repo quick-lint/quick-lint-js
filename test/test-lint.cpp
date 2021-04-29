8d7ee583eb33b0db5ef9bedd0a8a04c4062cdb46
Himanshu <himansingh24@gmail.com>
2021-04-29 12:29:49 +0530

    Report warning for function declared in blocked scope and used before
    Now we report a warning [E077] for the following code -
    f();
    {
        function f() {}
    }

--- test/test-lint.cpp
+++ test/test-lint.cpp
@@ -523,9 +523,8 @@ TEST(
   }
 }
 
-TEST(test_lint,
-     var_or_function_variable_use_before_declaration_in_block_scope) {
-  for (variable_kind kind : {variable_kind::_function, variable_kind::_var}) {
+TEST(test_lint, var_variable_use_before_declaration_in_block_scope) {
+  for (variable_kind kind : {variable_kind::_var}) {
     const char8 declaration[] = u8"x";
     const char8 use[] = u8"x";
 
@@ -545,10 +544,40 @@ TEST(test_lint,
   }
 }
 
-TEST(
-    test_lint,
-    var_or_function_variable_use_before_declaration_in_block_scope_all_in_function) {
-  for (variable_kind kind : {variable_kind::_function, variable_kind::_var}) {
+TEST(test_lint, function_variable_use_before_declaration_in_block_scope) {
+  for (variable_kind kind : {variable_kind::_function}) {
+    const char8 declaration[] = u8"f";
+    const char8 use[] = u8"f";
+
+    // f();
+    // {
+    //   function f() {}
+    // }
+    error_collector v;
+    linter l(&v);
+    l.visit_variable_use(identifier_of(use));
+    l.visit_enter_function_scope();
+    l.visit_exit_function_scope();
+    l.visit_enter_block_scope();
+    l.visit_variable_declaration(identifier_of(declaration),
+                                 variable_kind::_function);
+    l.visit_enter_function_scope();
+    l.visit_enter_function_scope_body();
+    l.visit_exit_function_scope();
+    l.visit_exit_block_scope();
+    l.visit_end_of_module();
+
+    EXPECT_THAT(v.errors,
+                ElementsAre(ERROR_TYPE_2_FIELDS(
+                    error_function_call_before_declaration_in_blocked_scope,  //
+                    use, span_matcher(use),                                   //
+                    declaration, span_matcher(declaration))));
+  }
+}
+
+TEST(test_lint,
+     var_variable_use_before_declaration_in_block_scope_all_in_function) {
+  for (variable_kind kind : {variable_kind::_var}) {
     const char8 declaration[] = u8"x";
     const char8 use[] = u8"x";
 
@@ -570,6 +599,43 @@ TEST(
     l.visit_end_of_module();
 
     EXPECT_THAT(v.errors, IsEmpty());
+  }
+}
+
+TEST(test_lint,
+     function_variable_use_before_declaration_in_block_scope_all_in_function) {
+  for (variable_kind kind : {variable_kind::_function}) {
+    const char8 declaration[] = u8"f";
+    const char8 use[] = u8"f";
+
+    // (() => {
+    //   f();
+    //   {
+    //     function f() {}
+    //   }
+    // });
+    error_collector v;
+    linter l(&v);
+    l.visit_enter_function_scope();
+    l.visit_enter_function_scope_body();
+    l.visit_variable_use(identifier_of(use));
+    l.visit_enter_function_scope();
+    l.visit_exit_function_scope();
+    l.visit_enter_block_scope();
+    l.visit_variable_declaration(identifier_of(declaration),
+                                 variable_kind::_function);
+    l.visit_enter_function_scope();
+    l.visit_enter_function_scope_body();
+    l.visit_exit_function_scope();
+    l.visit_exit_block_scope();
+    l.visit_exit_function_scope();
+    l.visit_end_of_module();
+
+    EXPECT_THAT(v.errors,
+                ElementsAre(ERROR_TYPE_2_FIELDS(
+                    error_function_call_before_declaration_in_blocked_scope,  //
+                    use, span_matcher(use),                                   //
+                    declaration, span_matcher(declaration))));
   }
 }
 
