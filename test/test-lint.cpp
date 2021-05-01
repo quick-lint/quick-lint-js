553c57a32976133bc63a067788ec9faf8f78fbb9
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-05-01 00:31:02 -0700

    Inline variable from single-item loop in test
    This commit should not change test coverage.

--- test/test-lint.cpp
+++ test/test-lint.cpp
@@ -571,29 +571,27 @@ TEST(test_lint, function_variable_use_before_declaration_in_block_scope) {
 
 TEST(test_lint,
      var_variable_use_before_declaration_in_block_scope_all_in_function) {
-  for (variable_kind kind : {variable_kind::_var}) {
-    const char8 declaration[] = u8"x";
-    const char8 use[] = u8"x";
+  const char8 declaration[] = u8"x";
+  const char8 use[] = u8"x";
 
-    // (() => {
-    //   x;
-    //   {
-    //     var x;  // x is hoisted
-    //   }
-    // });
-    error_collector v;
-    linter l(&v);
-    l.visit_enter_function_scope();
-    l.visit_enter_function_scope_body();
-    l.visit_variable_use(identifier_of(use));
-    l.visit_enter_block_scope();
-    l.visit_variable_declaration(identifier_of(declaration), kind);
-    l.visit_exit_block_scope();
-    l.visit_exit_function_scope();
-    l.visit_end_of_module();
+  // (() => {
+  //   x;
+  //   {
+  //     var x;  // x is hoisted
+  //   }
+  // });
+  error_collector v;
+  linter l(&v);
+  l.visit_enter_function_scope();
+  l.visit_enter_function_scope_body();
+  l.visit_variable_use(identifier_of(use));
+  l.visit_enter_block_scope();
+  l.visit_variable_declaration(identifier_of(declaration), variable_kind::_var);
+  l.visit_exit_block_scope();
+  l.visit_exit_function_scope();
+  l.visit_end_of_module();
 
-    EXPECT_THAT(v.errors, IsEmpty());
-  }
+  EXPECT_THAT(v.errors, IsEmpty());
 }
 
 TEST(test_lint,
