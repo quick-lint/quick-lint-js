8a980ca40f53941c80f873c4d85b8d06e663baac
Shivam Mehta <sm.cse17@gmail.com>
2021-05-13 05:42:57 +0530

    Added command line completion for CLI
    I've also added docs/COMPLETIONS.md which explains how to add shell
    completion for bash, zsh and fish shell manually!
    
    Note: This commit doesn't affect the working of quick-lint-js.
    
    Fixes issue: #188

---
+++ completions/README.md
@@ -0,0 +1,49 @@
+# Shell completions
+
+To get automatic completions for quick-lint-js's options you can install the provided shell completions.
+
+## Zsh
+
+To install the completions for zsh, you can place the `completions/_quick-lint-js` file in any
+directory referenced by `$fpath`.
+
+If you do not already have such a directory registered through your `~/.zshrc`, you can add one like this:
+
+```sh
+mkdir -p ${ZDOTDIR:-~}/.zsh_functions
+echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
+```
+
+Then copy the completion file to this directory:
+
+```sh
+cp completions/_quick-lint-js ${ZDOTDIR:-~}/.zsh_functions/_quick-lint-js
+```
+
+## Bash
+
+To install the completions for bash, you can `source` the `completions/quick-lint-js.bash` file
+in your `~/.bashrc` file.
+
+If you do not plan to delete the source folder of quick-lint-js, you can run
+
+```sh
+echo "source $(pwd)/completions/quick-lint-js.bash" >> ~/.bashrc
+```
+
+Otherwise you can copy it to the `~/.bash_completion` folder and source it from there:
+
+```sh
+mkdir -p ~/.bash_completion
+cp completions/quick-lint-js.bash ~/.bash_completion/quick-lint-js
+echo "source ~/.bash_completion/quick-lint-js" >> ~/.bashrc
+```
+
+## Fish
+
+To install the completions for fish, run
+
+```sh
+mkdir -p $fish_complete_path[1]
+cp completions/quick-lint-js.fish $fish_complete_path[1]/quick-lint-js.fish
+```
---
+++ completions/quick-lint-js.bash
@@ -0,0 +1,47 @@
+# Copyright (C) 2020  Matthew Glazar
+# See end of file for extended copyright information.
+
+_quick-lint-js () {
+        local cur opts
+        _init_completion -n = || return
+
+        opts='--help --version --lsp-server --exit-fail-on= --output-format= --vim-file-bufnr='
+
+        case $cur in
+                --output-format=*)
+                        COMPREPLY=($(compgen -W 'gnu-like vim-qflint-json' -- "${cur#*=}"))
+                        return
+                        ;;
+                --exit-fail-on=*|--vim-file-bufnr=*)
+                        return
+                        ;;
+        esac
+
+        if [[ $cur == -* ]]; then
+                COMPREPLY=($(compgen -W '${opts}' -- $cur))
+                [[ ${COMPREPLY-} == *= ]] && compopt -o nospace
+                return
+        else
+                _filedir
+        fi
+}
+
+complete -F _quick-lint-js quick-lint-js
+
+# quick-lint-js finds bugs in JavaScript programs.
+# Copyright (C) 2020  Matthew Glazar
+#
+# This file is part of quick-lint-js.
+#
+# quick-lint-js is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# quick-lint-js is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
