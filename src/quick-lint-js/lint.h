96065941a5a020eb5d51cbfcc81e1835bed5355b
Himanshu <himansingh24@gmail.com>
2021-05-14 13:12:45 +0530

    Disable variable lookup in presence of eval
    The following code doesn't report an error now
    
        eval("var x = 42;")
        console.log(x);

--- src/quick-lint-js/lint.h
+++ src/quick-lint-js/lint.h
@@ -210,11 +210,12 @@ class linter {
   void visit_variable_use(identifier name, used_variable_kind);
 
   void propagate_variable_uses_to_parent_scope(
-      bool allow_variable_use_before_declaration, bool consume_arguments);
+      bool allow_variable_use_before_declaration, bool consume_arguments,
+      bool propagate_eval_use);
   template <class Scope>
   void propagate_variable_uses_to_parent_scope(
       Scope &parent_scope, bool allow_variable_use_before_declaration,
-      bool consume_arguments);
+      bool consume_arguments, bool propagate_eval_use);
 
   void propagate_variable_declarations_to_parent_scope();
 
