f6dea18f441075f50cbe0a52a09c631929fbe44a
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-05-16 21:14:41 -0700

    Website: move web demo code into an .mjs file
    Make the web demo's code easier to bundle in the future by moving the
    entry point JS code into its own file.
    
    This commit should not change behavior.

---
+++ website/public/demo/demo.mjs
@@ -0,0 +1,68 @@
+// Copyright (C) 2020  Matthew Glazar
+// See end of file for extended copyright information.
+
+import { markEditorText } from "./editor.mjs";
+import { loadQuickLintJS } from "./quick-lint-js.mjs";
+
+let codeInputElement = document.getElementById("code-input");
+let shadowCodeInputElement = document.getElementById("shadow-code-input");
+
+codeInputElement.addEventListener("scroll", (event) => {
+  synchronizeScrolling();
+});
+synchronizeContent();
+
+new ResizeObserver(synchronizeSize).observe(codeInputElement);
+
+loadQuickLintJS()
+  .then((quickLintJS) => {
+    function lintAndUpdate() {
+      synchronizeContent();
+
+      // TODO(strager): On crash, show the error to the user.
+      let input = codeInputElement.value;
+      let marks = quickLintJS.parseAndLint(input);
+      markEditorText(shadowCodeInputElement, window, marks);
+    }
+    codeInputElement.addEventListener("input", (event) => {
+      lintAndUpdate();
+    });
+    lintAndUpdate();
+  })
+  .catch((error) => {
+    // TODO(strager): Show this error to the user.
+    console.error(error);
+  });
+
+function synchronizeContent() {
+  let input = codeInputElement.value;
+  shadowCodeInputElement.textContent = input + "\n\n\n";
+}
+
+function synchronizeScrolling() {
+  shadowCodeInputElement.scrollLeft = codeInputElement.scrollLeft;
+  shadowCodeInputElement.scrollTop = codeInputElement.scrollTop;
+}
+
+function synchronizeSize() {
+  shadowCodeInputElement.style.width = codeInputElement.style.width;
+  shadowCodeInputElement.style.height = codeInputElement.style.height;
+}
+
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew Glazar
+//
+// This file is part of quick-lint-js.
+//
+// quick-lint-js is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// quick-lint-js is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
