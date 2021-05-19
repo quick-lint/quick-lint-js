832984000832806a97f6516c7d2180ccde8eb0f1
Cahian Freire <cahianrocha.fr@gmail.com>
2021-05-19 15:42:59 -0300

    Make the event listener add squiggly underlines in regions with errors

--- plugin/sublime-text/main.py
+++ plugin/sublime-text/main.py
@@ -1,6 +1,8 @@
 import sublime
 import sublime_plugin
 
+from . import c_api
+
 
 class QuickLintJsListener(sublime_plugin.ViewEventListener):
     @classmethod
@@ -9,3 +11,25 @@ class QuickLintJsListener(sublime_plugin.ViewEventListener):
         if "JavaScript" in syntax:
             return True
         return False
+
+    def __init__(self, view):
+        self.view = view
+        self.parser = c_api.Parser()
+
+    def on_load(self):
+        allregion = sublime.Region(0, self.view.size())
+        allcontent = self.view.substr(allregion)
+        slices = self.parser.lint(allcontent)
+        regions = [sublime.Region(s.start, s.stop) for s in slices]
+        self.add_squiggly_underlines(regions)
+
+    def on_modified(self):
+        self.on_load()
+
+    def add_squiggly_underlines(self, regions):
+        flags = (
+            sublime.DRAW_SQUIGGLY_UNDERLINE
+            | sublime.DRAW_NO_FILL
+            | sublime.DRAW_NO_OUTLINE
+        )
+        self.view.add_regions("0", regions, "invalid.illegal", "", flags)
