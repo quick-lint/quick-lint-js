de1933961de6723055f6f8682ee47558482c6e5d
Cahian Freire <cahianfreire@gmail.com>
2021-05-25 10:51:38 -0300

    Add copyright

--- plugin/sublime-text/c_api.py
+++ plugin/sublime-text/c_api.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2020  Matthew Glazar
+# See end of file for extended copyright information.
+
 import os
 from ctypes import *
 
@@ -86,3 +89,22 @@ class Parser:
     def set_text_and_lint(self, text, count):
         self.set_text(text, count)
         return self.lint()
+
+
+# quick-lint-js finds bugs in JavaScript programs.
+# Copyright (C) 2020  Matthew Glazar
+#
+# This file is part of quick-lint-js.
+#
+# quick-lint-js is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# quick-lint-js is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
