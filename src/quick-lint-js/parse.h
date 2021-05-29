396e5b2479a8acf519dcbdcd25c6ff0cbdc8c368
Himanshu <himansingh24@gmail.com>
2021-05-29 08:34:46 +0530

    Reduce limit from 375 to 300

--- src/quick-lint-js/parse.h
+++ src/quick-lint-js/parse.h
@@ -3454,7 +3454,7 @@ class parser {
 #endif
 
   std::uint16_t depth_ = 0;
-  const std::uint16_t limit_ = 375;
+  const std::uint16_t limit_ = 300;
 
   using loop_guard = bool_guard<&parser::in_loop_statement_>;
   using switch_guard = bool_guard<&parser::in_switch_statement_>;
