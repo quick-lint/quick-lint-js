8a0a1db8685673b7d8d6b9957bc31b148175962f
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-06-15 22:03:02 -0700

    Add missing #include guards

--- src/quick-lint-js/math-overflow.h
+++ src/quick-lint-js/math-overflow.h
@@ -1,6 +1,9 @@
 // Copyright (C) 2020  Matthew "strager" Glazar
 // See end of file for extended copyright information.
 
+#ifndef QUICK_LINT_JS_MATH_OVERFLOW_H
+#define QUICK_LINT_JS_MATH_OVERFLOW_H
+
 #include <limits>
 #include <optional>
 #include <quick-lint-js/narrow-cast.h>
@@ -28,6 +31,8 @@ inline std::optional<int> checked_add(int x, int y) noexcept {
   }
 }
 }
+
+#endif
 
 // quick-lint-js finds bugs in JavaScript programs.
 // Copyright (C) 2020  Matthew "strager" Glazar
