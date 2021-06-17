f4c9c99e6107eefda5952d750c3a23607bca0f92
Himanshu <himansingh24@gmail.com>
2021-06-17 09:34:30 +0530

    Refactor: remove nested transactions

--- src/quick-lint-js/parse.h
+++ src/quick-lint-js/parse.h
@@ -350,16 +350,14 @@ class parser {
         token star_token = this->peek();
         this->skip();
         if (this->peek().type == token_type::kw_function) {
-          lexer_transaction function_begin_transaction =
-              this->lexer_.begin_transaction();
           this->skip();
           this->error_reporter_->report(
               error_generator_function_star_belongs_before_name{
                   .function_name = this->peek().span(),
                   .star = star_token.span(),
               });
-          this->lexer_.roll_back_transaction(
-              std::move(function_begin_transaction));
+          this->lexer_.roll_back_transaction(std::move(transaction));
+          this->skip();
           this->parse_and_visit_function_declaration(
               v, function_attributes::generator,
               /*begin=*/this->peek().begin,
