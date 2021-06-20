fa6bd55e23f009a4090e27443347b6c99107100f
Matheus <30329820+matttheus@users.noreply.github.com>
2021-06-20 19:21:07 -0300

    Fix demo tests
    Add message, code and severity to mark tags

--- website/test/test-demo.mjs
+++ website/test/test-demo.mjs
@@ -6,6 +6,7 @@ import jsdom from "jsdom";
 import { markEditorText, sanitizeMarks } from "../public/demo/editor.mjs";
 
 let dom = new jsdom.JSDOM("");
+const unKnownErrorCode = "E0XXX";
 
 function preElementWithHTML(html) {
   let element = dom.window.document.createElement("pre");
@@ -16,14 +17,36 @@ function preElementWithHTML(html) {
 describe("markEditorText", () => {
   it("mark first word on line", () => {
     let editor = preElementWithHTML("hello world");
-    markEditorText(editor, dom.window, [{ begin: 0, end: 5 }]);
-    assert.strictEqual(editor.innerHTML, "<mark>hello</mark> world");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 0,
+        end: 5,
+        message: "first word error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      '<mark data-message="first word error" data-code="E0XXX" data-severity="5">hello</mark> world'
+    );
   });
 
   it("mark last word on line", () => {
     let editor = preElementWithHTML("hello world");
-    markEditorText(editor, dom.window, [{ begin: 6, end: 11 }]);
-    assert.strictEqual(editor.innerHTML, "hello <mark>world</mark>");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 6,
+        end: 11,
+        message: "last word error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      'hello <mark data-message="last word error" data-code="E0XXX" data-severity="5">world</mark>'
+    );
   });
 
   it("mark across two text nodes", () => {
@@ -32,14 +55,36 @@ describe("markEditorText", () => {
     editor.appendChild(dom.window.document.createTextNode("hello"));
     editor.appendChild(dom.window.document.createTextNode("world"));
 
-    markEditorText(editor, dom.window, [{ begin: 3, end: 8 }]);
-    assert.strictEqual(editor.innerHTML, "hel<mark>lowor</mark>ld");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 3,
+        end: 8,
+        message: "lowor error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      'hel<mark data-message="lowor error" data-code="E0XXX" data-severity="5">lowor</mark>ld'
+    );
   });
 
   it("marking deletes existing non-overlapping marks", () => {
     let editor = preElementWithHTML("<mark>hello</mark> world");
-    markEditorText(editor, dom.window, [{ begin: 6, end: 11 }]);
-    assert.strictEqual(editor.innerHTML, "hello <mark>world</mark>");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 6,
+        end: 11,
+        message: "non-overlapping marks error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      'hello <mark data-message="non-overlapping marks error" data-code="E0XXX" data-severity="5">world</mark>'
+    );
   });
 
   it("marking with no marks deletes existing marks", () => {
@@ -51,76 +96,192 @@ describe("markEditorText", () => {
   it("multiple new marks", () => {
     let editor = preElementWithHTML("hello world");
     markEditorText(editor, dom.window, [
-      { begin: 0, end: 5 },
-      { begin: 6, end: 11 },
+      {
+        begin: 0,
+        end: 5,
+        message: "multi mark error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+      {
+        begin: 6,
+        end: 11,
+        message: "multi mark error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
     ]);
     assert.strictEqual(
       editor.innerHTML,
-      "<mark>hello</mark> <mark>world</mark>"
+      '<mark data-message="multi mark error" data-code="E0XXX" data-severity="5">hello</mark> <mark data-message="multi mark error" data-code="E0XXX" data-severity="5">world</mark>'
     );
   });
 
   it("marking removes empty <mark>", () => {
     let editor = preElementWithHTML("<mark></mark> world");
-    markEditorText(editor, dom.window, [{ begin: 1, end: 6 }]);
-    assert.strictEqual(editor.innerHTML, " <mark>world</mark>");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 1,
+        end: 6,
+        message: "empty mark error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      ' <mark data-message="empty mark error" data-code="E0XXX" data-severity="5">world</mark>'
+    );
   });
 
   it("marking preserves <br> immediately after mark", () => {
     let editor = preElementWithHTML("hello<br>world");
-    markEditorText(editor, dom.window, [{ begin: 0, end: 5 }]);
-    assert.strictEqual(editor.innerHTML, "<mark>hello</mark><br>world");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 0,
+        end: 5,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      '<mark data-message="unknow error" data-code="E0XXX" data-severity="5">hello</mark><br>world'
+    );
   });
 
   it("marking preserves <br> before inserted mark", () => {
     let editor = preElementWithHTML("one<br>twothree");
-    markEditorText(editor, dom.window, [{ begin: 7, end: 7 + "three".length }]);
-    assert.strictEqual(editor.innerHTML, "one<br>two<mark>three</mark>");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 7,
+        end: 7 + "three".length,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      'one<br>two<mark data-message="unknow error" data-code="E0XXX" data-severity="5">three</mark>'
+    );
   });
 
   it("marking preserves <br> after inserted mark", () => {
     let editor = preElementWithHTML("onetwo<br>three");
-    markEditorText(editor, dom.window, [{ begin: 0, end: 3 }]);
-    assert.strictEqual(editor.innerHTML, "<mark>one</mark>two<br>three");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 0,
+        end: 3,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      '<mark data-message="unknow error" data-code="E0XXX" data-severity="5">one</mark>two<br>three'
+    );
   });
 
   it("mark exactly over existing <mark>", () => {
     let editor = preElementWithHTML("<mark>hello</mark> world");
-    markEditorText(editor, dom.window, [{ begin: 0, end: 5 }]);
-    assert.strictEqual(editor.innerHTML, "<mark>hello</mark> world");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 0,
+        end: 5,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      '<mark data-message="unknow error" data-code="E0XXX" data-severity="5">hello</mark> world'
+    );
   });
 
   it("mark starts at end of existing <mark>", () => {
     let editor = preElementWithHTML("<mark>hello</mark>world");
-    markEditorText(editor, dom.window, [{ begin: 5, end: 10 }]);
-    assert.strictEqual(editor.innerHTML, "hello<mark>world</mark>");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 5,
+        end: 10,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      'hello<mark data-message="unknow error" data-code="E0XXX" data-severity="5">world</mark>'
+    );
   });
 
   it("add empty mark", () => {
     let editor = preElementWithHTML("helloworld");
-    markEditorText(editor, dom.window, [{ begin: 5, end: 5 }]);
-    assert.strictEqual(editor.innerHTML, "hello<mark></mark>world");
+    markEditorText(editor, dom.window, [
+      {
+        begin: 5,
+        end: 5,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+    ]);
+    assert.strictEqual(
+      editor.innerHTML,
+      'hello<mark data-message="unknow error" data-code="E0XXX" data-severity="5"></mark>world'
+    );
   });
 
   it("add empty mark immediately after non-empty mark", () => {
     let editor = preElementWithHTML("helloworld");
     markEditorText(editor, dom.window, [
-      { begin: 0, end: 5 },
-      { begin: 5, end: 5 },
+      {
+        begin: 0,
+        end: 5,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+      {
+        begin: 5,
+        end: 5,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
     ]);
     assert.strictEqual(
       editor.innerHTML,
-      "<mark>hello</mark><mark></mark>world"
+      '<mark data-message="unknow error" data-code="E0XXX" data-severity="5">hello</mark><mark data-message="unknow error" data-code="E0XXX" data-severity="5"></mark>world'
     );
   });
 
   it("identical marks are merged", () => {
     let editor = preElementWithHTML("helloworld");
     markEditorText(editor, dom.window, [
-      { begin: 0, end: 5 },
-      { begin: 0, end: 5 },
+      {
+        begin: 0,
+        end: 5,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
+      {
+        begin: 0,
+        end: 5,
+        message: "unknow error",
+        code: unKnownErrorCode,
+        severity: 5,
+      },
     ]);
-    assert.strictEqual(editor.innerHTML, "<mark>hello</mark>world");
+    assert.strictEqual(
+      editor.innerHTML,
+      '<mark data-message="unknow error" data-code="E0XXX" data-severity="5">hello</mark>world'
+    );
   });
 });
 
