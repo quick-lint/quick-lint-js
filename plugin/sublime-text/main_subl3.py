d75133e3233d5d450b1733deffdf1af6c8fcb6c8
Cahian Freire <cahianfreire@gmail.com>
2021-06-24 13:50:54 -0300

    Sublime Text: Set default value to empty string
    When obtaining the syntax of the file to be analyzed, if it is
    not defined there will be an error in the plugin because the value
    returned will be `None`. That's why I decided to set a default value
    for this situation and thus not get an error.

--- plugin/sublime-text/main_subl3.py
+++ plugin/sublime-text/main_subl3.py
@@ -10,7 +10,7 @@ from . import c_api
 class QuickLintJsListener(sublime_plugin.ViewEventListener):
     @classmethod
     def is_applicable(cls, settings):
-        syntax = settings.get("syntax")
+        syntax = settings.get("syntax", "")
         if "JavaScript" in syntax:
             return True
         return False
