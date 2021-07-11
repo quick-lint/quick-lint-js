742ccb3ca1bd14e00d9309b6b7658eecced66614
Matheus <30329820+matttheus@users.noreply.github.com>
2021-07-11 02:00:41 -0300

    Add branch requirement checking (#367)
    This commit add the check if the build is related to some branch, if the build is not related to a existing branch, then the build gonna be deleted.

--- tools/prune-old-builds.py
+++ tools/prune-old-builds.py
@@ -1,7 +1,8 @@
 import os
-from datetime import datetime
-from pathlib import Path
 import shutil
+import subprocess
+from pathlib import Path
+from datetime import datetime
 
 FOURTEEN_DAYS = 14
 
@@ -13,14 +14,37 @@ def how_old(date: str) -> int:
 
 if __name__ == '__main__':
     builds_path = Path('builds/')
+    repo_url = 'https://github.com/quick-lint/quick-lint-js.git'
+    repo_name = 'quick-lint-js'
+
+    if os.path.exists(repo_name):
+        shutil.rmtree(repo_name)
+
+    subprocess.run(['git', 'clone', '-q', repo_url])
+
+    print('Getting all commits of the repository')
+    commits = subprocess.getoutput(
+        f"cd {repo_name} && git rev-list --all --remotes").split('\n')
+
+    builds_path = Path('builds/')
 
     for folder in os.listdir(builds_path):
         folder_path = builds_path / folder
         created_at = datetime.fromtimestamp(
             os.path.getctime(folder_path)).strftime('%Y-%m-%d')
 
+        if not folder in commits:
+            print(
+                f'The build of hash {folder} is not related to a branch anymore and will be deleted')
+            shutil.rmtree(folder_path)
+
         if how_old(created_at) > FOURTEEN_DAYS:
+            print(
+                f'The build {folder} take more then fourteen days, then it will be deleted')
             try:
                 shutil.rmtree(folder_path)
             except OSError as e:
                 print("Error: %s : %s" % (folder_path, e.strerror))
+
+    print('Removing quick-lint-js repository')
+    shutil.rmtree(repo_name)
