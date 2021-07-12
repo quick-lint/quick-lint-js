4003d6cb8d170d24454f96317f791e5be9bbb0d1
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-07-11 18:43:14 -0700

    Refactor result's implementation
    result has four specializations with lots of repetition. Factor most of
    the repetition into a class template called result_base.
    
    This commit should not change behavior.

--- src/quick-lint-js/result.h
+++ src/quick-lint-js/result.h
@@ -13,44 +13,63 @@
 #include <variant>
 
 namespace quick_lint_js {
-// Like std::variant, but more ergonomic.
-//
-// Errors must be unique. For example, result<int, char, char> is illegal.
 template <class T, class... Errors>
-class result {
+class result;
+
+// Do not use result_error_tag directly. Call result<>::failure instead.
+template <class Error>
+struct result_error_tag {};
+
+// Do not use result_base directly. Use result instead.
+template <class T, class... Errors>
+class result_base {
+ private:
+  using result = quick_lint_js::result<T, Errors...>;
+  using value_type =
+      std::conditional_t<std::is_same_v<T, void>, std::monostate, T>;
+
  public:
   template <class... Args>
-  explicit result(Args&&... args)
+  explicit result_base(Args&&... args)
       : data_(std::in_place_index<0>, std::forward<Args>(args)...) {}
 
-  // TODO(strager): Allow copying.
-  result(const result&) = delete;
-  result& operator=(const result&) = delete;
+  // Private constructor used by failure. Do not call directly.
+  template <class Error, class... ErrorArgs>
+  explicit result_base(result_error_tag<Error>, ErrorArgs&&... error_args)
+      // TODO(strager): If std::is_same_v<Error, value_type>, then
+      // std::in_place_type is incorrect.
+      : data_(std::in_place_type<Error>,
+              std::forward<ErrorArgs>(error_args)...) {}
 
-  result(result&&) = default;
-  result& operator=(result&&) = default;
+  // TODO(strager): Allow copying.
+  result_base(const result_base&) = delete;
+  result_base& operator=(const result_base&) = delete;
+
+  result_base(result_base&&) = default;
+  result_base& operator=(result_base&&) = default;
 
   template <class Error>
   static result failure(Error&& error) {
-    return result(error_tag<Error>(), std::forward<Error>(error));
+    return result(result_error_tag<Error>(), std::forward<Error>(error));
   }
 
   template <class Error, class... ErrorArgs>
   static result failure(ErrorArgs&&... error_args) {
-    return result(error_tag<Error>(), std::forward<ErrorArgs>(error_args)...);
+    return result(result_error_tag<Error>(),
+                  std::forward<ErrorArgs>(error_args)...);
   }
 
   bool ok() const noexcept { return this->data_.index() == 0; }
 
-  T& value() noexcept {
+  value_type& value() noexcept {
     QLJS_ASSERT(this->ok());
     return std::get<0>(this->data_);
   }
 
   template <class Error>
   bool has_error() const noexcept {
-    // TODO(strager): If std::is_same_v<Error, T>, then std::holds_alternative
-    // is incorrect.
+    // TODO(strager): If std::is_same_v<Error, value_type>, then
+    // std::holds_alternative is incorrect.
     return std::holds_alternative<Error>(this->data_);
   }
 
@@ -58,182 +77,96 @@ class result {
   const Error& error() const noexcept {
     QLJS_ASSERT(!this->ok());
     QLJS_ASSERT(this->has_error<Error>());
-    // TODO(strager): If std::is_same_v<Error, T>, then std::get<Error> is
-    // incorrect.
+    // TODO(strager): If std::is_same_v<Error, value_type>, then std::get<Error>
+    // is incorrect.
     return std::get<Error>(this->data_);
   }
 
-  T& operator*() noexcept { return this->value(); }
-  T* operator->() noexcept { return &this->value(); }
+  value_type& operator*() noexcept { return this->value(); }
+  value_type* operator->() noexcept { return &this->value(); }
 
  private:
-  template <class Error>
-  struct error_tag {};
-
-  template <class Error, class... ErrorArgs>
-  explicit result(error_tag<Error>, ErrorArgs&&... error_args)
-      // TODO(strager): If std::is_same_v<Error, T>, then std::in_place_type is
-      // incorrect.
-      : data_(std::in_place_type<Error>,
-              std::forward<ErrorArgs>(error_args)...) {}
-
-  std::variant<T, Errors...> data_;
+  std::variant<value_type, Errors...> data_;
 };
 
-// Like std::variant, but more ergonomic.
+// Like std::variant<T, Errors...>, but more ergonomic.
+//
+// Errors must be unique. For example, result<int, char, char> is illegal.
+template <class T, class... Errors>
+class result : private result_base<T, Errors...> {
+ private:
+  using base = result_base<T, Errors...>;
+
+ public:
+  using base::base;
+  using base::error;
+  using base::failure;
+  using base::has_error;
+  using base::ok;
+  using base::operator*;
+  using base::operator->;
+  using base::operator=;
+  using base::value;
+};
+
+// Like std::variant<std::monostate, Errors...>, but more ergonomic.
 //
 // Errors must be unique. For example, result<void, char, char> is illegal.
 template <class... Errors>
-class result<void, Errors...> {
- public:
-  /*implicit*/ result() = default;
-
-  // TODO(strager): Allow copying.
-  result(const result&) = delete;
-  result& operator=(const result&) = delete;
-
-  result(result&&) = default;
-  result& operator=(result&&) = default;
-
-  template <class Error>
-  static result failure(Error&& error) {
-    return result(error_tag<Error>(), std::forward<Error>(error));
-  }
-
-  template <class Error, class... ErrorArgs>
-  static result failure(ErrorArgs&&... error_args) {
-    return result(error_tag<Error>(), std::forward<ErrorArgs>(error_args)...);
-  }
-
-  bool ok() const noexcept { return this->data_.index() == 0; }
-
-  template <class Error>
-  bool has_error() const noexcept {
-    return std::holds_alternative<Error>(this->data_);
-  }
-
-  template <class Error>
-  const Error& error() const noexcept {
-    QLJS_ASSERT(!this->ok());
-    QLJS_ASSERT(this->has_error<Error>());
-    return std::get<Error>(this->data_);
-  }
-
+class result<void, Errors...> : private result_base<void, Errors...> {
  private:
-  template <class Error>
-  struct error_tag {};
+  using base = result_base<void, Errors...>;
 
-  template <class Error, class... ErrorArgs>
-  explicit result(error_tag<Error>, ErrorArgs&&... error_args)
-      // TODO(strager): If std::is_same_v<Error, T>, then std::in_place_type is
-      // incorrect.
-      : data_(std::in_place_type<Error>,
-              std::forward<ErrorArgs>(error_args)...) {}
-
-  std::variant<std::monostate, Errors...> data_;
+ public:
+  using base::base;
+  using base::error;
+  using base::has_error;
+  using base::ok;
+  using base::operator=;
+  using base::failure;
 };
 
 // Like std::variant<T, Error>, but more ergonomic.
 template <class T, class Error>
-class result<T, Error> {
+class result<T, Error> : private result_base<T, Error> {
+ private:
+  using base = result_base<T, Error>;
+
  public:
-  template <class... Args>
-  explicit result(Args&&... args)
-      : value_(std::forward<Args>(args)...), ok_(true) {}
-
-  // TODO(strager): Allow copying.
-  result(const result&) = delete;
-  result& operator=(const result&) = delete;
-
-  result(result&& other) noexcept : ok_(other.ok_) {
-    if (this->ok_) {
-      new (&this->value_) T(std::move(other.value_));
-    } else {
-      new (&this->error_) Error(std::move(other.error_));
-    }
-  }
-
-  result& operator=(result&& other) noexcept {
-    this->~result();
-    new (this) result(std::move(other));
-    return *this;
-  }
-
-  ~result() {
-    if (this->ok_) {
-      this->value_.~T();
-    } else {
-      this->error_.~Error();
-    }
-  }
+  using base::base;
+  using base::ok;
+  using base::operator*;
+  using base::operator->;
+  using base::operator=;
+  using base::value;
 
   template <class... ErrorArgs>
   static result failure(ErrorArgs&&... error_args) {
-    return result(error_tag(), std::forward<ErrorArgs>(error_args)...);
+    return base::template failure<Error, ErrorArgs...>(
+        std::forward<ErrorArgs>(error_args)...);
   }
 
-  bool ok() const noexcept { return this->ok_; }
-
-  T& value() noexcept {
-    QLJS_ASSERT(this->ok());
-    return this->value_;
-  }
-
-  const Error& error() const noexcept {
-    QLJS_ASSERT(!this->ok());
-    return this->error_;
-  }
-
-  T& operator*() noexcept { return this->value(); }
-  T* operator->() noexcept { return &this->value(); }
-
- private:
-  struct error_tag {};
-
-  template <class... ErrorArgs>
-  explicit result(error_tag, ErrorArgs&&... error_args)
-      : error_(std::forward<ErrorArgs>(error_args)...), ok_(false) {}
-
-  union {
-    T value_;
-    Error error_;
-  };
-  bool ok_;
+  const Error& error() const noexcept { return base::template error<Error>(); }
 };
 
 // Like std::optional<Error>, but with a similar interface to result<T, Error>.
 template <class Error>
-class result<void, Error> {
+class result<void, Error> : private result_base<void, Error> {
+ private:
+  using base = result_base<void, Error>;
+
  public:
-  /*implicit*/ result() = default;
-
-  // TODO(strager): Allow copying.
-  result(const result&) = delete;
-  result& operator=(const result&) = delete;
-
-  result(result&& other) noexcept = default;
-  result& operator=(result&& other) noexcept = default;
+  using base::base;
+  using base::operator=;
+  using base::ok;
 
   template <class... ErrorArgs>
   static result failure(ErrorArgs&&... error_args) {
-    return result(error_tag(), std::forward<ErrorArgs>(error_args)...);
+    return base::template failure<Error, ErrorArgs...>(
+        std::forward<ErrorArgs>(error_args)...);
   }
 
-  bool ok() const noexcept { return !this->error_.has_value(); }
-
-  const Error& error() const noexcept {
-    QLJS_ASSERT(!this->ok());
-    return *this->error_;
-  }
-
- private:
-  struct error_tag {};
-
-  template <class... ErrorArgs>
-  explicit result(error_tag, ErrorArgs&&... error_args)
-      : error_(std::in_place, std::forward<ErrorArgs>(error_args)...) {}
-
-  std::optional<Error> error_;
+  const Error& error() const noexcept { return base::template error<Error>(); }
 };
 
 // Like std::variant<T, std::string>, but more ergonomic.
