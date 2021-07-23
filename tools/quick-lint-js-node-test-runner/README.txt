95bf61e3caf563584eb1b75322a907013c99b03e
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-07-22 18:20:03 -0700

    Factor website's run-tests.mjs into reusable module
    I want to use Jasmine and website/run-tests.mjs for testing code in
    wasm/. Make run-tests.mjs reusable by moving it into its own package.
    
    This commit should not change behavior.

---
+++ tools/quick-lint-js-node-test-runner/README.txt
@@ -0,0 +1,2 @@
+This directory contains a Node.js support package for
+running Jasmine-based tests.
--- website/run-tests.mjs
+++ website/run-tests.mjs
@@ -1,118 +1,14 @@
 // Copyright (C) 2020  Matthew "strager" Glazar
 // See end of file for extended copyright information.
 
-import Jasmine from "jasmine";
-import colors from "colors";
-import fs from "fs";
+import { main } from "quick-lint-js-node-test-runner";
 import path from "path";
 import url from "url";
 
 let __filename = url.fileURLToPath(import.meta.url);
 let __dirname = path.dirname(__filename);
 
-function main() {
-  let jasmine = new Jasmine();
-  jasmine.loadConfig({
-    spec_dir: path.relative("", __dirname),
-    spec_files: ["!node_modules/**", "**/test-*.mjs"],
-    stopSpecOnExpectationFailure: true,
-    random: false,
-  });
-  jasmine.clearReporters();
-  jasmine.addReporter(new BasicReporter());
-  let { fileNames, testNames } = parseCommandLineOptions();
-  jasmine.execute(fileNames, testNames);
-}
-
-function parseCommandLineOptions() {
-  let options = process.argv.slice(2);
-
-  let fileNames = [];
-  let testNames = [];
-  for (let option of options) {
-    if (fs.existsSync(option)) {
-      fileNames.push(option);
-    } else {
-      testNames.push(option);
-    }
-  }
-  if (testNames.length > 1) {
-    console.error(
-      `${colors.red("ERROR")} only one test name filter is allowed`
-    );
-    process.exit(1);
-  }
-  return { fileNames, testNames };
-}
-
-class BasicReporter {
-  constructor() {
-    this._executedTests = 0;
-  }
-
-  jasmineStarted(_summary) {}
-
-  jasmineDone(summary) {
-    switch (summary.overallStatus) {
-      case "passed":
-        if (this._executedTests === 0) {
-          console.error(`${colors.red("ERROR")} no tests executed`);
-          process.exit(1);
-        }
-        break;
-
-      case "incomplete":
-        console.error(`${colors.red("ERROR")} ${summary.incompleteReason}`);
-        process.exit(1);
-        break;
-
-      case "failed":
-        // specDone should have killed the process already. But just in case...
-        console.error(`${colors.red("ERROR")} tests failed`);
-        process.exit(1);
-        break;
-
-      default:
-        console.error(
-          `${colors.red("ERROR")} unknown status: ${summary.overallStatus}`
-        );
-        process.exit(1);
-        break;
-    }
-  }
-
-  suiteStarted(_suite) {}
-
-  suiteDone(_suite) {}
-
-  specStarted(spec) {
-    console.error(`Running ${spec.fullName} ...`);
-  }
-
-  specDone(spec) {
-    switch (spec.status) {
-      case "passed":
-        console.error(`${colors.green("OK")}      ${spec.fullName}`);
-        this._executedTests += 1;
-        break;
-
-      case "excluded":
-        console.error(`${colors.yellow("SKIP")}    ${spec.fullName}`);
-        break;
-
-      default:
-      case "failed":
-        for (let failure of spec.failedExpectations) {
-          console.error(failure.stack);
-        }
-        console.error(`${colors.red("FAIL")}    ${spec.fullName}`);
-        process.exit(1);
-        break;
-    }
-  }
-}
-
-main();
+main(__dirname);
 
 // quick-lint-js finds bugs in JavaScript programs.
 // Copyright (C) 2020  Matthew "strager" Glazar
