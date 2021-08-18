519a07c4810355fa4b1fb723dba25860960cb8bd
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-08-17 20:55:15 -0700

    VS Code: refactor: move overlaid document management into fs class

--- plugin/vscode/quick-lint-js-vscode-node.cpp
+++ plugin/vscode/quick-lint-js-vscode-node.cpp
@@ -227,16 +227,48 @@ class vscode_error_reporter final : public error_reporter {
 // as real files.
 class vscode_configuration_filesystem : public configuration_filesystem {
  public:
-  explicit vscode_configuration_filesystem(qljs_workspace* workspace)
-      : workspace_(workspace) {}
-
   result<canonical_path_result, canonicalize_path_io_error> canonicalize_path(
       const std::string&) override;
   result<padded_string, read_file_io_error, watch_io_error> read_file(
       const canonical_path&) override;
 
+  void clear() { this->overlaid_documents_.clear(); }
+
+  void overlay_document(std::string&& file_path, qljs_document* doc) {
+    auto [_it, inserted] =
+        this->overlaid_documents_.try_emplace(std::move(file_path), doc);
+    QLJS_ASSERT(inserted);
+  }
+
+  void forget_document(qljs_document* doc) {
+    auto doc_it = std::find_if(this->overlaid_documents_.begin(),
+                               this->overlaid_documents_.end(),
+                               [&](auto& pair) { return pair.second == doc; });
+    if (doc_it == this->overlaid_documents_.end()) {
+      // The document could be missing for any of the following reasons:
+      // * Our extension was loaded after the document was opened.
+      //   (TODO(strager): We should fix this.)
+      // * The closed document was unnamed.
+    } else {
+      this->overlaid_documents_.erase(doc_it);
+    }
+  }
+
  private:
-  qljs_workspace* workspace_;
+  qljs_document* find_document(std::string_view path) {
+#if QLJS_HAVE_STD_TRANSPARENT_KEYS
+    std::string_view key = path;
+#else
+    std::string key(path);
+#endif
+    auto doc_it = this->overlaid_documents_.find(key);
+    if (doc_it == this->overlaid_documents_.end()) {
+      return nullptr;
+    }
+    return doc_it->second;
+  }
+
+  std::unordered_map<std::string, qljs_document*> overlaid_documents_;
   basic_configuration_filesystem underlying_fs_;
 };
 
@@ -257,7 +289,6 @@ class qljs_workspace : public ::Napi::ObjectWrap<qljs_workspace> {
   explicit qljs_workspace(const ::Napi::CallbackInfo& info)
       : ::Napi::ObjectWrap<qljs_workspace>(info),
         vscode_(info[0].As<::Napi::Object>()),
-        fs_(this),
         qljs_documents_(info.Env()),
         vscode_diagnostic_collection_ref_(
             ::Napi::Persistent(info[1].As<::Napi::Object>())) {}
@@ -282,10 +313,6 @@ class qljs_workspace : public ::Napi::ObjectWrap<qljs_workspace> {
   ::Napi::Value create_document(::Napi::Env, ::Napi::Object vscode_document,
                                 document_type);
 
-  qljs_document* find_document(std::string_view path);
-
-  void forget_document(qljs_document*);
-
   void publish_diagnostics(qljs_document*, ::Napi::Value diagnostics);
   void delete_diagnostics(qljs_document*);
 
@@ -301,9 +328,6 @@ class qljs_workspace : public ::Napi::ObjectWrap<qljs_workspace> {
   // Mapping from vscode.Document to qljs.QLJSDocument (qljs_document).
   js_map qljs_documents_;
   ::Napi::ObjectReference vscode_diagnostic_collection_ref_;
-
-  // qljs_document-s opened for editing.
-  std::unordered_map<std::string, qljs_document*> documents_;
 
   friend class qljs_document;
 };
@@ -411,39 +435,13 @@ vscode_configuration_filesystem::canonicalize_path(const std::string& path) {
 
 result<padded_string, read_file_io_error, watch_io_error>
 vscode_configuration_filesystem::read_file(const canonical_path& path) {
-  qljs_document* doc = this->workspace_->find_document(path.path());
+  qljs_document* doc = this->find_document(path.path());
   if (!doc) {
     QLJS_DEBUG_LOG("Reading file from disk: %s\n", path.c_str());
     return this->underlying_fs_.read_file(path);
   }
   QLJS_DEBUG_LOG("Reading file from open document: %s\n", path.c_str());
   return padded_string(doc->document_string().string_view());
-}
-
-qljs_document* qljs_workspace::find_document(std::string_view path) {
-#if QLJS_HAVE_STD_TRANSPARENT_KEYS
-  std::string_view key = path;
-#else
-  std::string key(path);
-#endif
-  auto doc_it = this->documents_.find(key);
-  if (doc_it == this->documents_.end()) {
-    return nullptr;
-  }
-  return doc_it->second;
-}
-
-void qljs_workspace::forget_document(qljs_document* doc) {
-  auto doc_it = std::find_if(this->documents_.begin(), this->documents_.end(),
-                             [&](auto& pair) { return pair.second == doc; });
-  if (doc_it == this->documents_.end()) {
-    // The document could be missing for any of the following reasons:
-    // * Our extension was loaded after the document was opened.
-    //   (TODO(strager): We should fix this.)
-    // * The closed document was unnamed.
-  } else {
-    this->documents_.erase(doc_it);
-  }
 }
 
 void qljs_workspace::publish_diagnostics(qljs_document* doc,
@@ -493,7 +491,7 @@ void qljs_workspace::dispose_documents() {
     this->delete_diagnostics(doc);
   });
   this->qljs_documents_.clear();
-  this->documents_.clear();
+  this->fs_.clear();
 }
 
 ::Napi::Value qljs_workspace::dispose_linter(const ::Napi::CallbackInfo& info) {
@@ -506,7 +504,7 @@ void qljs_workspace::dispose_documents() {
     doc->dispose();
     this->delete_diagnostics(doc);
     this->qljs_documents_.erase(vscode_document);
-    this->forget_document(doc);
+    this->fs_.forget_document(doc);
   }
 
   return env.Undefined();
@@ -584,8 +582,7 @@ void qljs_workspace::dispose_documents() {
   }
   doc->init(env, this, file_path, type == document_type::config);
   if (file_path.has_value()) {
-    auto [_it, inserted] = this->documents_.try_emplace(*file_path, doc);
-    QLJS_ASSERT(inserted);
+    this->fs_.overlay_document(std::move(*file_path), doc);
   }
   return js_doc;
 }
