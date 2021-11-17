7fa115f0d6434cb90989329c29e43eb7fdd91da7
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-11-16 17:54:41 -0800

    benchmark-lsp: rewrite in C++
    benchmark-lsp is written in Haskell. This turned out to be a suboptimal
    decision:
    
    * lsp-test didn't do what we needed, so we had to effectively rewrite
      it.
    * Criterion doesn't give us control over the number of iterations. This
      makes comparing different linters impossible because *N* is different
      for different linters. We would need to effectively rewrite Criterion
      to fix this problem.
    * Haskell is foreign and scary for most people. (C++ is too, but less
      so.)
    
    Rewrite benchmark-lsp in C++ to address these problems.
    
    Notable differences:
    
    * CLI options are very different.
    * The output JSON format is very different.
    * The "broken" config option is no longer supported.
    * There is no HTML output format with pretty graphs.
    * Each benchmark now has a warmup phase.
    
    Because the CLI changed and the JSON format changed, the website's
    instructions and scripts for the benchmarks are out of date. The website
    will be fixed in a future commit.

---
+++ src/boost-json.cpp
@@ -0,0 +1,64 @@
+// Copyright (C) 2020  Matthew "strager" Glazar
+// See end of file for extended copyright information.
+
+#if defined(BOOST_JSON_STANDALONE)
+
+#include <boost/json/value.hpp>
+#include <quick-lint-js/boost-json.h>
+
+namespace quick_lint_js {
+::boost::json::array* if_array(::boost::json::object& object,
+                               ::boost::json::string_view key) {
+  if (::boost::json::value* value = object.if_contains(key)) {
+    return value->if_array();
+  } else {
+    return nullptr;
+  }
+}
+
+bool* if_bool(::boost::json::object& object, ::boost::json::string_view key) {
+  if (::boost::json::value* value = object.if_contains(key)) {
+    return value->if_bool();
+  } else {
+    return nullptr;
+  }
+}
+
+std::int64_t* if_int64(::boost::json::object& object,
+                       ::boost::json::string_view key) {
+  if (::boost::json::value* value = object.if_contains(key)) {
+    return value->if_int64();
+  } else {
+    return nullptr;
+  }
+}
+
+::boost::json::string* if_string(::boost::json::object& object,
+                                 ::boost::json::string_view key) {
+  if (::boost::json::value* value = object.if_contains(key)) {
+    return value->if_string();
+  } else {
+    return nullptr;
+  }
+}
+}
+
+#endif
+
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew "strager" Glazar
+//
+// This file is part of quick-lint-js.
+//
+// quick-lint-js is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// quick-lint-js is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
