9d3e735097dc22fcf338731af4a4a2144f1a2f3a
Matthew "strager" Glazar <strager.nds@gmail.com>
2021-12-07 16:48:11 -0800

    build-sizes: quickly generate build-sizes.json files
    The collect-binary-sizes script works on one build directory. Write a
    wrapper script which runs collect-binary-sizes on each build directory
    on c.quick-lint-js.com. Write it in Go for easy I/O parallelism.

---
+++ tools/build-sizes/collect-many-binary-sizes.go
@@ -0,0 +1,150 @@
+// Copyright (C) 2020  Matthew "strager" Glazar
+// See end of file for extended copyright information.
+
+package main
+
+import "path/filepath"
+import "sync"
+import "path"
+import "io"
+import "os/exec"
+import "bytes"
+import "errors"
+import "flag"
+import "fmt"
+import "log"
+import "os"
+
+var CollectBinarySizesPath string
+
+func main() {
+	var err error
+
+	flag.Parse()
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "error: missing builds directory\n")
+		os.Exit(2)
+	}
+	buildsDirectoryPath := flag.Arg(0)
+	workerCount := 8
+
+	CollectBinarySizesPath, err = filepath.Abs("./tools/build-sizes/collect-binary-sizes")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pathsChannel := make(chan string, 1024)
+
+	var workersFinished sync.WaitGroup
+
+	for i := 0; i < workerCount; i++ {
+		workersFinished.Add(1)
+		go func() {
+			defer workersFinished.Done()
+			RunWorker(pathsChannel)
+		}()
+	}
+
+	if err := FindBuildDirectories(buildsDirectoryPath, pathsChannel); err != nil {
+		log.Fatal(err)
+	}
+	close(pathsChannel)
+
+	workersFinished.Wait()
+}
+
+func RunWorker(pathsChannel chan string) {
+	for buildPath := range pathsChannel {
+		err := CollectBuildSizeIfNeeded(buildPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error processing %s: %v\n", buildPath, err)
+			// Continue.
+		}
+	}
+}
+
+func CollectBuildSizeIfNeeded(buildPath string) error {
+	buildSizesJSONFilePath := path.Join(buildPath, "build-sizes.json")
+	buildSizesJSONFileStat, err := os.Stat(buildSizesJSONFilePath)
+	if err == nil {
+		if buildSizesJSONFileStat.Size() != 0 {
+			// Build already processed. Skip.
+			return nil
+		}
+	} else if errors.Is(err, os.ErrNotExist) {
+		// Continue.
+	} else {
+		return err
+	}
+
+	fmt.Printf("Processing %s ...\n", buildPath)
+
+	process := exec.Command(CollectBinarySizesPath, ".")
+	process.Dir = buildPath
+	var buildSizesJSON bytes.Buffer
+	process.Stdout = &buildSizesJSON
+	process.Stderr = os.Stderr
+	if err := process.Start(); err != nil {
+		return err
+	}
+	if err := process.Wait(); err != nil {
+		return err
+	}
+	buildSizesJSONSize := int64(buildSizesJSON.Len())
+
+	buildSizesJSONFile, err := os.Create(buildSizesJSONFilePath)
+	if err != nil {
+		return err
+	}
+	defer buildSizesJSONFile.Close()
+
+	bytesWritten, err := io.Copy(buildSizesJSONFile, &buildSizesJSON)
+	if err != nil {
+		return err
+	}
+	if bytesWritten != buildSizesJSONSize {
+		return fmt.Errorf("failed to write entire JSON file: %s", buildSizesJSONFilePath)
+	}
+
+	return nil
+}
+
+func FindBuildDirectories(buildsDirectoryPath string, outputPaths chan string) error {
+	buildsDirectory, err := os.Open(buildsDirectoryPath)
+	if err != nil {
+		return err
+	}
+	defer buildsDirectory.Close()
+
+	for {
+		names, err := buildsDirectory.Readdirnames(64)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return err
+		}
+		for _, name := range names {
+			outputPaths <- path.Join(buildsDirectoryPath, name)
+		}
+	}
+	return nil
+}
+
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew "strager" Glazar
+//
+// This file is part of quick-lint-js.
+//
+// quick-lint-js is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// quick-lint-js is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
