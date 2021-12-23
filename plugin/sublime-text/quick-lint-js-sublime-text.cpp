26fb7ff342d9b3b3b0745537dfaaa6dc1bc58fd9
Cahian Freire <cahianfreire@gmail.com>
2021-12-23 17:20:19 -0300

    wip

--- plugin/sublime-text/quick-lint-js-sublime-text.cpp
+++ plugin/sublime-text/quick-lint-js-sublime-text.cpp
@@ -18,18 +18,14 @@ using sublime_text_document =
 
 struct qljs_st_parser final : public quick_lint_js::sublime_text_document {
  public:
-#if QLJS_ST_PLUGIN_VERSION == 3
   void set_text(quick_lint_js::string8_view replacement) {
     this->document_.set_text(replacement);
   }
-#else
+
   void replace_text(quick_lint_js::lsp_range range,
                     quick_lint_js::string8_view replacement) {
-    using namespace quick_lint_js;
-
     this->document_.replace_text(range, replacement);
   }
-#endif
 };
 
 qljs_st_parser* qljs_st_parser_new(void) { return new qljs_st_parser(); }
@@ -39,14 +35,7 @@ void qljs_st_parser_delete(qljs_st_parser* parser) { delete parser; }
 void qljs_st_parser_set_text(qljs_st_parser* parser, const qljs_st_text* text) {
   auto content8 = reinterpret_cast<const quick_lint_js::char8*>(text->content);
   auto replacement8 = quick_lint_js::string8_view(content, text->length);
-#if QLJS_ST_PLUGIN_VERSION == 3
-  parser->set_text(replacement);
-#else
-  auto lstart = lsp_position{.line = 0, .character = 0};
-  auto lend = lsp_position{.line = 0, .character = 0};
-  auto lrange = new lsp_range{.start = lstart, .end = lend};
-  parser->replace_text(lrange, replacement8);
-#endif
+  parser->set_text(replacement8);
 }
 
 void qljs_st_parser_replace_text(qljs_st_parser* parser,
@@ -60,43 +49,6 @@ void qljs_st_parser_replace_text(qljs_st_parser* parser,
 
 const qljs_st_diagnostic* qljs_st_parser_lint(qljs_st_parser* parser) {
   return parser->lint();
-}
-
-const qljs_st_3_result* qljs_st_3_lint(qljs_st_3_parser* p) {
-  return new qljs_st_3_result{.value = {.diagnostics = p->lint()},
-                              .is_diagnostics = true};
-}
-
-struct qljs_st_4_parser final
-    : public quick_lint_js::qljs_document_base<
-          quick_lint_js::lsp_locator,
-          quick_lint_js::c_api_error_reporter<qljs_st_4_diagnostic,
-                                              quick_lint_js::lsp_locator>> {
- public:
-};
-
-qljs_st_4_parser* qljs_st_4_create_parser(void) {
-  qljs_st_4_parser* p = new qljs_st_4_parser();
-  return p;
-}
-
-void qljs_st_4_destroy_parser(qljs_st_4_parser* p) { delete p; }
-
-const qljs_st_4_error* qljs_st_4_replace_text(
-    qljs_st_4_parser* p, int start_line, int start_character, int end_line,
-    int end_character, const void* replacement_text_utf_8,
-    size_t replacement_text_byte_count) {
-  p->replace_text(
-      start_line, start_character, end_line, end_character,
-      quick_lint_js::string8_view(
-          reinterpret_cast<const quick_lint_js::char8*>(replacement_text_utf_8),
-          replacement_text_byte_count));
-  return nullptr;
-}
-
-const qljs_st_4_result* qljs_st_4_lint(qljs_st_4_parser* p) {
-  return new qljs_st_4_result{.value = {.diagnostics = p->lint()},
-                              .is_diagnostics = true};
 }
 
 // quick-lint-js finds bugs in JavaScript programs.
