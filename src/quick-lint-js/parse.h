3bf4dd0ffd7d278cf56cb9ed9d35a9a82e0d2d41
Himanshu <himansingh24@gmail.com>
2021-12-29 13:21:29 +0530

    Don't report missing semicolon error

--- src/quick-lint-js/parse.h
+++ src/quick-lint-js/parse.h
@@ -2810,10 +2810,15 @@ class parser {
       this->skip();
       const char8 *end_of_else = this->lexer_.end_of_previous_token();
       bool has_left_paren = this->peek().type == token_type::left_paren;
-      parse_and_visit_body();
+      if (has_left_paren) {
+        this->parse_and_visit_expression(v);
+      } else {
+        parse_and_visit_body();
+      }
       bool has_left_curly = this->peek().type == token_type::left_curly;
       if (this->peek().has_leading_newline == false && has_left_paren &&
           has_left_curly) {
+        // if (cond) {} else (cond) {} // Invalid
         this->error_reporter_->report(error_missing_if_after_else{
             .expected_if = source_code_span(end_of_else, end_of_else),
         });
