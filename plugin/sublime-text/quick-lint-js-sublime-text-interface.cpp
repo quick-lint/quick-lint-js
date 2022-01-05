0d434757aed09d11ec24897c14e2646c2daef02e
Cahian Freire <cahianfreire@gmail.com>
2022-01-05 14:19:29 -0300

    wip

--- plugin/sublime-text/quick-lint-js-sublime-text-interface.cpp
+++ plugin/sublime-text/quick-lint-js-sublime-text-interface.cpp
@@ -107,10 +107,10 @@ void qljs_sublime_text_document_set_text(qljs_sublime_text_document *document,
 void qljs_sublime_text_document_replace_text(qljs_sublime_text_document *document,
                                              const qljs_sublime_text_range *range,
                                              const qljs_sublime_text_text *text) {
-  auto range_ = reinterpret_cast<qljs_sublime_text_document::range_type *>(range);
+  auto cpp_range = reinterpret_cast<qljs_sublime_text_document::range_type *>(range);
   auto content = reinterpret_cast<const quick_lint_js::char8 *>(text->content);
   auto replacement = quick_lint_js::string8_view(content8, text->length);
-  document->replace_text(*range_, replacement);
+  document->replace_text(*cpp_range, replacement);
 }
 #endif
 
