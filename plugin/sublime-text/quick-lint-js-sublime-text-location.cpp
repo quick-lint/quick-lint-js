32234c2d53dec9f5d4ed775fc645213c1fbc111a
Cahian Freire <cahianfreire@gmail.com>
2022-01-04 22:30:40 -0300

    wip

--- plugin/sublime-text/quick-lint-js-sublime-text-location.cpp
+++ plugin/sublime-text/quick-lint-js-sublime-text-location.cpp
@@ -15,25 +15,6 @@
 #include <quick-lint-js/utf-8.h>
 
 namespace quick_lint_js {
-namespace {
-// Like std::transform with an std::back_insert_iterator, but more efficient for
-// std::vector<int>.
-template <class InputIt, class Output, class Transformer>
-void insert_back_transform(InputIt input_begin,
-                           InputIt input_end,
-                           Output &output,
-                           Transformer &&transformer) {
-  using difference_type = typename Output::difference_type;
-  std::size_t original_size = output.size();
-  std::size_t input_size = narrow_cast<std::size_t>(input_end - input_begin);
-  std::size_t final_size = original_size + input_size;
-  output.resize(final_size);
-  auto output_it = output.begin() + narrow_cast<difference_type>(original_size);
-  output_it = std::transform(input_begin, input_end, output_it, transformer);
-  QLJS_ASSERT(output_it == output.end());
-}
-} // namespace
-
 sublime_text_locator::sublime_text_locator(padded_string_view input) noexcept
     : input_(input) {
   this->cache_offsets_of_lines();
--- plugin/sublime-text/quick-lint-js-sublime-text-utils.cpp
+++ plugin/sublime-text/quick-lint-js-sublime-text-utils.cpp
@@ -4,25 +4,36 @@
 //==============================================================================
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
-//                             sublime text have                              //
+//                             sublime text utils                             //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
-#ifndef QUICK_LINT_JS_SUBLIME_TEXT_HAVE_H
-#define QUICK_LINT_JS_SUBLIME_TEXT_HAVE_H
+#ifndef QUICK_LINT_JS_SUBLIME_TEXT_UTILS_H
+#define QUICK_LINT_JS_SUBLIME_TEXT_UTILS_H
 
-#if QUICK_LINT_JS_SUBLIME_TEXT_VERSION == 3
-#define QLJS_ST_HAVE_INCREMENTAL_CHANGES 0
-#elif QUICK_LINT_JS_SUBLIME_TEXT_VERSION > 3
-#define QLJS_ST_HAVE_INCREMENTAL_CHANGES 1
-#else
-#error "Unsupported Sublime Text version"
-#endif
+namespace quick_lint_js {
+// Like std::transform with an std::back_insert_iterator, but more efficient for
+// std::vector<int>.
+template <class InputIt, class Output, class Transformer>
+void insert_back_transform(InputIt input_begin,
+                           InputIt input_end,
+                           Output &output,
+                           Transformer &&transformer) {
+  using difference_type = typename Output::difference_type;
+  std::size_t original_size = output.size();
+  std::size_t input_size = narrow_cast<std::size_t>(input_end - input_begin);
+  std::size_t final_size = original_size + input_size;
+  output.resize(final_size);
+  auto output_it = output.begin() + narrow_cast<difference_type>(original_size);
+  output_it = std::transform(input_begin, input_end, output_it, transformer);
+  QLJS_ASSERT(output_it == output.end());
+}
 
-#endif // QUICK_LINT_JS_SUBLIME_TEXT_HAVE_H
+} // namespace quick_lint_js
+#endif // QUICK_LINT_JS_SUBLIME_TEXT_LOCATION_H
 
 // quick-lint-js finds bugs in JavaScript programs.
-// Copyright (C) 2020  Matthew Glazar
+// Copyright (C) 2020  Matthew "strager" Glazar
 //
 // This file is part of quick-lint-js.
 //
