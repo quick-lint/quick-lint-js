quick-lint/quick-lint-js

commit b8e0520ba0033d0d59b1fdb36ff4f339768bb0f9

Refactor: unsynchronized_pool_resource -> linked_bump_allocator
parser::buffering_visitor_memory_'s usage pattern is stack-like. Take
advantage of this by using linked_bump_allocator instead of
unsynchronized_pool_resource.

I did not test this commit's impact on time or memory usage.

This commit should not change behavior.

src/quick-lint-js/parse-statement-inl.h
@@ -686,6 +686,8 @@ void parser::parse_and_visit_export(Visitor &v) {
     // export {a as default, b};
     // export {a, b, c} from "module";
   case token_type::left_curly: {
+    auto exports_visitor_rewind_guard =
+        this->buffering_visitor_memory_.make_rewind_guard();
     buffering_visitor exports_visitor(&this->buffering_visitor_memory_);
     bump_vector<token, monotonic_allocator> exported_bad_tokens(
         "parse_and_visit_export exported_bad_tokens", &this->temporary_memory_);
@@ -1938,6 +1940,8 @@ void parser::parse_and_visit_for(Visitor &v) {
 
     lexer_transaction transaction = this->lexer_.begin_transaction();
     this->skip();
+    auto lhs_visitor_rewind_guard =
+        this->buffering_visitor_memory_.make_rewind_guard();
     buffering_visitor lhs(&this->buffering_visitor_memory_);
     if (declaring_token.type == token_type::kw_let &&
         this->is_let_token_a_variable_reference(this->peek(),

src/quick-lint-js/parse.h
@@ -558,9 +558,7 @@ class parser {
   // Memory used for temporary memory allocations (e.g. vectors on the stack).
   monotonic_allocator temporary_memory_;
 
-  // TODO(strager): unsynchronized_pool_resource is overkill. Pick a better
-  // allocator.
-  boost::container::pmr::unsynchronized_pool_resource buffering_visitor_memory_;
+  linked_bump_allocator<alignof(void *)> buffering_visitor_memory_;
 
   bool in_top_level_ = true;
   bool in_async_function_ = false;

commit 48376bd84e2d2bc819efd7e0258195b3f0d9ccea

Fix use-after-free bug when parsing for loops
Git commit b8e0520ba0033d0d59b1fdb36ff4f339768bb0f9 introduced a bug
where the buffering_visitor_memory_ linked_bump_allocator was rewound
prematurely, leading to a use-after-free when parsing code like the
following:

    for (var f = () => {
      for (var xs = [x, x, x, x, x, x, x, x, x, x, x, x, x, x];;) {}
    };;) {}

Fix the bug by avoiding rewinding and linked_bump_allocator entirely,
allocating on the heap instead.

I did not measure the performance or memory usage impact of this change.

src/quick-lint-js/parse-statement-inl.h
@@ -4,6 +4,7 @@
 #ifndef QUICK_LINT_JS_PARSE_STATEMENT_INL_H
 #define QUICK_LINT_JS_PARSE_STATEMENT_INL_H
 
+#include <boost/container/pmr/memory_resource.hpp>
 #include <cstdlib>
 #include <optional>
 #include <quick-lint-js/assert.h>
@@ -686,9 +687,8 @@ void parser::parse_and_visit_export(Visitor &v) {
     // export {a as default, b};
     // export {a, b, c} from "module";
   case token_type::left_curly: {
-    auto exports_visitor_rewind_guard =
-        this->buffering_visitor_memory_.make_rewind_guard();
-    buffering_visitor exports_visitor(&this->buffering_visitor_memory_);
+    buffering_visitor &exports_visitor = this->buffering_visitor_stack_.emplace(
+        boost::container::pmr::new_delete_resource());
     bump_vector<token, monotonic_allocator> exported_bad_tokens(
         "parse_and_visit_export exported_bad_tokens", &this->temporary_memory_);
     this->parse_and_visit_named_exports_for_export(
@@ -723,6 +723,10 @@ void parser::parse_and_visit_export(Visitor &v) {
       }
       exports_visitor.move_into(v);
     }
+
+    QLJS_ASSERT(&this->buffering_visitor_stack_.top() == &exports_visitor);
+    this->buffering_visitor_stack_.pop();
+
     this->consume_semicolon();
     break;
   }
@@ -1940,9 +1944,8 @@ void parser::parse_and_visit_for(Visitor &v) {
 
     lexer_transaction transaction = this->lexer_.begin_transaction();
     this->skip();
-    auto lhs_visitor_rewind_guard =
-        this->buffering_visitor_memory_.make_rewind_guard();
-    buffering_visitor lhs(&this->buffering_visitor_memory_);
+    buffering_visitor &lhs = this->buffering_visitor_stack_.emplace(
+        boost::container::pmr::new_delete_resource());
     if (declaring_token.type == token_type::kw_let &&
         this->is_let_token_a_variable_reference(this->peek(),
                                                 /*allow_declarations=*/true)) {
@@ -2046,6 +2049,10 @@ void parser::parse_and_visit_for(Visitor &v) {
       QLJS_PARSER_UNIMPLEMENTED();
       break;
     }
+
+    QLJS_ASSERT(&this->buffering_visitor_stack_.top() == &lhs);
+    this->buffering_visitor_stack_.pop();
+
     break;
   }
 

src/quick-lint-js/parse.h
@@ -21,6 +21,7 @@
 #include <quick-lint-js/parse-visitor.h>
 #include <quick-lint-js/token.h>
 #include <quick-lint-js/warning.h>
+#include <stack>
 #include <unordered_map>
 #include <utility>
 
@@ -555,7 +556,9 @@ class parser {
   // Memory used for strings in error messages.
   monotonic_allocator error_memory_;
 
-  linked_bump_allocator<alignof(void *)> buffering_visitor_memory_;
+  // These are stored in a stack here (rather than on the C++ stack via local
+  // variables) so that memory can be released in case we call setjmp.
+  std::stack<buffering_visitor> buffering_visitor_stack_;
 
   bool in_top_level_ = true;
   bool in_async_function_ = false;

test/test-parse-loop.cpp
@@ -1339,6 +1339,28 @@ TEST(test_parse,
           &code, error_cannot_assign_to_variable_named_async_in_for_of_loop,  //
           async_identifier, strlen(u8"for ("), u8"async")));
 }
+
+TEST(test_parse, for_loop_in_for_loop_header_crash) {
+  // There used to be a use-after-free bug caused by a buffering_visitor copying
+  // memory into another buffering_visitor, then the parser's
+  // buffering_visitor_memory_ being rewind-ed. This test makes sure a
+  // regression doesn't happen again (assuming Address Sanitizer catches the
+  // use-after-free).
+  padded_string code(
+      u8R"(
+        for (var f = () => {
+          for (var xs = [x, x, x, x, x, x, x, x, x, x, x, x, x, x];;) {}
+        };;) {}
+      )"_sv);
+  spy_visitor v;
+  parser p(&code, &v);
+  EXPECT_TRUE(p.parse_and_visit_statement(v));
+
+  EXPECT_THAT(v.variable_uses, Not(IsEmpty()));
+  EXPECT_THAT(v.variable_uses,
+              ::testing::Each(spy_visitor::visited_variable_use{u8"x"}));
+  EXPECT_THAT(v.errors, IsEmpty());
+}
 }
 }
 