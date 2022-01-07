88d779f6332204b47cbf22b30e78daa8bdcf1bf7
Joey Meisner <emptygrocerybag@hotmail.com>
2022-01-07 02:08:59 -0500

    Add license to `quick-lint-js/windows.h`

--- src/quick-lint-js/windows.h
+++ src/quick-lint-js/windows.h
@@ -1,3 +1,6 @@
+// Copyright (C) 2020  Matthew "strager" Glazar
+// See end of file for extended copyright information.
+
 #ifndef QUICK_LINT_JS_WINDOWS_H
 #define QUICK_LINT_JS_WINDOWS_H
 
@@ -49,3 +52,21 @@
 #include <Windows.h>
 
 #endif
+
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew "strager" Glazar
+//
+// This file is part of quick-lint-js.
+//
+// quick-lint-js is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// quick-lint-js is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
