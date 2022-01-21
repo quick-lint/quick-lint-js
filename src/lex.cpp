305c9eb835e399d8f6ef7abdd45e010d5f5dae3c
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-01-20 23:00:51 -0800

    Refactor: improve formatting of some case acros

--- src/lex.cpp
+++ src/lex.cpp
@@ -29,72 +29,24 @@
 #include <nmmintrin.h>
 #endif
 
-#define QLJS_CASE_IDENTIFIER_START \
-  case '\\':                       \
-  case '$':                        \
-  case '_':                        \
-  case 'A':                        \
-  case 'B':                        \
-  case 'C':                        \
-  case 'D':                        \
-  case 'E':                        \
-  case 'F':                        \
-  case 'G':                        \
-  case 'H':                        \
-  case 'I':                        \
-  case 'J':                        \
-  case 'K':                        \
-  case 'L':                        \
-  case 'M':                        \
-  case 'N':                        \
-  case 'O':                        \
-  case 'P':                        \
-  case 'Q':                        \
-  case 'R':                        \
-  case 'S':                        \
-  case 'T':                        \
-  case 'U':                        \
-  case 'V':                        \
-  case 'W':                        \
-  case 'X':                        \
-  case 'Y':                        \
-  case 'Z':                        \
-  case 'a':                        \
-  case 'b':                        \
-  case 'c':                        \
-  case 'd':                        \
-  case 'e':                        \
-  case 'f':                        \
-  case 'g':                        \
-  case 'h':                        \
-  case 'i':                        \
-  case 'j':                        \
-  case 'k':                        \
-  case 'l':                        \
-  case 'm':                        \
-  case 'n':                        \
-  case 'o':                        \
-  case 'p':                        \
-  case 'q':                        \
-  case 'r':                        \
-  case 's':                        \
-  case 't':                        \
-  case 'u':                        \
-  case 'v':                        \
-  case 'w':                        \
-  case 'x':                        \
-  case 'y':                        \
-  case 'z'
+// clang-format off
+#define QLJS_CASE_IDENTIFIER_START                                       \
+  case '\\': case '$': case '_':                                         \
+  case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G':  \
+  case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':  \
+  case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':  \
+  case 'V': case 'W': case 'X': case 'Y': case 'Z':                      \
+  case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':  \
+  case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':  \
+  case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':  \
+  case 'v': case 'w': case 'x': case 'y': case 'z'
+// clang-format on
 
+// clang-format off
 #define QLJS_CASE_OCTAL_DIGIT \
-  case '0':                   \
-  case '1':                   \
-  case '2':                   \
-  case '3':                   \
-  case '4':                   \
-  case '5':                   \
-  case '6':                   \
-  case '7'
+  case '0': case '1': case '2': case '3': \
+  case '4': case '5': case '6': case '7'
+// clang-format on
 
 #define QLJS_CASE_DECIMAL_DIGIT \
   QLJS_CASE_OCTAL_DIGIT:        \
