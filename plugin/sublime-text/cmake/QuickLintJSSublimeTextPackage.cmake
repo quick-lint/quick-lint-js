ee2c1f5678dddf11b057ce6ef2cd60d61500b933
Cahian Freire <cahianfreire@gmail.com>
2022-02-05 18:43:33 -0300

    wip

--- plugin/sublime-text/cmake/QuickLintJSSublimeTextPackage.cmake
+++ plugin/sublime-text/cmake/QuickLintJSSublimeTextPackage.cmake
@@ -54,49 +54,14 @@ endfunction ()
 
 function (quick_lint_js_sublime_text_get_package_files PACKAGE_FILES)
   set(
-    SUBLIME_TEXT_PACKAGE_FILES
+    PACKAGE_FILES
     $<TARGET_FILE:quick-lint-js-lib>
     $<TARGET_RUNTIME_DLLS:quick-lint-js-lib>
     "${CMAKE_CURRENT_SOURCE_DIR}/.no-sublime-package"
-    "${CMAKE_CURRENT_SOURCE_DIR}/interface.py"
     "${CMAKE_CURRENT_SOURCE_DIR}/extension.py"
     "${CMAKE_CURRENT_SOURCE_DIR}/interface.py"
   )
 endfunction ()
-
-# if(NOT CMAKE_PROPERTY_LIST)
-#     execute_process(COMMAND cmake --help-property-list OUTPUT_VARIABLE CMAKE_PROPERTY_LIST)
-#     
-#     # Convert command output into a CMake list
-#     string(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
-#     string(REGEX REPLACE "\n" ";" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
-# endif()
-#     
-# function(print_properties)
-#     message("CMAKE_PROPERTY_LIST = ${CMAKE_PROPERTY_LIST}")
-# endfunction()
-#     
-# function(print_target_properties target)
-#     if(NOT TARGET ${target})
-#       message(STATUS "There is no target named '${target}'")
-#       return()
-#     endif()
-# 
-#     foreach(property ${CMAKE_PROPERTY_LIST})
-#         string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" property ${property})
-# 
-#         # Fix https://stackoverflow.com/questions/32197663/how-can-i-remove-the-the-location-property-may-not-be-read-from-target-error-i
-#         if(property STREQUAL "LOCATION" OR property MATCHES "^LOCATION_" OR property MATCHES "_LOCATION$")
-#             continue()
-#         endif()
-# 
-#         get_property(was_set TARGET ${target} PROPERTY ${property} SET)
-#         if(was_set)
-#             get_target_property(value ${target} ${property})
-#             message("${target} ${property} = ${value}")
-#         endif()
-#     endforeach()
-# endfunction()
 
 # quick-lint-js finds bugs in JavaScript programs.
 # Copyright (C) 2020  Matthew "strager" Glazar
