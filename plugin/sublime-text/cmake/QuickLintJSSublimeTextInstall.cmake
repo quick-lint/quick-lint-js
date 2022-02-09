201e6e78dd1aecd793852100aed1ad6ab88eb9ad
Cahian Freire <cahianfreire@gmail.com>
2022-02-09 19:14:44 -0300

    wip

--- plugin/sublime-text/cmake/QuickLintJSSublimeTextInstall.cmake
+++ plugin/sublime-text/cmake/QuickLintJSSublimeTextInstall.cmake
@@ -1,3 +1,6 @@
+list(APPEND CMAKE_MODULE_PATH ".")
+include(QuickLintJSSublimeTextPackage)
+
 quick_lint_js_sublime_text_get_package_pathname(SUBLIME_TEXT_PACKAGE_PATHNAME)
 quick_lint_js_sublime_text_get_package_files(SUBLIME_TEXT_PACKAGE_FILES)
 quick_lint_js_sublime_text_get_package_destination(SUBLIME_TEXT_PACKAGE_DESTINATION)
@@ -5,8 +8,14 @@ quick_lint_js_sublime_text_get_package_destination(SUBLIME_TEXT_PACKAGE_DESTINAT
 execute_process(
   COMMAND "${CMAKE_COMMAND}" -E
   make_directory "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
+  VERBATIM
+)
+execute_process(
   COMMAND "${CMAKE_COMMAND}" -E
   copy ${SUBLIME_TEXT_PACKAGE_FILES} "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
+  VERBATIM
+)
+execute_process(
   COMMAND "${CMAKE_COMMAND}" -E
   copy "${SUBLIME_TEXT_PACKAGE_PATHNAME}" "${SUBLIME_TEXT_PACKAGE_DESTINATION}"
   VERBATIM
