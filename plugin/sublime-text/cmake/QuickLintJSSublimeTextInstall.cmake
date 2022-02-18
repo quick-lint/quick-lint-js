760db5b3b1efa631764d2e86785c9db62325cbbb
Cahian Freire <cahianfreire@gmail.com>
2022-02-18 16:51:16 -0300

    wip

--- plugin/sublime-text/cmake/QuickLintJSSublimeTextInstall.cmake
+++ plugin/sublime-text/cmake/QuickLintJSSublimeTextInstall.cmake
@@ -2,28 +2,41 @@
 # See end of file for extended copyright information.
 
 # list(APPEND CMAKE_MODULE_PATH ".")
-# list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}")
 include(QuickLintJSSublimeTextPackage)
 
 quick_lint_js_sublime_text_get_package_pathname(SUBLIME_TEXT_PACKAGE_PATHNAME)
 quick_lint_js_sublime_text_get_package_files(SUBLIME_TEXT_PACKAGE_FILES)
 quick_lint_js_sublime_text_get_package_destination(SUBLIME_TEXT_PACKAGE_DESTINATION)
 
+# execute_process(
+#   COMMAND "${CMAKE_COMMAND}" -E
+#   make_directory "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
+# )
+# execute_process(
+#   COMMAND "${CMAKE_COMMAND}" -E
+#   copy ${SUBLIME_TEXT_PACKAGE_FILES} "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
+# )
+# execute_process(
+#   COMMAND "${CMAKE_COMMAND}" -E
+#   copy "${SUBLIME_TEXT_PACKAGE_PATHNAME}" "${SUBLIME_TEXT_PACKAGE_DESTINATION}"
+# )
+
 execute_process(
   COMMAND "${CMAKE_COMMAND}" -E
   make_directory "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
-  VERBATIM
 )
 execute_process(
   COMMAND "${CMAKE_COMMAND}" -E
   copy ${SUBLIME_TEXT_PACKAGE_FILES} "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
-  VERBATIM
 )
+
+###
 execute_process(
   COMMAND "${CMAKE_COMMAND}" -E
-  copy "${SUBLIME_TEXT_PACKAGE_PATHNAME}" "${SUBLIME_TEXT_PACKAGE_DESTINATION}"
-  VERBATIM
+  make_directory "${SUBLIME_TEXT_PACKAGE_DESTINATION}"
 )
+###
 
 # quick-lint-js finds bugs in JavaScript programs.
 # Copyright (C) 2020  Matthew "strager" Glazar
