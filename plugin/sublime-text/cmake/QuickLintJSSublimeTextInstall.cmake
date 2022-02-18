6a5543e42054d1f056ca3338492543f6f4ea9efd
Cahian Freire <cahianfreire@gmail.com>
2022-02-18 16:59:19 -0300

    wip

--- plugin/sublime-text/cmake/QuickLintJSSublimeTextInstall.cmake
+++ plugin/sublime-text/cmake/QuickLintJSSublimeTextInstall.cmake
@@ -9,32 +9,22 @@ quick_lint_js_sublime_text_get_package_pathname(SUBLIME_TEXT_PACKAGE_PATHNAME)
 quick_lint_js_sublime_text_get_package_files(SUBLIME_TEXT_PACKAGE_FILES)
 quick_lint_js_sublime_text_get_package_destination(SUBLIME_TEXT_PACKAGE_DESTINATION)
 
-# execute_process(
-#   COMMAND "${CMAKE_COMMAND}" -E
-#   make_directory "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
-# )
-# execute_process(
-#   COMMAND "${CMAKE_COMMAND}" -E
-#   copy ${SUBLIME_TEXT_PACKAGE_FILES} "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
-# )
-# execute_process(
-#   COMMAND "${CMAKE_COMMAND}" -E
-#   copy "${SUBLIME_TEXT_PACKAGE_PATHNAME}" "${SUBLIME_TEXT_PACKAGE_DESTINATION}"
-# )
-
-execute_process(
-  COMMAND "${CMAKE_COMMAND}" -E
-  make_directory "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
-)
-execute_process(
-  COMMAND "${CMAKE_COMMAND}" -E
-  copy ${SUBLIME_TEXT_PACKAGE_FILES} "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
-)
-
 ###
 execute_process(
-  COMMAND "${CMAKE_COMMAND}" -E
-  make_directory "${SUBLIME_TEXT_PACKAGE_DESTINATION}"
+  COMMAND
+  "${CMAKE_COMMAND}" -E make_directory "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
+)
+execute_process(
+  COMMAND
+  "${CMAKE_COMMAND}" -E copy ${SUBLIME_TEXT_PACKAGE_FILES} "${SUBLIME_TEXT_PACKAGE_PATHNAME}"
+)
+execute_process(
+  COMMAND
+  "${CMAKE_COMMAND}" -E make_directory "${SUBLIME_TEXT_PACKAGE_DESTINATION}"
+)
+execute_process(
+  COMMAND
+  "${CMAKE_COMMAND}" -E copy_directory "${SUBLIME_TEXT_PACKAGE_PATHNAME}" "${SUBLIME_TEXT_PACKAGE_DESTINATION}"
 )
 ###
 
