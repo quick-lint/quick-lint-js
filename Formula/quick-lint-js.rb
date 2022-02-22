c059982436d40ffda99f22338507dd1f04393289
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-02-21 21:41:27 -0800

    Homebrew: push upstream and relicense
    quick-lint-js is now in Homebrew proper:
    https://github.com/Homebrew/homebrew-core/pull/95648
    
    * Remove references to our tap in our installation instructions
    * Relicense as BSD-2 and give copyright to "Homebrew contributors" to
      make sharing with Homebrew core easier

--- Formula/quick-lint-js.rb
+++ Formula/quick-lint-js.rb
@@ -1,5 +1,5 @@
-# Copyright (C) 2020  Matthew "strager" Glazar
-# See end of file for extended copyright information.
+# Copyright (c) 2009-present, Homebrew contributors
+# See the LICENSE file for extended copyright information.
 
 class QuickLintJs < Formula
   desc "Find bugs in your JavaScript code"
@@ -59,21 +59,3 @@ class QuickLintJs < Formula
     assert_empty shell_output("#{bin}/quick-lint-js no-errors.js")
   end
 end
-
-# quick-lint-js finds bugs in JavaScript programs.
-# Copyright (C) 2020  Matthew "strager" Glazar
-#
-# This file is part of quick-lint-js.
-#
-# quick-lint-js is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# quick-lint-js is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
