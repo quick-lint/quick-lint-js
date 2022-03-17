fd21027eb8864526784e02eb49b38175b9190d2b
Lee Wannacott <wannacottL@gmail.com>
2022-03-17 21:16:57 +1300

    Add more data from GitHub tags api to the struct including sha for commitish.

--- docs/quick-release-notes/main.go
+++ docs/quick-release-notes/main.go
@@ -16,6 +16,11 @@ type Tag struct {
 	Name       string `json:"name"`
 	ZipballURL string `json:"zipball_url"`
 	TarballURL string `json:"tarball_url"`
+	Commit     struct {
+		SHA string `json:"sha"`
+		URL string `json:"url"`
+	} `json:"commit"`
+	NodeID string `json:"node_id"`
 }
 
 func getTagsFromAPI(owner string, repo string) []Tag {
@@ -94,9 +99,10 @@ func makeReleaseSlice(versionLineNumbers []int, changeLogText []string, changeLo
 func sendToGitHubAPI(tagForRelease Tag, releaseNote string, versionTitle string, owner string, repo string) {
 	// https://docs.github.com/en/rest/reference/releases
 	postBody, _ := json.Marshal(map[string]string{
-		"tag_name": tagForRelease.Name,
-		"name":     versionTitle,
-		"body":     releaseNote,
+		"tag_name":         tagForRelease.Name,
+		"name":             versionTitle,
+		"body":             releaseNote,
+		"target_commitish": tagForRelease.Commit.SHA,
 	})
 	responseBody := bytes.NewBuffer(postBody)
 	url := fmt.Sprintf("https://api.github.com/repos/%v/%v/releases", owner, repo)
