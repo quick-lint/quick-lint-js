b3afccf285e957c0317f6cc7571b0ea6f4497e5c
Tony Sathre <tony@ghostbit.org>
2022-03-19 16:55:50 -0500

    added completions for powershell

---
+++ completions/quick-lint-js.ps1
@@ -0,0 +1,16 @@
+Register-ArgumentCompleter -CommandName quick-lint-js -Native -ScriptBlock {
+	param ($wordToComplete)
+
+    $opts = @('--help',
+              '--version',
+              '--lsp-server',
+              '--config-file=',
+              '--stdin',
+              '--exit-fail-on=',
+              '--output-format=',
+              '--diagnostic-hyperlinks=',
+              '--vim-file-bufnr='
+            )
+
+    $opts.Where({$_ | Select-String $wordToComplete})
+}
