4652a3d67adf9ab75e0a411a5c48f19df5802145
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-05-18 21:56:32 -0700

    feat(analyze-trace): check 'sync' events with --check-document-consistency
    Create a mode in analyze-trace which verifies that quick-lint-js
    correctly understands the document, using vscode_document_sync as the
    source of truth.

--- tools/analyze-trace.cpp
+++ tools/analyze-trace.cpp
@@ -7,12 +7,16 @@
 #include <quick-lint-js/arg-parser.h>
 #include <quick-lint-js/char8.h>
 #include <quick-lint-js/document.h>
+#include <quick-lint-js/file-path.h>
 #include <quick-lint-js/file.h>
 #include <quick-lint-js/lsp-location.h>
 #include <quick-lint-js/narrow-cast.h>
 #include <quick-lint-js/padded-string.h>
+#include <quick-lint-js/temporary-directory.h>
 #include <quick-lint-js/trace-stream-reader.h>
+#include <string>
 #include <string_view>
+#include <unordered_map>
 #include <vector>
 
 using namespace std::literals::string_view_literals;
@@ -22,7 +26,33 @@ namespace {
 struct analyze_options {
   std::vector<const char*> trace_files;
   std::optional<std::uint64_t> dump_final_document_content_document_id;
+  bool check_document_consistency = false;
 };
+
+lsp_range to_lsp_range(
+    const trace_stream_event_visitor::vscode_document_range& range) {
+  return lsp_range{
+      .start =
+          {
+              .line = narrow_cast<int>(range.start.line),
+              .character = narrow_cast<int>(range.start.character),
+          },
+      .end =
+          {
+              .line = narrow_cast<int>(range.end.line),
+              .character = narrow_cast<int>(range.end.character),
+          },
+  };
+}
+
+string8 to_utf8(std::u16string_view s) {
+  // TODO(strager): Support non-ASCII.
+  string8 result;
+  for (char16_t c : s) {
+    result += narrow_cast<char8>(c);
+  }
+  return result;
+}
 
 class document_content_dumper : public trace_stream_event_visitor {
  public:
@@ -50,7 +80,7 @@ class document_content_dumper : public trace_stream_event_visitor {
     if (event.document_id != this->document_id_) {
       return;
     }
-    this->doc_.set_text(this->to_utf8(event.content));
+    this->doc_.set_text(to_utf8(event.content));
   }
 
   void visit_vscode_document_closed_event(
@@ -68,31 +98,12 @@ class document_content_dumper : public trace_stream_event_visitor {
     }
 
     for (const auto& change : event.changes) {
-      this->doc_.replace_text(
-          lsp_range{
-              .start =
-                  {
-                      .line = narrow_cast<int>(change.range.start.line),
-                      .character =
-                          narrow_cast<int>(change.range.start.character),
-                  },
-              .end =
-                  {
-                      .line = narrow_cast<int>(change.range.end.line),
-                      .character = narrow_cast<int>(change.range.end.character),
-                  },
-          },
-          this->to_utf8(change.text));
+      this->doc_.replace_text(to_lsp_range(change.range), to_utf8(change.text));
     }
   }
 
   void visit_vscode_document_sync_event(
-      const vscode_document_sync_event& event) override {
-    if (event.document_id != this->document_id_) {
-      return;
-    }
-    // TODO(strager): Verify that this->doc_ matches event.content.
-  }
+      const vscode_document_sync_event&) override {}
 
   void print_document_content() {
     padded_string_view s = this->doc_.string();
@@ -100,17 +111,98 @@ class document_content_dumper : public trace_stream_event_visitor {
   }
 
  private:
-  string8 to_utf8(std::u16string_view s) {
-    // TODO(strager): Support non-ASCII.
-    string8 result;
-    for (char16_t c : s) {
-      result += narrow_cast<char8>(c);
-    }
-    return result;
-  }
-
   document<lsp_locator> doc_;
   std::uint64_t document_id_;
+};
+
+class document_content_checker : public trace_stream_event_visitor {
+ public:
+  void visit_error_invalid_magic() override {
+    std::fprintf(stderr, "error: invalid magic\n");
+  }
+
+  void visit_error_invalid_uuid() override {
+    std::fprintf(stderr, "error: invalid UUID\n");
+  }
+
+  void visit_error_unsupported_compression_mode(std::uint8_t mode) override {
+    std::fprintf(stderr, "error: unsupported compression mode: %#02x\n", mode);
+  }
+
+  void visit_packet_header(const packet_header&) override {}
+
+  void visit_init_event(const init_event&) override {}
+
+  void visit_vscode_document_opened_event(
+      const vscode_document_opened_event& event) override {
+    if (event.document_id == 0) {
+      return;
+    }
+    this->documents_[event.document_id].set_text(to_utf8(event.content));
+  }
+
+  void visit_vscode_document_closed_event(
+      const vscode_document_closed_event& event) override {
+    if (event.document_id == 0) {
+      return;
+    }
+    this->documents_.erase(event.document_id);
+  }
+
+  void visit_vscode_document_changed_event(
+      const vscode_document_changed_event& event) override {
+    if (event.document_id == 0) {
+      return;
+    }
+
+    auto doc_it = this->documents_.find(event.document_id);
+    if (doc_it == this->documents_.end()) {
+      std::fprintf(stderr,
+                   "warning: document %#llx changed but wasn't opened\n",
+                   narrow_cast<unsigned long long>(event.document_id));
+      return;
+    }
+    document<lsp_locator>& doc = doc_it->second;
+    for (const auto& change : event.changes) {
+      doc.replace_text(to_lsp_range(change.range), to_utf8(change.text));
+    }
+  }
+
+  void visit_vscode_document_sync_event(
+      const vscode_document_sync_event& event) override {
+    if (event.document_id == 0) {
+      return;
+    }
+
+    auto doc_it = this->documents_.find(event.document_id);
+    if (doc_it == this->documents_.end()) {
+      std::fprintf(stderr, "warning: document %#llx synced but wasn't opened\n",
+                   narrow_cast<unsigned long long>(event.document_id));
+      return;
+    }
+    document<lsp_locator>& doc = doc_it->second;
+
+    string8_view actual = doc.string().string_view();
+    string8 expected = to_utf8(event.content);
+    if (actual != expected) {
+      std::fprintf(stderr, "error: document mismatch\n");
+
+      std::string dir = make_temporary_directory();
+      std::string expected_path =
+          dir + QLJS_PREFERRED_PATH_DIRECTORY_SEPARATOR + "expected";
+      write_file_or_exit(expected_path, expected);
+      std::fprintf(stderr, "note: expected written to %s\n",
+                   expected_path.c_str());
+
+      std::string actual_path =
+          dir + QLJS_PREFERRED_PATH_DIRECTORY_SEPARATOR + "actual";
+      write_file_or_exit(actual_path, actual);
+      std::fprintf(stderr, "note: actual written to %s\n", actual_path.c_str());
+    }
+  }
+
+ private:
+  std::unordered_map<std::uint64_t, document<lsp_locator>> documents_;
 };
 
 class event_dumper : public trace_stream_event_visitor {
@@ -212,6 +304,9 @@ analyze_options parse_analyze_options(int argc, char** argv) {
   while (!parser.done()) {
     if (const char* argument = parser.match_argument()) {
       o.trace_files.push_back(argument);
+    } else if (parser.match_flag_option("--check-document-consistency"sv,
+                                        "--check"sv)) {
+      o.check_document_consistency = true;
     } else if (const char* arg_value = parser.match_option_with_value(
                    "--dump-final-document-content"sv)) {
       errno = 0;
@@ -254,12 +349,18 @@ int main(int argc, char** argv) {
     file.error().print_and_exit();
   }
 
+  if (o.check_document_consistency) {
+    document_content_checker checker;
+    read_trace_stream(file->data(), narrow_cast<std::size_t>(file->size()),
+                      checker);
+  }
+
   if (o.dump_final_document_content_document_id.has_value()) {
     document_content_dumper dumper(*o.dump_final_document_content_document_id);
     read_trace_stream(file->data(), narrow_cast<std::size_t>(file->size()),
                       dumper);
     dumper.print_document_content();
-  } else {
+  } else if (!o.check_document_consistency) {
     event_dumper dumper;
     read_trace_stream(file->data(), narrow_cast<std::size_t>(file->size()),
                       dumper);
