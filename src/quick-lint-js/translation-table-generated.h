21549c1c98f4d6ce317828dcd81ec166255f8da7
Asaduzzaman Pavel <info@codegenie.co>
2022-05-31 05:51:06 +0600

    run format

--- src/quick-lint-js/translation-table-generated.h
+++ src/quick-lint-js/translation-table-generated.h
@@ -4,12 +4,8 @@
 // Copyright (C) 2020  Matthew "strager" Glazar
 // See end of file for extended copyright information.
 
-
 #ifndef QUICK_LINT_JS_TRANSLATION_TABLE_GENERATED_H
 #define QUICK_LINT_JS_TRANSLATION_TABLE_GENERATED_H
-
-
-
 
 #include <cstddef>
 #include <cstdint>
