584094c46e967c018a982e740e926aae2747f4c8
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-06-14 20:51:56 -0700

    feat(fuzz): add fuzz test for parsing+linting

---
+++ fuzz/fuzz-parse-lint.cpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2020  Matthew "strager" Glazar
+// See end of file for extended copyright information.
+
+#include <cstddef>
+#include <cstdint>
+#include <quick-lint-js/char8.h>
+#include <quick-lint-js/diag-reporter.h>
+#include <quick-lint-js/lint.h>
+#include <quick-lint-js/parse.h>
+
+extern "C" {
+int LLVMFuzzerTestOneInput(const std::uint8_t *data, std::size_t size) {
+  quick_lint_js::padded_string source(quick_lint_js::string8(
+      reinterpret_cast<const quick_lint_js::char8 *>(data), size));
+  quick_lint_js::parser p(&source,
+                          &quick_lint_js::null_diag_reporter::instance);
+  quick_lint_js::linter l;
+  [[maybe_unused]] bool ok =
+      p.parse_and_visit_module_catching_fatal_parse_errors(l);
+
+  return 0;
+}
+}
+
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew "strager" Glazar
+//
+// This file is part of quick-lint-js.
+//
+// quick-lint-js is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// quick-lint-js is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
