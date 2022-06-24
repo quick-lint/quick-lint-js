d86a90a9b1f3355335f5ebd8a86565a7e3f0c9ab
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-06-23 20:55:56 -0700

    feat(typescript): parse optional methods in object types

--- test/test-parse-typescript-type.cpp
+++ test/test-parse-typescript-type.cpp
@@ -281,6 +281,15 @@ TEST(test_parse_typescript_type, object_type_with_optional_properties) {
     EXPECT_THAT(v.variable_uses,
                 ElementsAre(spy_visitor::visited_variable_use{u8"Type"}));
   }
+
+  {
+    spy_visitor v = parse_and_visit_typescript_type(u8"{ method?(): Type }"_sv);
+    EXPECT_THAT(v.visits, ElementsAre("visit_enter_function_scope",   // method
+                                      "visit_variable_type_use",      // Type
+                                      "visit_exit_function_scope"));  // method
+    EXPECT_THAT(v.variable_uses,
+                ElementsAre(spy_visitor::visited_variable_use{u8"Type"}));
+  }
 }
 
 TEST(test_parse_typescript_type, object_type_with_method) {
