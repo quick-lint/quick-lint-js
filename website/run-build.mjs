2bdd21613ecfaf95d7ad0a0ad352ecac786c5018
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-07-13 03:15:33 -0700

    perf(website): speed up 'yarn build' on multi-core machines
    Process multiple files in parallel. This causes esbuild loading+building
    (in demo pages) to overlap with quick-lint-js loading (in error docs
    pages) on multi-core machines, reducing wall times for 'yarn build'.
    
    On my MacBookPro (M1) machine:
    
    Before:                     24.176 seconds
    After but no promise-limit: 14.508 seconds
    After:                      11.903 seconds

--- website/run-build.mjs
+++ website/run-build.mjs
@@ -3,6 +3,7 @@
 
 import fs from "fs";
 import path from "path";
+import promiseLimit from "promise-limit";
 import { websiteConfig } from "./src/config.mjs";
 import {
   IndexConflictVFSError,
@@ -11,6 +12,9 @@ import {
   VFS,
   VFSDirectory,
 } from "./src/vfs.mjs";
+
+let maxConcurrentJobs = 8;
+let semaphore = promiseLimit(maxConcurrentJobs);
 
 async function mainAsync() {
   let { targetDirectory } = parseArguments(process.argv.slice(2));
@@ -25,22 +29,26 @@ async function mainAsync() {
 async function buildDirAsync(vfs, uri, outputDirectory) {
   let listing = await vfs.listDirectoryAsync(uri);
   await fs.promises.mkdir(outputDirectory, { recursive: true });
-  for (let childName of listing.names()) {
-    let child = listing.get(childName);
-    let childPath = path.join(
-      outputDirectory,
-      childName === "" ? "index.html" : childName
-    );
-    if (child instanceof VFSDirectory) {
-      await buildDirAsync(vfs, `${uri}${childName}/`, childPath);
-    } else if (child instanceof IndexConflictVFSError) {
-      throw child;
-    } else {
-      console.log(`generating ${childPath} ...`);
-      let data = await child.getContentsAsync();
-      await fs.promises.writeFile(childPath, data);
-    }
-  }
+  await Promise.all(
+    listing.names().map(async (childName) => {
+      let child = listing.get(childName);
+      let childPath = path.join(
+        outputDirectory,
+        childName === "" ? "index.html" : childName
+      );
+      if (child instanceof VFSDirectory) {
+        await buildDirAsync(vfs, `${uri}${childName}/`, childPath);
+      } else if (child instanceof IndexConflictVFSError) {
+        throw child;
+      } else {
+        await semaphore(async () => {
+          console.log(`generating ${childPath} ...`);
+          let data = await child.getContentsAsync();
+          await fs.promises.writeFile(childPath, data);
+        });
+      }
+    })
+  );
 }
 
 function parseArguments(args) {
