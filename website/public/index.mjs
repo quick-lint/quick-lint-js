01f6aafbaabc5383239046ebbf861b85417543b4
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-08-03 19:12:16 -0700

    refactor(website): create <qljs-icon> custom element
    Shorten code on install pages with two new custom elements:
    
    * <qljs-icon>
    * <qljs-content-icon>
    
    The home page and blog pages also have icons, but this patch doesn't
    change those pages.

--- website/public/index.mjs
+++ website/public/index.mjs
@@ -1,6 +1,9 @@
 // Copyright (C) 2020  Matthew "strager" Glazar
 // See end of file for extended copyright information.
 
+import path from "path";
+import { html } from "../src/html-tag.mjs";
+
 export let routes = {
   "/error-box.bundled.js": {
     type: "esbuild",
@@ -9,6 +12,87 @@ export let routes = {
     },
   },
 };
+
+export let customComponents = {
+  "qljs-content-icon": qljsContentIcon,
+  "qljs-icon": qljsIcon,
+};
+
+// <qljs-content-icon name="ubuntu" />
+//
+// Create an <img> with a descriptive alt attribute. Intended for icons
+// replacing content.
+function qljsContentIcon(attributes, { currentURI }) {
+  let icon = getIcon(attributes.name);
+  let sizeAttributes = attributes.size
+    ? `width="${attributes.size}" height="${attributes.size}"`
+    : "";
+  return html`<img
+    src="${makeRelativeIconURI(icon, currentURI)}"
+    alt="${icon.alt}"
+    title="${icon.alt}"
+    class="os-logo"
+    ${sizeAttributes}
+  />`;
+}
+
+// <qljs-icon name="ubuntu" />
+//
+// Create an <img> with a descriptive alt attribute. Intended for decorative
+// icons, with an explanation for the icon elsewhere.
+function qljsIcon(attributes, { currentURI }) {
+  let icon = getIcon(attributes.name);
+  let sizeAttributes = attributes.size
+    ? `width="${attributes.size}" height="${attributes.size}"`
+    : "";
+  return html`<img
+    src="${makeRelativeIconURI(icon, currentURI)}"
+    class="os-logo"
+    alt=""
+    title="${icon.alt}"
+    ${sizeAttributes}
+  />`;
+}
+
+let icons = {
+  chocolatey: { path: "chocolatey.svg", alt: "Chocolatey" },
+  "arch-linux": { path: "arch-linux.svg", alt: "Arch Linux" },
+  atom: { path: "atom.svg", alt: "Atom" },
+  "cli-and-lsp-server": {
+    path: "gnome-terminal.svg",
+    alt: "CLI and LSP server",
+  },
+  codespaces: { path: "codespaces.png", alt: "GitHub Codespaces" },
+  debian: { path: "debian.svg", alt: "Debian" },
+  emacs: { path: "emacs.svg", alt: "Emacs" },
+  github: { path: "github.svg", alt: "GitHub" },
+  homebrew: { path: "homebrew.svg", alt: "Homebrew" },
+  kate: { path: "kate.svg", alt: "Kate" },
+  linux: { path: "linux.svg", alt: "GNU/Linux" },
+  macos: { path: "macos.svg", alt: "macOS" },
+  neovim: { path: "neovim.svg", alt: "Neovim" },
+  nix: { path: "nix.svg", alt: "Nix" },
+  "notepad-plus-plus": { path: "notepad-plus-plus.svg", alt: "Notepad++" },
+  npm: { path: "npm.svg", alt: "npm" },
+  "open-vsx": { path: "open-vsx.svg", alt: "Open VSX" },
+  "sublime-text": { path: "sublime-text.svg", alt: "Sublime Text" },
+  ubuntu: { path: "ubuntu.svg", alt: "Ubuntu" },
+  vim: { path: "vim.gif", alt: "Vim" },
+  vscode: { path: "vscode.png", alt: "Visual Studio Code" },
+  webstorm: { path: "webstorm.svg", alt: "WebStorm" },
+  windows: { path: "windows.svg", alt: "Windows" },
+};
+
+function getIcon(name) {
+  if (!Object.prototype.hasOwnProperty.call(icons, name)) {
+    throw new Error(`unknown icon: ${name}`);
+  }
+  return icons[name];
+}
+
+function makeRelativeIconURI(icon, currentURI) {
+  return path.posix.relative(currentURI, "/" + icon.path);
+}
 
 // quick-lint-js finds bugs in JavaScript programs.
 // Copyright (C) 2020  Matthew "strager" Glazar
