381382166212c3f0544644b8d9f50f89abc1674a
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-08-04 18:34:36 -0700

    perf(website): remove EJS child process logic
    We now automatically restart the web server when a .mjs or .wasm file
    changes. This means we don't need the ugly and slow EJS-specific module
    cache busting which is implemented using child processes. Delete it,
    simplifying the code and speeding up builds.

--- website/src/router.mjs
+++ website/src/router.mjs
@@ -1,118 +1,69 @@
 // Copyright (C) 2020  Matthew "strager" Glazar
 // See end of file for extended copyright information.
 
-import assert from "assert";
-import child_process from "child_process";
+import ejs from "ejs";
+import fs from "fs";
 import path from "path";
 import url from "url";
-
-let __filename = url.fileURLToPath(import.meta.url);
-let __dirname = path.dirname(__filename);
+import { getQuickLintJSVersionInfoAsync } from "./qljs-version.mjs";
+import { makeRelativeURI } from "./uri.mjs";
 
 export async function renderEJSFileAsync(ejsFilePath, { currentURI }) {
-  let childProcess = await renderEJSChildProcessPool.takeAsync();
-  try {
-    let page = await childProcess.renderAsync({ currentURI, ejsFilePath });
-    let pagePostProcess = page.replace(/<script>\s*\/\/\s*<\/script>/g, "");
-    return pagePostProcess;
-  } finally {
-    renderEJSChildProcessPool.recycle(childProcess);
-  }
-}
+  ejsFilePath = path.resolve(ejsFilePath);
+  let ejsHTML = await fs.promises.readFile(ejsFilePath, "utf-8");
 
-class RenderEJSChildProcess {
-  constructor({ child }) {
-    this._child = child;
+  let state = {
+    cwd: path.dirname(ejsFilePath),
+  };
 
-    this._errorPromise = new Promise((resolve, reject) => {
-      child.on("message", (message, _sendHandle) => {
-        if (message.cmd === "error") {
-          reject(message.error);
-        }
-      });
-      child.on("close", (_code, _signal) => {
-        reject(new Error("worker exited without returning a response"));
-      });
-      child.on("exit", (_code, _signal) => {
-        reject(new Error("worker exited without returning a response"));
-      });
-      child.on("error", (error) => {
-        reject(error);
-      });
-      child.on("disconnect", () => {
-        reject(new Error("worker disconnected without returning a response"));
-      });
-    });
-
-    this._readyPromise = new Promise((resolve, reject) => {
-      child.on("message", (message, _sendHandle) => {
-        if (message.cmd === "ready") {
-          resolve();
-        }
-      });
-    });
-
-    this._resultPromise = new Promise((resolve, reject) => {
-      child.on("message", (message, _sendHandle) => {
-        if (message.cmd === "result") {
-          resolve(message.result);
-        }
-      });
-    });
+  function includer(_path, resolvedPath) {
+    // include() (defined by our prelude) will restore state.cwd for us.
+    state.cwd = path.dirname(resolvedPath);
   }
 
-  async waitForReadyAsync() {
-    return await Promise.race([this._readyPromise, this._errorPromise]);
-  }
-
-  async renderAsync({ currentURI, ejsFilePath }) {
-    let sent = this._child.send({ currentURI, ejsFilePath });
-    assert.ok(sent);
-    return await Promise.race([this._resultPromise, this._errorPromise]);
-  }
-
-  static fork() {
-    let child = child_process.fork(
-      path.join(__dirname, "render-ejs-file-child-process.mjs"),
-      [],
-      {
-        serialization: "advanced",
+  let prelude = `
+    let __realInclude = include;
+    include = async function (...args) {
+      let oldCWD = _state.cwd;
+      try {
+        /* __realInclude will call includer which will call process.chdir. */
+        return await __realInclude(...args);
+      } finally {
+        _state.cwd = oldCWD;
       }
-    );
-    return new RenderEJSChildProcess({ child: child });
-  }
-}
-
-class RenderEJSChildProcessPool {
-  constructor() {
-    let processCount = 6;
-
-    this._processes = [];
-    for (let i = 0; i < processCount; ++i) {
-      this._forkProcess();
     }
+  `;
+  prelude = prelude.replace(/\n/g, " "); // Preserve line numbers in user code.
+
+  function absoluteFilePath(p) {
+    return path.resolve(state.cwd, p);
   }
 
-  async takeAsync() {
-    if (this._processes.length === 0) {
-      this._forkProcess();
+  return await ejs.render(
+    `<% ${prelude} %>${ejsHTML}`,
+    {
+      _state: state,
+      currentURI: currentURI,
+      absoluteFilePath: absoluteFilePath,
+      importFileAsync: async (pathToImport) => {
+        return await import(url.pathToFileURL(absoluteFilePath(pathToImport)));
+      },
+      makeRelativeURI: (uri) => {
+        return makeRelativeURI(currentURI, uri);
+      },
+      qljsVersionInfo: await getQuickLintJSVersionInfoAsync(),
+      collapseInteriorWhitespace: (s) => {
+        return s.replace(/\s+/g, " ");
+      },
+    },
+    {
+      async: true,
+      compileDebug: true,
+      filename: ejsFilePath,
+      includer: includer,
     }
-    let process = this._processes.shift();
-    await process.waitForReadyAsync();
-    return process;
-  }
-
-  recycle(_process) {
-    // We can't use the old process because its module cache has been poisoned.
-    this._forkProcess();
-  }
-
-  _forkProcess() {
-    this._processes.push(RenderEJSChildProcess.fork());
-  }
+  );
 }
-
-let renderEJSChildProcessPool = new RenderEJSChildProcessPool();
 
 // quick-lint-js finds bugs in JavaScript programs.
 // Copyright (C) 2020  Matthew "strager" Glazar
