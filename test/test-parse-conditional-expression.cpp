eec690f88aaffe3331a99515faa9f289df0c6486
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-09-29 01:14:35 -0700

    feat(test): run ?: tests for both JavaScript and TypeScript
    Make it easier to write tests which cover both JS and TS by running all
    tests for the conditional operator with both modes

--- test/test-parse-conditional-expression.cpp
+++ test/test-parse-conditional-expression.cpp
@@ -27,11 +27,13 @@ using namespace std::literals::string_literals;
 
 namespace quick_lint_js {
 namespace {
-class test_parse_conditional_expression : public test_parse_expression {};
+class test_parse_conditional_expression
+    : public test_parse_expression,
+      public ::testing::WithParamInterface<parser_options> {};
 
-TEST_F(test_parse_conditional_expression, conditional_expression) {
+TEST_P(test_parse_conditional_expression, conditional_expression) {
   {
-    test_parser p(u8"x?y:z"_sv, capture_diags);
+    test_parser p(u8"x?y:z"_sv, GetParam(), capture_diags);
     expression* ast = p.parse_expression();
     EXPECT_EQ(ast->kind(), expression_kind::conditional);
     EXPECT_EQ(summarize(ast->child_0()), "var x");
@@ -42,7 +44,7 @@ TEST_F(test_parse_conditional_expression, conditional_expression) {
   }
 
   {
-    test_parser p(u8"x+x?y+y:z+z"_sv);
+    test_parser p(u8"x+x?y+y:z+z"_sv, GetParam());
     expression* ast = p.parse_expression();
     EXPECT_EQ(ast->kind(), expression_kind::conditional);
     EXPECT_EQ(summarize(ast->child_0()), "binary(var x, var x)");
@@ -51,23 +53,23 @@ TEST_F(test_parse_conditional_expression, conditional_expression) {
   }
 
   {
-    test_parser p(u8"a ? b : c ? d : e"_sv);
+    test_parser p(u8"a ? b : c ? d : e"_sv, GetParam());
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "cond(var a, var b, cond(var c, var d, var e))");
   }
 
   {
     // Regression test: This code once failed to parse with TypeScript.
-    test_parser p(u8"a ? !b : !c"_sv, typescript_options);
+    test_parser p(u8"a ? !b : !c"_sv, GetParam());
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "cond(var a, unary(var b), unary(var c))");
   }
 }
 
-TEST_F(test_parse_conditional_expression,
+TEST_P(test_parse_conditional_expression,
        conditional_expression_with_missing_condition) {
   {
-    test_parser p(u8"? b : c"_sv, capture_diags);
+    test_parser p(u8"? b : c"_sv, GetParam(), capture_diags);
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "cond(missing, var b, var c)");
     EXPECT_THAT(p.errors, ElementsAre(DIAG_TYPE_OFFSETS(
@@ -77,10 +79,10 @@ TEST_F(test_parse_conditional_expression,
   }
 }
 
-TEST_F(test_parse_conditional_expression,
+TEST_P(test_parse_conditional_expression,
        conditional_expression_with_missing_true_component) {
   {
-    test_parser p(u8"a ? : c"_sv, capture_diags);
+    test_parser p(u8"a ? : c"_sv, GetParam(), capture_diags);
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "cond(var a, missing, var c)");
     EXPECT_THAT(p.errors, ElementsAre(DIAG_TYPE_OFFSETS(
@@ -90,10 +92,10 @@ TEST_F(test_parse_conditional_expression,
   }
 }
 
-TEST_F(test_parse_conditional_expression,
+TEST_P(test_parse_conditional_expression,
        conditional_expression_with_missing_false_component) {
   {
-    test_parser p(u8"a ? b : "_sv, capture_diags);
+    test_parser p(u8"a ? b : "_sv, GetParam(), capture_diags);
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "cond(var a, var b, missing)");
     EXPECT_THAT(p.errors, ElementsAre(DIAG_TYPE_OFFSETS(
@@ -103,7 +105,7 @@ TEST_F(test_parse_conditional_expression,
   }
 
   {
-    test_parser p(u8"(a ? b :)"_sv, capture_diags);
+    test_parser p(u8"(a ? b :)"_sv, GetParam(), capture_diags);
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "paren(cond(var a, var b, missing))");
     EXPECT_THAT(p.errors, ElementsAre(DIAG_TYPE_OFFSETS(
@@ -114,10 +116,10 @@ TEST_F(test_parse_conditional_expression,
   }
 }
 
-TEST_F(test_parse_conditional_expression,
+TEST_P(test_parse_conditional_expression,
        conditional_expression_with_missing_colon_and_false_component) {
   {
-    test_parser p(u8"a ? b "_sv, capture_diags);
+    test_parser p(u8"a ? b "_sv, GetParam(), capture_diags);
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "cond(var a, var b, missing)");
     EXPECT_THAT(p.errors,
@@ -129,7 +131,7 @@ TEST_F(test_parse_conditional_expression,
   }
 
   {
-    test_parser p(u8"a ? b c"_sv, capture_diags);
+    test_parser p(u8"a ? b c"_sv, GetParam(), capture_diags);
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "cond(var a, var b, missing)");
     EXPECT_THAT(p.errors,
@@ -141,7 +143,7 @@ TEST_F(test_parse_conditional_expression,
   }
 
   {
-    test_parser p(u8"(a ? b)"_sv, capture_diags);
+    test_parser p(u8"(a ? b)"_sv, GetParam(), capture_diags);
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "paren(cond(var a, var b, missing))");
     EXPECT_THAT(p.errors,
@@ -153,6 +155,15 @@ TEST_F(test_parse_conditional_expression,
                 p.matches_offsets(strlen(u8"("), u8"a ? b"));
   }
 }
+
+INSTANTIATE_TEST_SUITE_P(javascript, test_parse_conditional_expression,
+                         ::testing::Values(javascript_options));
+
+// In TypeScript, ':' is used for both type annotations in arrow functions and
+// for the conditional operator. Ensure we parse all of the examples as
+// conditional operators.
+INSTANTIATE_TEST_SUITE_P(typescript, test_parse_conditional_expression,
+                         ::testing::Values(typescript_options));
 }
 }
 
