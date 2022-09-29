quick-lint/quick-lint-js

commit a1cc1d06ba208bdea9d0c54cecadf1725712c005

fix(typescript): parse '<number>expr' correctly
quick-lint-js parses '<T>expr' in TypeScript mode as a type assertion,
but parsing fails if 'T' is a built-in type like 'number'. Fix this by
adding built-in types to the list of tokens recognized after '<'.

src/quick-lint-js/fe/parse-expression.cpp
@@ -3031,6 +3031,15 @@ expression* parser::parse_jsx_or_typescript_generic_expression(
     // <(Type)>expr
     // < | Type>expr
     case token_type::ampersand:
+    case token_type::kw_bigint:
+    case token_type::kw_boolean:
+    case token_type::kw_null:
+    case token_type::kw_number:
+    case token_type::kw_object:
+    case token_type::kw_string:
+    case token_type::kw_symbol:
+    case token_type::kw_undefined:
+    case token_type::kw_void:
     case token_type::left_curly:
     case token_type::left_paren:
     case token_type::left_square:

test/test-parse-typescript-angle-type-assertion.cpp
@@ -94,6 +94,15 @@ TEST_F(test_parse_typescript_angle_type_assertion, angle_type_assertion) {
                                       "visit_variable_use"));     // expr
     EXPECT_THAT(p.variable_uses, ElementsAre(u8"Type", u8"expr"));
   }
+
+  for (const string8& type : typescript_builtin_type_keywords) {
+    string8 code = u8"<" + type + u8">expr;";
+    SCOPED_TRACE(out_string8(code));
+    test_parser p(code, typescript_options);
+    p.parse_and_visit_statement();
+    EXPECT_THAT(p.visits, ElementsAre("visit_variable_use"));  // expr
+    EXPECT_THAT(p.variable_uses, ElementsAre(u8"expr"));
+  }
 }
 
 TEST_F(test_parse_typescript_angle_type_assertion,

commit 454d83a447424ac9fdbdee4192f4234c1dca8e0e

fix(typescript): parse <number></number> as JSX in TS+JSX mode
In TS+JSX mode, commit a1cc1d06 broke parsing of JSX with tags named
'number', 'void', etc. Fix this by treating number/void/etc. as possibly
a JSX tag name, similar to identifiers.

Refs: a1cc1d06ba208bdea9d0c54cecadf1725712c005

src/quick-lint-js/fe/parse-expression.cpp
@@ -3083,15 +3083,6 @@ expression* parser::parse_jsx_or_typescript_generic_expression(
     // <(Type)>expr
     // < | Type>expr
     case token_type::ampersand:
-    case token_type::kw_bigint:
-    case token_type::kw_boolean:
-    case token_type::kw_null:
-    case token_type::kw_number:
-    case token_type::kw_object:
-    case token_type::kw_string:
-    case token_type::kw_symbol:
-    case token_type::kw_undefined:
-    case token_type::kw_void:
     case token_type::left_curly:
     case token_type::left_paren:
     case token_type::left_square:
@@ -3105,6 +3096,15 @@ expression* parser::parse_jsx_or_typescript_generic_expression(
     // <T,>(params) => {}    // Arrow function.
     // <Component></Component>  // JSX element.
     case token_type::identifier:
+    case token_type::kw_bigint:
+    case token_type::kw_boolean:
+    case token_type::kw_null:
+    case token_type::kw_number:
+    case token_type::kw_object:
+    case token_type::kw_string:
+    case token_type::kw_symbol:
+    case token_type::kw_undefined:
+    case token_type::kw_void:
       this->skip();
       switch (this->peek().type) {
       // <T,>() => {}                 // Generic arrow function.

test/test-parse-typescript-angle-type-assertion.cpp
@@ -131,6 +131,12 @@ TEST_F(test_parse_typescript_angle_type_assertion,
     expression* ast = p.parse_expression();
     EXPECT_EQ(summarize(ast), "jsxelement(Component)");
   }
+
+  {
+    test_parser p(u8"<number>text;\n// </number>;"_sv, typescript_jsx_options);
+    expression* ast = p.parse_expression();
+    EXPECT_EQ(summarize(ast), "jsxelement(number)");
+  }
 }
 
 TEST_F(test_parse_typescript_angle_type_assertion,