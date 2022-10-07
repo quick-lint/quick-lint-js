7d76a1f5b17d91d9bb898dd2aaf77a719e16a9bb
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-10-06 20:04:19 -0700

    refactor(fe): make debug-parse-visitor.h include what it uses
    debug-parse-visitor.h assumes that std::literals::string_view_literals
    was using-d. This means it can't be included first. Fix this by making
    it use our operator""_sv instead.

--- src/quick-lint-js/fe/debug-parse-visitor.h
+++ src/quick-lint-js/fe/debug-parse-visitor.h
@@ -6,35 +6,36 @@
 
 #include <quick-lint-js/fe/parse-visitor.h>
 #include <quick-lint-js/io/output-stream.h>
+#include <quick-lint-js/port/char8.h>
 
 namespace quick_lint_js {
 class debug_parse_visitor final : public parse_visitor_base {
  public:
   void visit_end_of_module() override {
-    this->output_->append_copy(u8"end of module\n"sv);
+    this->output_->append_copy(u8"end of module\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_block_scope() override {
-    this->output_->append_copy(u8"entered block scope\n"sv);
+    this->output_->append_copy(u8"entered block scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_with_scope() override {
-    this->output_->append_copy(u8"entered with scope\n"sv);
+    this->output_->append_copy(u8"entered with scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_class_scope() override {
-    this->output_->append_copy(u8"entered class scope\n"sv);
+    this->output_->append_copy(u8"entered class scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_class_scope_body(
       const std::optional<identifier> &class_name) override {
-    this->output_->append_copy(u8"entered class scope body"sv);
+    this->output_->append_copy(u8"entered class scope body"_sv);
     if (class_name.has_value()) {
-      this->output_->append_copy(u8": "sv);
+      this->output_->append_copy(u8": "_sv);
       this->output_->append_copy(class_name->normalized_name());
     }
     this->output_->append_copy(u8'\n');
@@ -42,102 +43,102 @@ class debug_parse_visitor final : public parse_visitor_base {
   }
 
   void visit_enter_enum_scope() override {
-    this->output_->append_copy(u8"entered enum scope\n"sv);
+    this->output_->append_copy(u8"entered enum scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_for_scope() override {
-    this->output_->append_copy(u8"entered for scope\n"sv);
+    this->output_->append_copy(u8"entered for scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_function_scope() override {
-    this->output_->append_copy(u8"entered function scope\n"sv);
+    this->output_->append_copy(u8"entered function scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_function_scope_body() override {
-    this->output_->append_copy(u8"entered function scope body\n"sv);
+    this->output_->append_copy(u8"entered function scope body\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_index_signature_scope() override {
-    this->output_->append_copy(u8"entered index signature scope\n"sv);
+    this->output_->append_copy(u8"entered index signature scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_interface_scope() override {
-    this->output_->append_copy(u8"entered interface scope\n"sv);
+    this->output_->append_copy(u8"entered interface scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_named_function_scope(identifier) override {
-    this->output_->append_copy(u8"entered named function scope\n"sv);
+    this->output_->append_copy(u8"entered named function scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_namespace_scope() override {
-    this->output_->append_copy(u8"entered namespace scope\n"sv);
+    this->output_->append_copy(u8"entered namespace scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_enter_type_alias_scope() override {
-    this->output_->append_copy(u8"entered type alias scope\n"sv);
+    this->output_->append_copy(u8"entered type alias scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_block_scope() override {
-    this->output_->append_copy(u8"exited block scope\n"sv);
+    this->output_->append_copy(u8"exited block scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_with_scope() override {
-    this->output_->append_copy(u8"exited with scope\n"sv);
+    this->output_->append_copy(u8"exited with scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_class_scope() override {
-    this->output_->append_copy(u8"exited class scope\n"sv);
+    this->output_->append_copy(u8"exited class scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_enum_scope() override {
-    this->output_->append_copy(u8"exited enum scope\n"sv);
+    this->output_->append_copy(u8"exited enum scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_for_scope() override {
-    this->output_->append_copy(u8"exited for scope\n"sv);
+    this->output_->append_copy(u8"exited for scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_function_scope() override {
-    this->output_->append_copy(u8"exited function scope\n"sv);
+    this->output_->append_copy(u8"exited function scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_index_signature_scope() override {
-    this->output_->append_copy(u8"exited index signature scope\n"sv);
+    this->output_->append_copy(u8"exited index signature scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_interface_scope() override {
-    this->output_->append_copy(u8"exited interface scope\n"sv);
+    this->output_->append_copy(u8"exited interface scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_namespace_scope() override {
-    this->output_->append_copy(u8"exited namespace scope\n"sv);
+    this->output_->append_copy(u8"exited namespace scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_exit_type_alias_scope() override {
-    this->output_->append_copy(u8"exited type alias scope\n"sv);
+    this->output_->append_copy(u8"exited type alias scope\n"_sv);
     this->output_->flush();
   }
 
   void visit_keyword_variable_use(identifier name) override {
-    this->output_->append_copy(u8"keyword variable use: "sv);
+    this->output_->append_copy(u8"keyword variable use: "_sv);
     this->output_->append_copy(name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
@@ -145,9 +146,9 @@ class debug_parse_visitor final : public parse_visitor_base {
 
   void visit_property_declaration(
       const std::optional<identifier> &name) override {
-    this->output_->append_copy(u8"property declaration"sv);
+    this->output_->append_copy(u8"property declaration"_sv);
     if (name.has_value()) {
-      this->output_->append_copy(u8": "sv);
+      this->output_->append_copy(u8": "_sv);
       this->output_->append_copy(name->normalized_name());
     }
     this->output_->append_copy(u8'\n');
@@ -155,7 +156,7 @@ class debug_parse_visitor final : public parse_visitor_base {
   }
 
   void visit_variable_assignment(identifier name) override {
-    this->output_->append_copy(u8"variable assignment: "sv);
+    this->output_->append_copy(u8"variable assignment: "_sv);
     this->output_->append_copy(name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
@@ -163,7 +164,7 @@ class debug_parse_visitor final : public parse_visitor_base {
 
   void visit_variable_declaration(identifier name, variable_kind,
                                   variable_init_kind) override {
-    this->output_->append_copy(u8"variable declaration: "sv);
+    this->output_->append_copy(u8"variable declaration: "_sv);
     this->output_->append_copy(name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
@@ -172,49 +173,49 @@ class debug_parse_visitor final : public parse_visitor_base {
   void visit_variable_delete_use(
       identifier name,
       [[maybe_unused]] source_code_span delete_keyword) override {
-    this->output_->append_copy(u8"variable delete use: "sv);
+    this->output_->append_copy(u8"variable delete use: "_sv);
     this->output_->append_copy(name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
   }
 
   void visit_variable_export_use(identifier name) override {
-    this->output_->append_copy(u8"variable export use: "sv);
+    this->output_->append_copy(u8"variable export use: "_sv);
     this->output_->append_copy(name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
   }
 
   void visit_variable_namespace_use(identifier name) override {
-    this->output_->append_copy(u8"variable namespace use: "sv);
+    this->output_->append_copy(u8"variable namespace use: "_sv);
     this->output_->append_copy(name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
   }
 
   void visit_variable_type_use(identifier name) override {
-    this->output_->append_copy(u8"variable type use: "sv);
+    this->output_->append_copy(u8"variable type use: "_sv);
     this->output_->append_copy(name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
   }
 
   void visit_variable_type_predicate_use(identifier parameter_name) override {
-    this->output_->append_copy(u8"variable type predicate use: "sv);
+    this->output_->append_copy(u8"variable type predicate use: "_sv);
     this->output_->append_copy(parameter_name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
   }
 
   void visit_variable_typeof_use(identifier name) override {
-    this->output_->append_copy(u8"variable typeof use: "sv);
+    this->output_->append_copy(u8"variable typeof use: "_sv);
     this->output_->append_copy(name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
   }
 
   void visit_variable_use(identifier name) override {
-    this->output_->append_copy(u8"variable use: "sv);
+    this->output_->append_copy(u8"variable use: "_sv);
     this->output_->append_copy(name.normalized_name());
     this->output_->append_copy(u8'\n');
     this->output_->flush();
