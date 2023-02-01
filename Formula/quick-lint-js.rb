ec92d7910d4f0dae8ae2d51b56edd9ced69d08b5
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-01-31 20:55:22 -0800

    feat(homebrew): enable testing
    The quick-lint-js Homebrew formula specifies -DBUILD_TESTING=ON and also
    runs the ctest command. Unfortunately, this isn't enough to enable
    testing because -DBUILD_TESTING=ON is overwritten by std_cmake_args [1].
    
    Move -DBUILD_TESTING=ON so that it overwrites std_cmake_args. Also work
    around Homebrew's googletest being compiled without -fchar8_t support.
    
    [1] Override introduced in Homebrew commit
        842a2ea890d3cff935c301ba2b41277869a872e1
        (https://github.com/Homebrew/brew/pull/11159)

--- Formula/quick-lint-js.rb
+++ Formula/quick-lint-js.rb
@@ -21,7 +21,6 @@ class QuickLintJs < Formula
 
   def install
     system "cmake", "-S", ".", "-B", "build",
-                    "-DBUILD_TESTING=ON",
                     "-DQUICK_LINT_JS_ENABLE_BENCHMARKS=OFF",
                     "-DQUICK_LINT_JS_INSTALL_EMACS_DIR=#{elisp}",
                     "-DQUICK_LINT_JS_INSTALL_VIM_NEOVIM_TAGS=ON",
@@ -29,7 +28,14 @@ class QuickLintJs < Formula
                     "-DQUICK_LINT_JS_USE_BUNDLED_GOOGLE_BENCHMARK=OFF",
                     "-DQUICK_LINT_JS_USE_BUNDLED_GOOGLE_TEST=OFF",
                     "-DQUICK_LINT_JS_USE_BUNDLED_SIMDJSON=OFF",
-                    *std_cmake_args
+                    # HACK(strager): googletest was compiled without -fchar8_t (default for
+                    # -std=c++20). It is missing symbols which might be referenced if quick-lint-js
+                    # is built with -fchar8_t. Tell quick-lint-js' build system that -fchar8_t
+                    # doesn't work (QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T=NO), causing it to build
+                    # with -fno-char8_t. This fixes undefined symbol errors when linking tests.
+                    "-DQUICK_LINT_JS_HAVE_WORKING_FCHAR8_T=NO",
+                    *std_cmake_args,
+                    "-DBUILD_TESTING=ON"
     system "cmake", "--build", "build"
     system "ctest", "--verbose", "--parallel", ENV.make_jobs, "--test-dir", "build"
     system "cmake", "--install", "build"
