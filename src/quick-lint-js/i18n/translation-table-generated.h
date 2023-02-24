506081811aaeda8be437d4ece827411d5c777633
Alek Lefebvre <alek.lefebvre@gmail.com>
2023-02-23 19:27:34 -0500

    clang + translation

--- src/quick-lint-js/i18n/translation-table-generated.h
+++ src/quick-lint-js/i18n/translation-table-generated.h
@@ -21,7 +21,7 @@ using namespace std::literals::string_view_literals;
 
 constexpr std::uint32_t translation_table_locale_count = 5;
 constexpr std::uint16_t translation_table_mapping_table_size = 405;
-constexpr std::size_t translation_table_string_table_size = 74655;
+constexpr std::size_t translation_table_string_table_size = 74716;
 constexpr std::size_t translation_table_locale_table_size = 35;
 
 QLJS_CONSTEVAL std::uint16_t translation_table_const_look_up(
