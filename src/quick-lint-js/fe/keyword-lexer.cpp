5740cad684020273a81e207bb1c093267f70ed34
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-02-27 01:24:13 -0800

    @@@ refactor(fe): optimize keyword string comparisons on AArch64 and WebAssembly

--- src/quick-lint-js/fe/keyword-lexer.cpp
+++ src/quick-lint-js/fe/keyword-lexer.cpp
@@ -8,6 +8,9 @@
 #include <quick-lint-js/port/char8.h>
 #include <quick-lint-js/port/have.h>
 #include <quick-lint-js/port/simd.h>
+#include <quick-lint-js/util/byte-order.h>
+
+#define QLJS_OPTIMIZE_KEY_STRINGS_EQUAL 1
 
 namespace quick_lint_js {
 #if QLJS_HAVE_CHAR8_T
@@ -20,6 +23,7 @@ bool keyword_lexer::key_strings_equal(const char* a, const char* b,
 
 bool keyword_lexer::key_strings_equal(const char8* a, const char8* b,
                                       std::size_t size) {
+#if QLJS_OPTIMIZE_KEY_STRINGS_EQUAL
 #if QLJS_HAVE_X86_SSE2
   char_vector_16_sse2 a_unmasked = char_vector_16_sse2::load(a);
   char_vector_16_sse2 b_unmasked = char_vector_16_sse2::load(b);
@@ -35,8 +39,22 @@ bool keyword_lexer::key_strings_equal(const char8* a, const char8* b,
   std::uint32_t not_equal_mask = ~equal_mask;
   return (mask & not_equal_mask) == 0;
 #else
-  // TODO(strager): Optimize ARM NEON.
-  // TODO(strager): Optimize WebAssembly SIMD128.
+  std::uint64_t a_first_8 = load_u64_le(a);
+  std::uint32_t a_last_4 = load_u32_le(a + 8);
+
+  std::uint64_t b_first_8 = load_u64_le(b);
+  std::uint32_t b_last_4 = load_u32_le(b + 8);
+
+  std::uint64_t first_8_mask = size >= 8 ? 0xffff'ffff'ffff'ffffULL
+                                         : (std::uint64_t(1) << (size * 8)) - 1;
+  std::uint64_t last_4_mask =
+      size <= 8 ? 0 : (std::uint64_t(1) << ((size - 8) * 8)) - 1;
+
+  std::uint64_t first_8_comparison = (a_first_8 ^ b_first_8) & first_8_mask;
+  std::uint64_t last_4_comparison = (a_last_4 ^ b_last_4) & last_4_mask;
+  return (first_8_comparison | last_4_comparison) == 0;
+#endif
+#else
   return std::memcmp(a, b, size) == 0;
 #endif
 }
