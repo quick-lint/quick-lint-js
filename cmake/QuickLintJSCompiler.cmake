quick-lint/quick-lint-js

commit cd91abac4f276b0f219ea23f0893d702dd8b18fa

fix(cmake): fix -DQUICK_LINT_JS_HAVE_WORKING_FCHAR8_T=0 not being respected
If you run 'cmake -DQUICK_LINT_JS_HAVE_WORKING_FCHAR8_T=0 ...', then the build
system does not use the -fchar8_t GCC/Clang flag. However, if you run
'CXXFLAGS=-fno-rtti cmake -DQUICK_LINT_JS_HAVE_WORKING_FCHAR8_T=0 ...', then the
-fchar8_t flag is used. This can lead to link failures with
-DQUICK_LINT_JS_USE_BUNDLED_GOOGLE_TEST=NO (documented in
dist/debian/debian/rules).

The problem is that our CMake code overwrites the user's
QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T if QUICK_LINT_JS_HAVE_TYPEID is false.

Fix the code to never overwrite the user's QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T.

This fixed exposes an implicit char->char8 conversion in benchmark-lsp.
Fix that issue too.

cmake/QuickLintJSCompiler.cmake
@@ -22,6 +22,8 @@ endfunction ()
 function (quick_lint_js_enable_char8_t_if_supported)
   check_cxx_compiler_flag(-fchar8_t QUICK_LINT_JS_HAVE_FCHAR8_T_FLAG)
   if (QUICK_LINT_JS_HAVE_FCHAR8_T_FLAG)
+    set(QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T TRUE CACHE BOOL "Whether the -fchar8_t C++ flag works reliably")
+
     check_cxx_source_compiles(
       "#include <cstdio>
       #include <typeinfo>
@@ -40,8 +42,6 @@ function (quick_lint_js_enable_char8_t_if_supported)
           std::puts(typeid(char8_t).name());
           return 0;
         }" QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T)
-    else ()
-      set(QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T TRUE)
     endif ()
 
     if (QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T)

benchmark/benchmark-lsp/lsp-benchmarks.cpp
@@ -465,7 +465,7 @@ std::vector<benchmark_factory> get_benchmark_factories() {
               // In Cart's render function in react-quickly-ch10.jsx, clear the
               // "Your cart is empty" text then re-type it character by
               // character.
-              static constexpr char text[] = "Your cart is empty";
+              static constexpr char8 text[] = u8"Your cart is empty";
               static constexpr int text_length = sizeof(text) - 1;
               int characters_already_typed = (i % text_length == 0)
                                                  ? text_length

commit e7d5b500e7781693f1c5592a37b201314fcf3c4c

fix(cmake): fix check for char8_t compiler bugs
Commit cd91abac broke the check_cxx_source_compiles check for char8_t working
properly. check_cxx_source_compiles does nothing if the
QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T variable was already set.

Make the check_cxx_source_compiles call work as intended by only setting
QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T if we don't call check_cxx_source_compiles.

Refs: cd91abac4f276b0f219ea23f0893d702dd8b18fa

cmake/QuickLintJSCompiler.cmake
@@ -22,8 +22,6 @@ endfunction ()
 function (quick_lint_js_enable_char8_t_if_supported)
   check_cxx_compiler_flag(-fchar8_t QUICK_LINT_JS_HAVE_FCHAR8_T_FLAG)
   if (QUICK_LINT_JS_HAVE_FCHAR8_T_FLAG)
-    set(QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T TRUE CACHE BOOL "Whether the -fchar8_t C++ flag works reliably")
-
     check_cxx_source_compiles(
       "#include <cstdio>
       #include <typeinfo>
@@ -42,6 +40,8 @@ function (quick_lint_js_enable_char8_t_if_supported)
           std::puts(typeid(char8_t).name());
           return 0;
         }" QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T)
+    else ()
+      set(QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T TRUE CACHE BOOL "Whether the -fchar8_t C++ flag works reliably")
     endif ()
 
     if (QUICK_LINT_JS_HAVE_WORKING_FCHAR8_T)