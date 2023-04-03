510e33002b061d40bd4e83384b7955656ac51eb0
LeeWannacott <WannacottL@gmail.com>
2023-04-03 15:43:22 +1200

    Update copyright 2020 => 2023.

--- dist/update-release-notes.go
+++ dist/update-release-notes.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2020  Matthew "strager" Glazar
+// Copyright (C) 2023  Matthew "strager" Glazar
 // See end of file for extended copyright information.
 
 package main
@@ -430,7 +430,7 @@ func updateOrCreateGitHubRelease(releaseRequest releaseRequest, requestURL strin
 }
 
 // quick-lint-js finds bugs in JavaScript programs.
-// Copyright (C) 2020  Matthew "strager" Glazar
+// Copyright (C) 2023  Matthew "strager" Glazar
 //
 // This file is part of quick-lint-js.
 //
