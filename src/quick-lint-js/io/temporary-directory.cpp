f49083a9e41afa2374dc63cfc9c1bea72beb1680
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-04-04 18:20:47 -0700

    feat(io): create list_directory_recursive function
    This function will be used in the future.

--- src/quick-lint-js/io/temporary-directory.cpp
+++ src/quick-lint-js/io/temporary-directory.cpp
@@ -261,11 +261,11 @@ bool is_dot_or_dot_dot(const Char *path) {
   return path[0] == '.' &&
          (path[1] == '\0' || (path[1] == '.' && path[2] == '\0'));
 }
-}
 
-result<void, platform_file_io_error> list_directory(
-    const char *directory, function_ref<void(const char *)> visit_file) {
 #if QLJS_HAVE_WINDOWS_H
+result<void, platform_file_io_error> list_directory_raw(
+    const char *directory,
+    function_ref<void(::WIN32_FIND_DATAW &)> visit_entry) {
   std::optional<std::wstring> search_pattern = mbstring_to_wstring(directory);
   if (!search_pattern.has_value()) {
     QLJS_UNIMPLEMENTED();
@@ -280,14 +280,7 @@ result<void, platform_file_io_error> list_directory(
     });
   }
   do {
-    std::optional<std::string> entry_name =
-        wstring_to_mbstring(entry.cFileName);
-    if (!entry_name.has_value()) {
-      QLJS_UNIMPLEMENTED();
-    }
-    if (!is_dot_or_dot_dot(entry_name->c_str())) {
-      visit_file(entry_name->c_str());
-    }
+    visit_entry(entry);
   } while (::FindNextFileW(finder, &entry));
 
   ::DWORD error = ::GetLastError();
@@ -299,7 +292,12 @@ result<void, platform_file_io_error> list_directory(
 
   ::FindClose(finder);
   return {};
-#elif QLJS_HAVE_DIRENT_H
+}
+#endif
+
+#if QLJS_HAVE_DIRENT_H
+result<void, platform_file_io_error> list_directory_raw(
+    const char *directory, function_ref<void(::dirent *)> visit_entry) {
   ::DIR *d = ::opendir(directory);
   if (d == nullptr) {
     return failed_result(posix_file_io_error{
@@ -317,15 +315,125 @@ result<void, platform_file_io_error> list_directory(
       }
       break;
     }
-    if (!is_dot_or_dot_dot(entry->d_name)) {
-      visit_file(entry->d_name);
-    }
+    visit_entry(entry);
   }
   ::closedir(d);
   return {};
+}
+#endif
+}
+
+result<void, platform_file_io_error> list_directory(
+    const char *directory, function_ref<void(const char *)> visit_file) {
+#if QLJS_HAVE_WINDOWS_H
+  auto visit_entry = [&](::WIN32_FIND_DATAW &entry) -> void {
+    // TODO(strager): Reduce allocations.
+    std::optional<std::string> entry_name =
+        wstring_to_mbstring(entry.cFileName);
+    if (!entry_name.has_value()) {
+      QLJS_UNIMPLEMENTED();
+    }
+    if (!is_dot_or_dot_dot(entry_name->c_str())) {
+      visit_file(entry_name->c_str());
+    }
+  };
+  return list_directory_raw(directory, visit_entry);
+#elif QLJS_HAVE_DIRENT_H
+  auto visit_entry = [&](::dirent *entry) -> void {
+    if (!is_dot_or_dot_dot(entry->d_name)) {
+      visit_file(entry->d_name);
+    }
+  };
+  return list_directory_raw(directory, visit_entry);
 #else
 #error "Unsupported platform"
 #endif
+}
+
+result<void, platform_file_io_error> list_directory(
+    const char *directory,
+    function_ref<void(const char *, bool is_directory)> visit_file) {
+#if QLJS_HAVE_WINDOWS_H
+  auto visit_entry = [&](::WIN32_FIND_DATAW &entry) -> void {
+    // TODO(strager): Reduce allocations.
+    std::optional<std::string> entry_name =
+        wstring_to_mbstring(entry.cFileName);
+    if (!entry_name.has_value()) {
+      QLJS_UNIMPLEMENTED();
+    }
+    if (!is_dot_or_dot_dot(entry_name->c_str())) {
+      bool is_directory = (entry.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) ==
+                          FILE_ATTRIBUTE_DIRECTORY;
+      visit_file(entry_name->c_str(), is_directory);
+    }
+  };
+  return list_directory_raw(directory, visit_entry);
+#elif QLJS_HAVE_DIRENT_H
+  std::string temp_path;
+  auto visit_entry = [&](::dirent *entry) -> void {
+    if (is_dot_or_dot_dot(entry)) {
+      return;
+    }
+    bool is_directory;
+    if (entry->d_type == DT_UNKNOWN) {
+      temp_path.clear();
+      temp_path += directory;
+      temp_path += QLJS_PREFERRED_PATH_DIRECTORY_SEPARATOR;
+      temp_path += entry->d_name;
+      struct stat s;
+      int lstat_rc = ::lstat(temp_path.c_str(), &s);
+      if (lstat_rc == -1) {
+        if (errno == ENOENT) {
+          return;
+        }
+        is_directory = false;
+      } else {
+        is_directory = S_ISDIR(s.st_mode);
+      }
+    } else {
+      is_directory = entry->d_type == DT_DIR;
+    }
+    visit_file(entry->d_name, is_directory);
+  };
+  return list_directory_raw(directory, visit_entry);
+#else
+#error "Unsupported platform"
+#endif
+}
+
+void list_directory_recursively(
+    const char *directory, function_ref<void(const std::string &)> visit_file,
+    function_ref<void(const platform_file_io_error &, int depth)> on_error) {
+  struct finder {
+    std::string path;
+    function_ref<void(const std::string &)> visit_file;
+    function_ref<void(const platform_file_io_error &, int depth)> on_error;
+
+    void recurse(int depth) {
+      std::size_t path_length = this->path.size();
+
+      auto visit_child = [&](const char *child_name,
+                             bool is_directory) -> void {
+        this->path.resize(path_length);
+        this->path += QLJS_PREFERRED_PATH_DIRECTORY_SEPARATOR;
+        this->path += child_name;
+        if (is_directory) {
+          this->recurse(depth + 1);
+        } else {
+          this->visit_file(path);
+        }
+      };
+      // TODO(strager): Reduce allocations on Windows. Windows uses wchar_t
+      // paths and also needs a "\*" suffix.
+      result<void, platform_file_io_error> list =
+          list_directory(this->path.c_str(), visit_child);
+      if (!list.ok()) {
+        this->on_error(list.error(), depth);
+      }
+    }
+  };
+  finder f = {directory, visit_file, on_error};
+  f.recurse(0);
 }
 
 result<std::string, platform_file_io_error> get_current_working_directory() {
