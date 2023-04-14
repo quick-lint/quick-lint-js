7079eed370c69076f0f8c56031b794dea1aaf8eb
yasserare <younessasserare63@gmail.com>
2023-04-14 03:04:44 +0000

    commit#2

--- dist/webinstaller/README.md
+++ dist/webinstaller/README.md
@@ -1,10 +1,13 @@
 # quick-lint-js webinstaller manifest
 
-This directory contains manifests for [webinstaller][], a developer tools installer.
+This directory contains manifests for [webinstaller](https://webinstall.dev/), a developer tools installer.
 
-The `make-webinstaller-manifest.js` script insert the ouput file with the appropriate release version number and its release date.
+The `make-webinstaller-manifest.js` script creates the output file with the appropriate release version number and its release date.
 
 ## Manually building the manifest
 
 Run:
-   `node  dist/webinstaller/make-webinstaller-manifest.js -BaseURI https://c.quick-lint-js.com/releases/RELEASE_VERSION/ -releaseDate RELEASE_DATE -Out OUTPUT_FILE.json`
+
+```sh
+node  dist/webinstaller/make-webinstaller-manifest.js -releaseVersion VERSION -releaseDate RELEASE_DATE -Out OUTPUT_FILE.json
+```
--- dist/webinstaller/make-webinstaller-manifest.js
+++ dist/webinstaller/make-webinstaller-manifest.js
@@ -1,47 +1,63 @@
-const fs = require('fs');
+// Copyright (C) 2020  Matthew "strager" Glazar
+// See end of file for extended copyright information.
 
-const [, , ...args] = process.argv;
-const [baseUriArg, BaseURI, releaseDateArg, releaseDate, outputArg, outputFile] = args;
+const fs = require("fs");
 
-function validateArg(args, argName, regex) {
-    const argIndex = args.indexOf(argName);
-    if (argIndex < 0) {
-      console.log(`error: missing ${argName}`);
-      return false;
-    }
-  
-    const argValue = args[argIndex + 1];
-    if (!regex.test(argValue)) {
-      console.log(`error: invalid ${argName}; must match regular expression: ${regex}`);
-      return false;
-    }
-  
-    return true;
-  }
-  
-  if (!validateArg(args, '-BaseURI', /^https?:\/\/.*\/$/)) {
-    return 0;
-  }
-  
-  if (!validateArg(args, '-releaseDate', /^\d{4}-\d{2}-\d{2}$/)) {
-    return 0;
-  }
-  
+const args = process.argv.slice(2);
+const releaseVersion = process.argv[3];
+const releaseDate = process.argv[5];
+const outputFile = process.argv[7];
 
-//get the version
-const releasesRegex = /\/(\d+\.\d+\.\d+)\/$/;
-const releasesVersion = BaseURI.match(releasesRegex);
-
-const template = fs.readFileSync('quick-lint-js-template.json', 'utf8');
-
-const data = JSON.parse(template);
-
-// set version and insert download with the approriate value 
-for (const release of data.release) {
-    release.version = releasesVersion[1];
-    release.download = release.download.replace('${version}', releasesVersion[1]);
-    release.date = releaseDate;
+if (!args.includes("-releaseVersion")) {
+  console.log("error: missing -releaseVersion");
+  return 0;
+}
+if (!args.includes("-releaseDate")) {
+  console.log("error: missing -releaseDate");
+  return 0;
+}
+if (!args.includes("-Out")) {
+  console.log("error: missing -Out");
+  return 0;
 }
 
-// Write the modified data back to a file
-fs.writeFileSync(outputFile, JSON.stringify(data, null, 2), 'utf8');
+const releaseVersionRegex = /^\d+\.\d+\.\d+$/;
+if (!releaseVersionRegex.test(releaseVersion)) {
+  console.log(
+    "error: invalid -releaseVersion; must match regular expression: " +
+      releaseVersionRegex
+  );
+  return 0;
+}
+
+const releasesDateRegex = /^\d{4}-\d{2}-\d{2}$/;
+if (!releasesDateRegex.test(releaseDate)) {
+  console.log(
+    "error: invalid -releaseDate; must match regular expression: " +
+      releasesDateRegex
+  );
+  return 0;
+}
+
+let data = fs.readFileSync("quick-lint-js-template.json", "utf8");
+data = data.replace(/{version}/g, releaseVersion);
+data = data.replace(/\${releaseDate}/g, releaseDate);
+fs.writeFileSync(outputFile, data, "utf8");
+
+// quick-lint-js finds bugs in JavaScript programs.
+// Copyright (C) 2020  Matthew "strager" Glazar
+//
+// This file is part of quick-lint-js.
+//
+// quick-lint-js is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// quick-lint-js is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with quick-lint-js.  If not, see <https://www.gnu.org/licenses/>.
