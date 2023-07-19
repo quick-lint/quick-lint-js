8cd52bb10192f17ae06a0b55bec2b960f3e8fdb9
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-07-18 20:18:10 -0700

    fix(docs): fix typo in docs

--- src/quick-lint-js/fe/buffering-visitor.h
+++ src/quick-lint-js/fe/buffering-visitor.h
@@ -119,7 +119,7 @@ class Buffering_Visitor final : public Parse_Visitor_Base {
   };
 
   // These 'add' functions significantly reduces code size by discouraging the
-  // inlining of visit::visit and linked_vector<>::emplace_back.
+  // inlining of visit::visit and Linked_Vector<>::emplace_back.
   [[gnu::noinline]] void add(Visit_Kind kind) {
     this->visits_.emplace_back(kind);
   }
