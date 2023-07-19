07dd39acabea0e498c0419a5260b8275cad94d5a
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-07-18 19:00:41 -0700

    chore: prepare for 2.15.0 release

--- Formula/quick-lint-js.rb
+++ Formula/quick-lint-js.rb
@@ -4,7 +4,7 @@
 class QuickLintJs < Formula
   desc "Find bugs in your JavaScript code"
   homepage "https://quick-lint-js.com/"
-  url "https://c.quick-lint-js.com/releases/2.14.0/source/quick-lint-js-2.14.0.tar.gz"
+  url "https://c.quick-lint-js.com/releases/2.15.0/source/quick-lint-js-2.15.0.tar.gz"
   license "GPL-3.0-or-later"
   head "https://github.com/quick-lint/quick-lint-js.git", branch: "master"
 
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -6,7 +6,9 @@ based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 quick-lint-js' version numbers are arbitrary. quick-lint-js does *not* adhere to
 Semantic Versioning.
 
-## Unreleased
+## 2.15.0 (2023-07-18)
+
+[Downloads](https://c.quick-lint-js.com/releases/2.15.0/)
 
 ### Added
 
