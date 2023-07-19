cfca01cbdb021b8b318ba5af6e313b5ee826571f
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-07-18 20:13:30 -0700

    refactor(test): fix capitalization of test name (Cli -> CLI)

--- test/test-cli-location.cpp
+++ test/test-cli-location.cpp
@@ -15,7 +15,7 @@
 
 namespace quick_lint_js {
 namespace {
-TEST(Test_Cli_Location, ranges_on_first_line) {
+TEST(Test_CLI_Location, ranges_on_first_line) {
   Padded_String code(u8"let x = 2;"_sv);
   CLI_Locator l(&code);
   CLI_Source_Range x_range = l.range(Source_Code_Span(&code[4], &code[5]));
@@ -29,7 +29,7 @@ TEST(Test_Cli_Location, ranges_on_first_line) {
   EXPECT_EQ(x_range.end().column_number, 6);
 }
 
-TEST(Test_Cli_Location, ranges_on_second_line) {
+TEST(Test_CLI_Location, ranges_on_second_line) {
   for (String8_View line_terminator : line_terminators) {
     Padded_String code(
         concat(u8"let x = 2;"_sv, line_terminator, u8"let y = 3;"_sv));
@@ -47,7 +47,7 @@ TEST(Test_Cli_Location, ranges_on_second_line) {
   }
 }
 
-TEST(Test_Cli_Location, first_character_on_line_has_column_1) {
+TEST(Test_CLI_Location, first_character_on_line_has_column_1) {
   for (String8_View line_terminator : line_terminators) {
     Padded_String code(
         concat(u8"function f() {}"_sv, line_terminator, u8"g();"_sv));
@@ -60,7 +60,7 @@ TEST(Test_Cli_Location, first_character_on_line_has_column_1) {
   }
 }
 
-TEST(Test_Cli_Location, lf_cr_is_two_line_terminators) {
+TEST(Test_CLI_Location, lf_cr_is_two_line_terminators) {
   Padded_String code(u8"let x = 2;\n\rlet y = 3;"_sv);
   const Char8* y = strchr(code.c_str(), u8'y');
   CLI_Locator l(&code);
@@ -71,7 +71,7 @@ TEST(Test_Cli_Location, lf_cr_is_two_line_terminators) {
   EXPECT_EQ(y_range.begin().column_number, 5);
 }
 
-TEST(Test_Cli_Location, location_after_null_byte) {
+TEST(Test_CLI_Location, location_after_null_byte) {
   Padded_String code(String8(u8"hello\0beautiful\nworld"_sv));
   const Char8* r = &code[18];
   ASSERT_EQ(*r, u8'r');
@@ -84,7 +84,7 @@ TEST(Test_Cli_Location, location_after_null_byte) {
   EXPECT_EQ(r_range.begin().column_number, 3);
 }
 
-TEST(Test_Cli_Location, position_backwards) {
+TEST(Test_CLI_Location, position_backwards) {
   Padded_String code(u8"ab\nc\n\nd\nefg\nh"_sv);
 
   std::vector<CLI_Source_Position> expected_positions;
@@ -107,7 +107,7 @@ TEST(Test_Cli_Location, position_backwards) {
   EXPECT_EQ(actual_positions, expected_positions);
 }
 
-TEST(Test_Cli_Location, position_after_multi_byte_character) {
+TEST(Test_CLI_Location, position_after_multi_byte_character) {
   {
     // U+2603 has three UTF-8 code units: e2 98 83
     Padded_String code(u8"\u2603 x"_sv);
@@ -125,7 +125,7 @@ TEST(Test_Cli_Location, position_after_multi_byte_character) {
   }
 }
 
-TEST(Test_Cli_Location, position_within_multi_byte_character) {
+TEST(Test_CLI_Location, position_within_multi_byte_character) {
   // U+2603 has three UTF-8 code units: e2 98 83
   Padded_String code(u8"\u2603"_sv);
   CLI_Locator l(&code);
