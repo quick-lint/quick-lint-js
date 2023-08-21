quick-lint/quick-lint-js

commit 31ba8b8582ea7a6bbdb8151c0fad8b70b796532b

refactor(benchmark-lsp): use new Event_Loop2 for benchmarks
Switch LSP_Server_Process's pipe I/o from the old Event_Loop class to
the new Event_Loop2 class.

This change caught a bug: we were not marking the read pipe as
non-blocking on Linux and macOS, thus the event loop could have
deadlocked with a big client->server message.

benchmark/benchmark-lsp/lsp-server-process.cpp
@@ -128,15 +128,6 @@ void LSP_Server_Process::kill() {
   }
 }
 
-void LSP_Server_Process::stop_event_loop() {
-  // Caller should have flushed our writer.
-#if !QLJS_PIPE_WRITER_SEPARATE_THREAD && QLJS_HAVE_POLL
-  QLJS_ASSERT(!this->message_writer_.get_event_fd().has_value());
-#endif
-
-  this->should_stop_event_loop_ = true;
-}
-
 void LSP_Server_Process::wait_for_exit() { wait_for_process_exit(this->pid_); }
 
 bool LSP_Server_Process::wait_for_exit_for(std::chrono::milliseconds timeout) {

benchmark/benchmark-lsp/quick-lint-js/lsp-server-process.h
@@ -10,7 +10,7 @@
 #include <optional>
 #include <quick-lint-js/assert.h>
 #include <quick-lint-js/container/byte-buffer.h>
-#include <quick-lint-js/io/event-loop.h>
+#include <quick-lint-js/io/event-loop2.h>
 #include <quick-lint-js/io/file-handle.h>
 #include <quick-lint-js/lsp/lsp-message-parser.h>
 #include <quick-lint-js/lsp/lsp-pipe-writer.h>
@@ -184,7 +184,12 @@ enum {
   TEXT_DOCUMENT_SYNC_KIND_INCREMENTAL = 2,
 };
 
-class LSP_Server_Process {
+class LSP_Server_Process : private Event_Loop2_Pipe_Read_Delegate
+#if QLJS_EVENT_LOOP2_PIPE_WRITE
+    ,
+                           private Event_Loop2_Pipe_Write_Delegate
+#endif
+{
  private:
   class Get_Message_Awaitable;
 
@@ -195,6 +200,8 @@ class LSP_Server_Process {
   // stop the server.
   template <class Func>
   void run_and_kill(Func&& func) {
+    Event_Loop2 event_loop;
+
     [[maybe_unused]] LSP_Task<void> task = [&]() -> LSP_Task<void> {
       co_await std::forward<Func>(func)();
       this->message_writer_.flush();  // FIXME(strager): Might deadlock.
@@ -203,25 +210,29 @@ class LSP_Server_Process {
           std::chrono::milliseconds(300));  // FIXME(strager): Might deadlock.
       if (!exited) {
         this->kill();
+        this->wait_for_exit();
 
         // HACK(strager): Flow and Rome don't ever close our reader (its
         // writer), even if we kill the process. (They fork their own processes
         // which we can't directly kill.) Work around this by forcefully
         // stopping our event loop.
-        this->stop_event_loop();
-
-        this->wait_for_exit();
+        event_loop.un_keep_alive();
       }
     }();
-    this->event_loop_.run();
+
+    event_loop.keep_alive();
+#if QLJS_EVENT_LOOP2_READ_PIPE_NON_BLOCKING
+    this->reader_.set_pipe_non_blocking();
+#endif
+    event_loop.register_pipe_read(this->reader_.ref(), this);
+#if QLJS_EVENT_LOOP2_PIPE_WRITE
+    event_loop.register_pipe_write(this->message_writer_.get_pipe_fd(), this);
+#endif
+    event_loop.run();
   }
 
   // Forcefully stop the server.
   void kill();
-
-  // HACK(strager): Close the server->client pipe to make Flow's and Rome's LSP
-  // servers stop.
-  void stop_event_loop();
 
   // Wait for the server to stop on its own. Should be called to clean up
   // OS resources.
@@ -317,59 +328,27 @@ class LSP_Server_Process {
     String8_View* out_message_content_ = nullptr;
   };
 
-  class LSP_Event_Loop : public Event_Loop<LSP_Event_Loop> {
-   public:
-    explicit LSP_Event_Loop(LSP_Server_Process* process) : process_(process) {}
+  void on_pipe_read_data(Event_Loop2_Base*, Platform_File_Ref,
+                         String8_View data) override {
+    this->message_parser_.append(data);
+  }
+  void on_pipe_read_end(Event_Loop2_Base* event_loop,
+                        Platform_File_Ref) override {
+    event_loop->un_keep_alive();
+  }
+  void on_pipe_read_error(Event_Loop2_Base* event_loop, Platform_File_Ref,
+                          Platform_File_IO_Error) override {
+    event_loop->un_keep_alive();
+  }
 
-    std::optional<Platform_File_Ref> get_readable_pipe() const {
-      if (this->process_->should_stop_event_loop_) {
-        return std::nullopt;
-      }
-      return this->process_->reader_.ref();
-    }
-
-    void append(String8_View data) {
-      this->process_->message_parser_.append(data);
-    }
-
-#if QLJS_HAVE_KQUEUE || QLJS_HAVE_POLL
-    std::optional<POSIX_FD_File_Ref> get_pipe_write_fd() {
-      if (!this->process_->writer_.valid()) {
-        return std::nullopt;
-      }
-      return this->process_->message_writer_.get_event_fd();
-    }
+#if QLJS_EVENT_LOOP2_PIPE_WRITE
+  void on_pipe_write_ready(Event_Loop2_Base*, Platform_File_Ref) override {
+    this->message_writer_.on_pipe_write_ready();
+  }
+  void on_pipe_write_end(Event_Loop2_Base*, Platform_File_Ref) override {
+    this->message_writer_.on_pipe_write_end();
+  }
 #endif
-
-#if QLJS_HAVE_KQUEUE
-    void on_pipe_write_event(const struct ::kevent& event) {
-      this->process_->message_writer_.on_poll_event(event);
-    }
-#elif QLJS_HAVE_POLL
-    void on_pipe_write_event(const ::pollfd& event) {
-      this->process_->message_writer_.on_poll_event(event);
-    }
-#endif
-
-#if QLJS_HAVE_KQUEUE
-    void on_fs_changed_kevent(const struct ::kevent&) {}
-
-    void on_fs_changed_kevents() {}
-#endif
-
-#if QLJS_HAVE_INOTIFY
-    std::optional<POSIX_FD_File_Ref> get_inotify_fd() { return std::nullopt; }
-
-    void on_fs_changed_event(const ::pollfd&) {}
-#endif
-
-#if defined(_WIN32)
-    void on_fs_changed_event(::OVERLAPPED*, ::DWORD, ::DWORD) {}
-#endif
-
-   private:
-    LSP_Server_Process* process_;
-  };
 
   explicit LSP_Server_Process(std::filesystem::path server_root,
                               const Benchmark_Config_Server& config,
@@ -396,13 +375,11 @@ class LSP_Server_Process {
   bool need_files_on_disk_;
 
   ::pid_t pid_;
-  bool should_stop_event_loop_ = false;
   Platform_File reader_;  // server -> client
   Platform_File writer_;  // client -> server
 
   Continuing_LSP_Message_Parser message_parser_;
   LSP_Pipe_Writer message_writer_ = LSP_Pipe_Writer(this->writer_.ref());
-  LSP_Event_Loop event_loop_ = LSP_Event_Loop(this);
 
   // This holds the most recently parsed message (if any).
   //

src/quick-lint-js/io/pipe-writer.cpp
@@ -140,6 +140,10 @@ std::optional<POSIX_FD_File_Ref> Non_Blocking_Pipe_Writer::get_event_fd() {
 }
 #endif
 
+Platform_File_Ref Non_Blocking_Pipe_Writer::get_pipe_fd() {
+  return this->pipe_;
+}
+
 #if QLJS_HAVE_KQUEUE
 void Non_Blocking_Pipe_Writer::on_poll_event(const struct ::kevent& event) {
   QLJS_ASSERT(narrow_cast<int>(event.ident) != this->pipe_.get());
@@ -164,6 +168,18 @@ void Non_Blocking_Pipe_Writer::on_poll_event(const ::pollfd& fd) {
   }
 }
 #endif
+
+void Non_Blocking_Pipe_Writer::on_pipe_write_ready() {
+  this->write_as_much_as_possible_now_non_blocking(this->pending_);
+}
+
+QLJS_WARNING_PUSH
+QLJS_WARNING_IGNORE_CLANG("-Wmissing-noreturn")
+QLJS_WARNING_IGNORE_GCC("-Wsuggest-attribute=noreturn")
+
+void Non_Blocking_Pipe_Writer::on_pipe_write_end() { QLJS_UNIMPLEMENTED(); }
+
+QLJS_WARNING_POP
 
 void Non_Blocking_Pipe_Writer::write(Byte_Buffer&& data) {
   this->pending_.append(std::move(data));

src/quick-lint-js/io/pipe-writer.h
@@ -93,6 +93,7 @@ class Non_Blocking_Pipe_Writer {
 #if QLJS_HAVE_KQUEUE || QLJS_HAVE_POLL
   std::optional<POSIX_FD_File_Ref> get_event_fd();
 #endif
+  Platform_File_Ref get_pipe_fd();
 
 #if QLJS_HAVE_KQUEUE
   void on_poll_event(const struct ::kevent &);
@@ -100,6 +101,9 @@ class Non_Blocking_Pipe_Writer {
 #if QLJS_HAVE_POLL
   void on_poll_event(const ::pollfd &);
 #endif
+
+  void on_pipe_write_ready();
+  void on_pipe_write_end();
 
  private:
   void write_as_much_as_possible_now_non_blocking(Byte_Buffer_IOVec &);

src/quick-lint-js/lsp/lsp-pipe-writer.h
@@ -29,6 +29,9 @@ class LSP_Pipe_Writer : public LSP_Endpoint_Remote, private Pipe_Writer {
   using Pipe_Writer::flush;
 #if !QLJS_PIPE_WRITER_SEPARATE_THREAD && QLJS_HAVE_POLL
   using Pipe_Writer::get_event_fd;
+  using Pipe_Writer::get_pipe_fd;
+  using Pipe_Writer::on_pipe_write_end;
+  using Pipe_Writer::on_pipe_write_ready;
   using Pipe_Writer::on_poll_event;
 #endif
 };

commit fc15a4059b41a4e4d6258bf87aeb68871bc50c41

fix(benchmark-lsp): fix duplicate/broken set_pipe_non_blocking call
In commit 31ba8b85, I mentioned in the commit message that the patch
caught a bug. This is incorrect; the patch introduced a bug by
undefining QLJS_EVENT_LOOP_READ_PIPE_NON_BLOCKING, then worked around
the bug elsewhere.

Fix the original bug by using the correct macro. Then delete the
duplicate set_pipe_non_blocking call.

Refs: 31ba8b8582ea7a6bbdb8151c0fad8b70b796532b

benchmark/benchmark-lsp/lsp-server-process.cpp
@@ -107,7 +107,7 @@ LSP_Server_Process LSP_Server_Process::spawn(
 
   posix_spawn_file_actions_destroy(&file_actions);
 
-#if QLJS_EVENT_LOOP_READ_PIPE_NON_BLOCKING
+#if QLJS_EVENT_LOOP2_READ_PIPE_NON_BLOCKING
   server_to_client.reader.set_pipe_non_blocking();
 #endif
 #if !QLJS_PIPE_WRITER_SEPARATE_THREAD

benchmark/benchmark-lsp/quick-lint-js/lsp-server-process.h
@@ -221,9 +221,6 @@ class LSP_Server_Process : private Event_Loop2_Pipe_Read_Delegate
     }();
 
     event_loop.keep_alive();
-#if QLJS_EVENT_LOOP2_READ_PIPE_NON_BLOCKING
-    this->reader_.set_pipe_non_blocking();
-#endif
     event_loop.register_pipe_read(this->reader_.ref(), this);
 #if QLJS_EVENT_LOOP2_PIPE_WRITE
     event_loop.register_pipe_write(this->message_writer_.get_pipe_fd(), this);