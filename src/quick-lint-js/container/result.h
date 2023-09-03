418ad5646961f6e10bbc4598fd1a1bbd025f8405
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-09-03 14:28:14 -0700

    refactor(container): rewrite Result class
    * Use a storage byte array instead of Variant/union.
    * Combine Result and Result_Base.
    * Delete some unused functions.
    * Keep the same public interface for compatibility.

--- src/quick-lint-js/container/result.h
+++ src/quick-lint-js/container/result.h
@@ -4,9 +4,8 @@
 #pragma once
 
 #include <new>
-#include <optional>
 #include <quick-lint-js/assert.h>
-#include <quick-lint-js/container/variant.h>
+#include <quick-lint-js/port/math.h>
 #include <quick-lint-js/port/unreachable.h>
 #include <string>
 #include <type_traits>
@@ -15,13 +14,11 @@
 namespace quick_lint_js {
 template <class T, class Error>
 class Result;
-template <class T, class Error>
-class Result_Base;
 
 // Do not use result_propagation directly. Call Result<>::propagate instead.
 template <class T, class Error>
 struct Result_Propagation {
-  Result_Base<T, Error>& to_propagate;
+  Result<T, Error>& to_propagate;
 };
 
 // Do not use result_error directly. Call failed_result instead.
@@ -38,99 +35,111 @@ Result_Error<Error&&> failed_result(Error&& e) {
   return Result_Error<Error&&>{.e = std::forward<Error&&>(e)};
 }
 
-// Do not use result_base directly. Use result instead.
-template <class T, class Error>
-class Result_Base {
- private:
-  template <class U>
-  using To_Value_Type =
-      std::conditional_t<std::is_same_v<U, void>, Monostate, U>;
+// Do not use Result<Result_Monostate, ...> directly. Use Result<void, ...>
+// instead.
+struct Result_Monostate {};
 
-  using Result = quick_lint_js::Result<T, Error>;
-  using Value_Type = To_Value_Type<T>;
+// Like std::variant<T, Error> or std::expected<T, Error>, but more ergonomic.
+template <class T, class Error>
+class Result {
+ private:
+  inline static constexpr bool has_void_value_type = std::is_same_v<T, void>;
+
+  using Stored_Value_Type =
+      std::conditional_t<has_void_value_type, Result_Monostate, T>;
+
+  using Value_Type_Ref =
+      std::conditional_t<has_void_value_type, void, Stored_Value_Type&>;
+  using Value_Type_Ref_Ref =
+      std::conditional_t<has_void_value_type, void, Stored_Value_Type&&>;
+  using Value_Type_Const_Ref =
+      std::conditional_t<has_void_value_type, void, const Stored_Value_Type&>;
 
  public:
   // TODO(strager): Make explicit iff T(Args...) is explicit.
   template <class... Args,
-            class = decltype(Value_Type(std::declval<Args>()...))>
-  /*implicit*/ Result_Base(Args&&... args)
-      : data_(std::in_place_index<0>, std::forward<Args>(args)...) {}
+            class = decltype(Stored_Value_Type(std::declval<Args>()...))>
+  /*implicit*/ Result(Args&&... args) {
+    new (this->value_storage()) Stored_Value_Type(std::forward<Args>(args)...);
+    this->is_ok_ = true;
+  }
 
   template <class E>
-  /*implicit*/ Result_Base(Result_Error<E>&& error)
-      // TODO(strager): If std::is_same_v<E, value_type>, then
-      // std::in_place_type is incorrect.
-      : data_(std::in_place_type<std::decay_t<E>>, std::forward<E>(error.e)) {}
-
-  // Private constructor used by propagate. Do not call directly.
-  /*implicit*/ Result_Base(Result_Propagation<T, Error>&& propagation)
-      : data_(std::move(propagation.to_propagate).data_) {}
+  /*implicit*/ Result(Result_Error<E>&& error) {
+    new (this->error_storage()) Error(std::forward<E>(error.e));
+    this->is_ok_ = false;
+  }
 
   // Private constructor used by propagate. Do not call directly.
   template <class U, class Other_E>
-  /*implicit*/ Result_Base(Result_Propagation<U, Other_E>&& propagation)
-      : data_(quick_lint_js::visit(Propagate_Visitor<U>(),
-                                   std::move(propagation.to_propagate).data_)) {
+  /*implicit*/ Result(Result_Propagation<U, Other_E>&& propagation) {
+    new (this->error_storage())
+        Error(std::move(propagation.to_propagate).error());
+    this->is_ok_ = false;
   }
 
   // TODO(strager): Allow copying.
-  Result_Base(const Result_Base&) = delete;
-  Result_Base& operator=(const Result_Base&) = delete;
+  Result(const Result&) = delete;
+  Result& operator=(const Result&) = delete;
 
-  Result_Base(Result_Base&&) = default;
-  Result_Base& operator=(Result_Base&&) = default;
+  Result(Result&& other) { this->move_construct_from(std::move(other)); }
 
-  template <class Other_E>
-  /*implicit*/ Result_Base(quick_lint_js::Result<T, Other_E>&& other)
-      : data_(visit(Move_Data_Visitor(), std::move(other).data_)) {}
+  Result& operator=(Result&& other) {
+    if (this->ok() && other.ok()) {
+      *this->value_storage() = std::move(*other.value_storage());
+    } else if (!this->ok() && !other.ok()) {
+      *this->error_storage() = std::move(*other.error_storage());
+    } else {
+      this->destroy();
+      this->move_construct_from(std::move(other));
+    }
+    return *this;
+  }
+
+  ~Result() { this->destroy(); }
 
   // FIXME(strager): The following code is a landmine:
   //
   //   r = failed_result(std::move(r.error()));
   template <class E>
-  Result_Base& operator=(Result_Error<E>&& error) {
-    // TODO(strager): If std::is_same_v<E, value_type>, then the emplace
-    // template parameter is incorrect.
-    this->data_.template emplace<std::decay_t<E>>(std::forward<E>(error.e));
+  Result& operator=(Result_Error<E>&& error) {
+    this->destroy();
+
+    new (this->error_storage()) Error(std::forward<E>(error.e));
+    this->is_ok_ = false;
     return *this;
   }
 
-  bool ok() const { return this->data_.index() == 0; }
+  bool ok() const { return this->is_ok_; }
 
-  Value_Type& value() & {
+  Value_Type_Ref value() & {
     QLJS_ASSERT(this->ok());
-    return get<0>(this->data_);
+    return *this->value_storage();
   }
 
-  Value_Type&& value() && {
+  Value_Type_Ref_Ref value() && {
     QLJS_ASSERT(this->ok());
-    return get<0>(std::move(this->data_));
+    return std::move(*this->value_storage());
   }
 
-  const Value_Type& value() const& {
+  Value_Type_Const_Ref value() const& {
     QLJS_ASSERT(this->ok());
-    return get<0>(this->data_);
+    return *this->value_storage();
   }
 
   const Error& error() const& {
     QLJS_ASSERT(!this->ok());
-    // TODO(strager): If std::is_same_v<E, value_type>, then get<Error>
-    // is incorrect.
-    return get<Error>(this->data_);
+    return *this->error_storage();
   }
 
   Error&& error() && {
     QLJS_ASSERT(!this->ok());
-    // TODO(strager): If std::is_same_v<Error, value_type>, then get<Error>
-    // is incorrect.
-    return get<Error>(std::move(this->data_));
+    return std::move(*this->error_storage());
   }
 
   std::string error_to_string() const {
     QLJS_ASSERT(!this->ok());
-    // TODO(strager): If std::is_same_v<Error, value_type>, then visit is
-    // incorrect.
-    return visit(To_String_Visitor(), this->data_);
+    return this->error_storage()->to_string();
   }
 
   Result_Propagation<T, Error> propagate() & {
@@ -140,109 +149,69 @@ class Result_Base {
 
   Result_Propagation<T, Error> propagate() && = delete;
 
-  // For tests.
-  template <class U>
-  friend bool holds_alternative(const Result_Base<T, Error>& r) {
-    // TODO(strager): Make this work properly if T is void.
-    return holds_alternative<U>(r.data_);
+  Value_Type_Ref operator*() & { return this->value(); }
+  Value_Type_Ref_Ref operator*() && { return std::move(*this).value(); }
+  Value_Type_Const_Ref operator*() const& { return this->value(); }
+
+  T* operator->() { return &this->value(); }
+  const T* operator->() const { return &this->value(); }
+
+  friend bool operator==(const Result& lhs, const Result& rhs) {
+    if (lhs.ok() != rhs.ok()) {
+      return false;
+    }
+    if (lhs.ok()) {
+      return lhs.value() == rhs.value();
+    } else {
+      return lhs.error() == rhs.error();
+    }
   }
 
-  // For tests.
-  template <class U>
-  friend const U& get(const Result_Base<T, Error>& r) {
-    // TODO(strager): Make this work properly if T is void.
-    return get<U>(r.data_);
-  }
-
-  Value_Type& operator*() & { return this->value(); }
-  Value_Type&& operator*() && { return std::move(*this).value(); }
-  const Value_Type& operator*() const& { return this->value(); }
-
-  Value_Type* operator->() { return &this->value(); }
-  const Value_Type* operator->() const { return &this->value(); }
-
-  friend bool operator==(const Result_Base& lhs, const Result_Base& rhs) {
-    return lhs.data_ == rhs.data_;
-  }
-
-  friend bool operator!=(const Result_Base& lhs, const Result_Base& rhs) {
+  friend bool operator!=(const Result& lhs, const Result& rhs) {
     return !(lhs == rhs);
   }
 
  private:
-  struct Move_Data_Visitor {
-    template <class TOrError>
-    auto operator()(TOrError&& datum) {
-      // TODO(strager): If (std::is_same_v<Errors, value_type> || ...),
-      // then std::in_place_type is incorrect.
-      return Variant<Value_Type, Error>(
-          std::in_place_type<std::decay_t<TOrError>>, std::move(datum));
+  Stored_Value_Type* value_storage() {
+    return reinterpret_cast<Stored_Value_Type*>(this->storage_);
+  }
+  const Stored_Value_Type* value_storage() const {
+    return reinterpret_cast<const Stored_Value_Type*>(this->storage_);
+  }
+
+  Error* error_storage() { return reinterpret_cast<Error*>(this->storage_); }
+  const Error* error_storage() const {
+    return reinterpret_cast<const Error*>(this->storage_);
+  }
+
+  template <class Other_Result>
+  void move_construct_from(Other_Result&& other) {
+    if (other.ok()) {
+      new (this->value_storage())
+          Stored_Value_Type(std::move(*other.value_storage()));
+      this->is_ok_ = true;
+    } else {
+      new (this->error_storage()) Error(std::move(*other.error_storage()));
+      this->is_ok_ = false;
     }
-  };
+  }
 
-  template <class U>
-  struct Propagate_Visitor {
-    Variant<Value_Type, Error> operator()(To_Value_Type<U>&&) {
-      QLJS_UNREACHABLE();
+  void destroy() {
+    if (this->ok()) {
+      this->value_storage()->~Stored_Value_Type();
+    } else {
+      this->error_storage()->~Error();
     }
+  }
 
-    Variant<Value_Type, Error> operator()(Error&& error) {
-      // TODO(strager): If (std::is_same_v<Error, value_type> || ...), then
-      // std::in_place_type is incorrect.
-      return Variant<Value_Type, Error>(std::in_place_type<std::decay_t<Error>>,
-                                        std::move(error));
-    }
-  };
-
-  struct To_String_Visitor {
-    std::string operator()(const Value_Type&) { QLJS_UNREACHABLE(); }
-
-    std::string operator()(const Error& error) { return error.to_string(); }
-  };
-
-  Variant<Value_Type, Error> data_;
-
-  template <class, class>
-  friend class Result_Base;
-};
-
-// Like variant<T, Error>, but more ergonomic.
-template <class T, class Error>
-class Result : public Result_Base<T, Error> {
- private:
-  using Base = Result_Base<T, Error>;
-
- public:
-  using Base::Base;
-  using Base::error;
-  using Base::error_to_string;
-  using Base::ok;
-  using Base::operator*;
-  using Base::operator->;
-  using Base::operator=;
-  using Base::propagate;
-  using Base::value;
-
-  template <class, class>
-  friend class Result_Base;
-};
-
-// Like std::optional<Error>, but with a similar interface to Result<T, Error>.
-template <class Error>
-class Result<void, Error> : public Result_Base<void, Error> {
- private:
-  using Base = Result_Base<void, Error>;
-
- public:
-  using Base::Base;
-  using Base::error;
-  using Base::error_to_string;
-  using Base::ok;
-  using Base::operator=;
-  using Base::propagate;
-
-  template <class, class>
-  friend class Result_Base;
+  bool is_ok_;
+  // If is_ok_ == true, then storage_ holds an object of type T or
+  // Result_Monostate.
+  // If is_ok_ == false, then storage_ holds an object of type Error.
+  alignas(
+      maximum(alignof(Stored_Value_Type),
+              alignof(Error))) char storage_[maximum(sizeof(Stored_Value_Type),
+                                                     sizeof(Error))];
 };
 }
 
