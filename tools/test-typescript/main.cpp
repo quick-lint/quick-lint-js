31c4befffb6108513cb9295b42523aead97515bf
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-12-27 15:40:54 -0500

    fix(test-typescript): disable failing UTF-16 test

--- tools/test-typescript/main.cpp
+++ tools/test-typescript/main.cpp
@@ -37,7 +37,9 @@ constexpr String8_View ignored_tests[] = {
     u8"/importedModuleClassNameClash.ts"sv,
 
     // TODO(#1123): quick-lint-js does not parse UTF-16 source files.
+    u8"/bom-utf16le.ts"sv,
     u8"/unicodeIdentifierNames.ts"sv,
+    u8"/collisionCodeGenModuleWithUnicodeNames.ts"sv,
 
     // This test correctly emits E0196 (a warning).
     // TODO(strager): Disable E0196 for this test but still check this test.
