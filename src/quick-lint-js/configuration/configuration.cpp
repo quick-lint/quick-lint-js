ac96b53e56a3467f901d70e3c7d877c6136d7960
Matthew "strager" Glazar <strager.nds@gmail.com>
2024-01-20 20:32:36 -0500

    refactor(configuration): swap '==' for readability

--- src/quick-lint-js/configuration/configuration.cpp
+++ src/quick-lint-js/configuration/configuration.cpp
@@ -410,10 +410,10 @@ bool Configuration::should_remove_global_variable(String8_View name) {
       std::ptrdiff_t group_index = &group - global_groups;
       // clang-format off
       const char* prefix =
-          group.globals == group_globals ? "" :
-          group.non_shadowable_globals == group_globals ? "non_shadowable_" :
-          group.non_writable_globals == group_globals ? "non_writable_" :
-          group.type_only_globals == group_globals ? "type_only_" :
+          group_globals == group.globals ? "" :
+          group_globals == group.non_shadowable_globals ? "non_shadowable_" :
+          group_globals == group.non_writable_globals ? "non_writable_" :
+          group_globals == group.type_only_globals ? "type_only_" :
           "???";
       // clang-format on
       std::fprintf(
